bbdc57eb977b605d65f11234508dfb57000fc82b
William Chong <williamchong007@msn.com>
2019-01-25 21:51:46 +0800

    ✨ Add simple reader and list

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,77 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-navigation-drawer
+      fixed
+      app
+    >
+      <v-list dense>
+        <v-list-tile
+          v-for="user in $store.state.users"
+          :key="user"
+          @click="setUser(user)"
+        >
+          <v-list-tile-content>
+            <v-list-tile-title>{{ user }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout justify-center align-center>
+          <v-flex>
+            <v-list>
+              <v-list-tile
+                v-for="item in list"
+                :key="item.url"
+                :href="item.url"
+                target="blank"
+              >
+                <v-list-tile-avatar>
+                  <img :src="item.image">
+                </v-list-tile-avatar>
+                <v-list-tile-content>
+                  <v-list-tile-title>{{ item.title }} - {{ item.user }}</v-list-tile-title>
+                  <v-list-tile-sub-title>{{ item.description }}</v-list-tile-sub-title>
+                </v-list-tile-content>
+              </v-list-tile>
+            </v-list>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-footer color="blue-grey" class="white--text" app>
+      <span>Vuetify</span>
+      <v-spacer></v-spacer>
+      <span>&copy; 2017</span>
+    </v-footer>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<script>
+
+export default {
+  name: 'App',
+  computed: {
+    list() {
+      if (!this.$store.state.users) return [];
+      if (!this.user) return this.$store.getters.getAllArticles;
+      return this.$store.state.articles[this.user];
+    },
+  },
+  data() {
+    return {
+      user: '',
+    };
+  },
+  async mounted() {
+    await this.$store.dispatch('fetchUser');
+    this.$store.state.users.forEach(u => this.$store.dispatch('fetchArticle', u));
+  },
+  methods: {
+    setUser(user) {
+      this.user = user;
+    },
+  },
+};
+</script>
--- src/store.js
+++ src/store.js
@@ -1,16 +1,43 @@
+/* eslint-disable no-param-reassign */
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    users: ['a'],
+    articles: { a: ['test'] },
   },
   mutations: {
-
+    UPDATE_USER_LIST(state, users) {
+      state.users = users;
+    },
+    UPDATE_USER_URL(state, { user, list }) {
+      Vue.set(state.articles, user, list);
+    },
   },
   actions: {
-
+    async fetchUser({ commit }) {
+      const { data } = await axios.get('https://api.rinkeby.like.co/like/info/liked/list', { withCredentials: true });
+      commit('UPDATE_USER_LIST', data.list);
+    },
+    async fetchArticle({ commit }, user) {
+      const { data } = await axios.get(`https://api.rinkeby.like.co/like/info/user/${user}/latest`, { withCredentials: true });
+      commit('UPDATE_USER_URL', { user, list: data.list });
+    },
+  },
+  getters: {
+    getAllArticles: (state) => {
+      const res = state.users.reduce((a, u) => {
+        if (state.articles[u]) {
+          return a.concat(state.articles[u]);
+        }
+        return a;
+      }, []);
+      res.sort((a, b) => a.ts > b.ts);
+      return res;
+    },
   },
 });
