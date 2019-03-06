4a9fcb8e55d169a932bad23751a958b63b6630ed
William Chong <williamchong007@msn.com>
2019-03-06 18:06:26 +0800

    ♻️ Move server api env into config

--- src/server/api/util/api.js
+++ src/server/api/util/api.js
@@ -1,7 +1,9 @@
-const IS_TESTNET = process.env.IS_TESTNET; // eslint-disable-line prefer-destructuring
-const ENV_EXTERNAL_URL = process.env.EXTERNAL_URL;
-const CLIENT_ID = process.env.LIKE_CO_CLIENT_ID;
-const CLIENT_SECRET = process.env.LIKE_CO_CLIENT_SECRET;
+const {
+  IS_TESTNET,
+  EXTERNAL_URL: CONFIG_EXTERNAL_URL,
+  LIKE_CO_CLIENT_ID,
+  LIKE_CO_CLIENT_SECRET,
+} = require('../../config/config');
 
 const LIKECOIN_API_BASE = IS_TESTNET
   ? 'https://api.rinkeby.like.co'
@@ -10,7 +12,7 @@ const LIKE_CO_URL_BASE = IS_TESTNET
   ? 'https://rinkeby.like.co'
   : 'https://like.co';
 const EXTERNAL_URL =
-  ENV_EXTERNAL_URL || IS_TESTNET
+  CONFIG_EXTERNAL_URL || IS_TESTNET
     ? 'https://civic-liker-develop.firebaseapp.com'
     : 'https://civic-liker.firebaseapp.com';
 const OAUTH_REDIRECT_URI = encodeURIComponent(`${EXTERNAL_URL}/redirect`);
@@ -19,7 +21,7 @@ const getFetchLikedUserApi = () => `${LIKECOIN_API_BASE}/like/info/liked/list`;
 const getFetchUserArticlesAPI = user =>
   `${LIKECOIN_API_BASE}/like/info/user/${user}/latest`;
 const getOAuthCallbackAPI = authCode =>
-  `${LIKE_CO_URL_BASE}/api/oauth/access_token?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=authorization_code&redirect_uri=${OAUTH_REDIRECT_URI}&auth_code=${authCode}`;
+  `${LIKE_CO_URL_BASE}/api/oauth/access_token?client_id=${LIKE_CO_CLIENT_ID}&client_secret=${LIKE_CO_CLIENT_SECRET}&grant_type=authorization_code&redirect_uri=${OAUTH_REDIRECT_URI}&auth_code=${authCode}`;
 
 module.exports = {
   getFetchLikedUserApi,
