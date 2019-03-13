0b7e3f980fcda418e0faa68e43112ae04ad2e89d
William Chong <williamchong007@msn.com>
2019-03-13 13:33:06 +0800

    ✨ Add nuxtServerInit for user fetch and autheticated middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,22 @@
 /* eslint no-param-reassign: "off" */
 import Vuex from 'vuex'; // eslint-disable-line import/no-extraneous-dependencies
+import * as types from '@/store/mutation-types';
+import * as api from '@/util/api';
 import reader from './modules/reader';
 import user from './modules/user';
 
 const createStore = () =>
   new Vuex.Store({
+    actions: {
+      async nuxtServerInit({ commit }, ctx) {
+        try {
+          const { user: id } = await this.$axios.$get(api.getLoginStatus());
+          commit(types.USER_SET_USER_INFO, { id });
+        } catch (err) {
+          // no op
+        }
+      },
+    },
     modules: {
       reader,
       user,
