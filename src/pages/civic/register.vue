2b1e52760a519f7a85c41b41e1ea8467839bb3a6
William Chong <williamchong007@msn.com>
2019-03-19 14:46:11 +0800

    👌 Move paypal page url into util api

--- src/pages/civic/register.vue
+++ src/pages/civic/register.vue
@@ -4,16 +4,13 @@
 
 
 <script>
-import { IS_TESTNET } from '@/constant';
+import { getPaypalPaymentPageURL } from '@/util/api';
 
 export default {
   middleware: 'authenticated',
   asyncData({ store, redirect }) {
-    const LikerId = store.getters.getUserId;
-    const paypalPaymentPage = IS_TESTNET
-      ? `https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=LFJTWE4PM4VGQ&on0=LikerID&os0=${LikerId}`
-      : `https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=65HMAZFASPMJQ&on0=LikerID&os0=${LikerId}`;
-    redirect(paypalPaymentPage);
+    const likerId = store.getters.getUserId;
+    redirect(getPaypalPaymentPageURL(likerId));
   },
 };
 </script>
--- src/util/api/index.js
+++ src/util/api/index.js
@@ -1,3 +1,9 @@
+import { IS_TESTNET } from '@/constant';
+
+export const getPaypalPaymentPageURL = likerId =>
+  IS_TESTNET
+    ? `https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=LFJTWE4PM4VGQ&on0=LikerID&os0=${likerId}`
+    : `https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=65HMAZFASPMJQ&on0=LikerID&os0=${likerId}`;
 export const getFetchLikedUserApi = () => `/api/reader/index`;
 export const getFetchUserArticlesAPI = user => `/api/reader/user/${user}`;
 export const getSubscribeUserAPI = user => `/api/reader/subscribe/user/${user}`;
