a2c2e1be2d1ab76233db4122e3b361ef19406c74
Ng Wing Tat, David <i@ngwingt.at>
2019-03-21 17:40:26 +0800

    🚧 Add settings page base

---
+++ src/pages/settings.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="settings-page bg-gray-f7">
+    <PageHeader>
+      <template>
+        <SiteNavBar class="text-like-green" />
+      </template>
+    </PageHeader>
+  </div>
+</template>
+
+<script>
+import PageHeader from '~/components/PageHeader';
+import SiteNavBar from '~/components/SiteNavBar';
+
+export default {
+  name: 'SettingsPage',
+  components: {
+    PageHeader,
+    SiteNavBar,
+  },
+};
+</script>
