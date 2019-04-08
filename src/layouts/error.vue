57d2dbe26161dbeeb3a6141cd05f062e5463293d
Ng Wing Tat, David <i@ngwingt.at>
2019-04-08 16:02:20 +0800

    💄 Remade error page

--- src/layouts/error.vue
+++ src/layouts/error.vue
@@ -1,23 +1,40 @@
 <template>
-  <main class="error-page">
-
-    <h1 class="text-24 mt-16">{{ formattedMessage }}</h1>
-
-    <div class="mt-32 px-80 phone:px-0">
-      <a
-        v-if="isLoginError"
-        class="btn btn--outlined btn--block"
-        :href="getOAuthLoginAPI()"
-      >{{ $t('signInOrSignUp') }}</a>
-      <NuxtLink
-        class="btn btn--outlined btn--block"
-        :to="{ name: 'index' }"
+  <Transition
+    name="error-dialog-"
+    appear
+  >
+    <div class="error-dialog page-wrapper">
+      <Transition
+        name="error-dialog-content-"
+        appear
       >
-        {{ $t('backToHome') }}
-      </NuxtLink>
-    </div>
+        <main class="error-dialog-content">
 
-  </main>
+          <h1 class="text-24 mt-16">{{ formattedMessage }}</h1>
+
+          <div class="mt-32 px-12 phone:px-0">
+            <div v-if="isLoginError">
+              <a
+                class="btn btn--outlined"
+                :href="getOAuthLoginAPI()"
+              >{{ $t('signIn') }}</a>
+              <a
+                class="btn btn--outlined"
+                :href="getOAuthLoginAPI()"
+              >{{ $t('signUp') }}</a>
+            </div>
+            <a
+              class="btn btn--plain btn--auto-size text-14"
+              href="/"
+            >
+              {{ $t('backToHome') }}
+            </a>
+          </div>
+
+        </main>
+      </Transition>
+    </div>
+  </Transition>
 </template>
 
 <script>
@@ -31,7 +48,15 @@ const LOGIN_ERROR_MESSAGE_SET = new Set([
 ]);
 
 export default {
-  layout: 'dialog',
+  layout: 'empty',
+  transition: {
+    name: '',
+    css: false,
+  },
+  layoutTransition: {
+    name: '',
+    css: false,
+  },
   mixins: [IntercomMixin],
   props: {
     error: {
@@ -79,3 +104,73 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+.error-dialog {
+  @apply absolute;
+  @apply pin;
+
+  @apply flex;
+  @apply justify-center;
+  @apply items-center;
+
+  @apply bg-like-green;
+
+  @apply p-16;
+
+  &-- {
+    &enter,
+    &leave-to {
+      opacity: 0;
+    }
+
+    &enter-active,
+    &leave-active {
+      transition-property: opacity !important;
+      transition-duration: 0.5;
+    }
+    &enter-active {
+      transition-timing-function: cubic-bezier(0, 0, 0.3, 1);
+    }
+    &leave-active {
+      transition-timing-function: cubic-bezier(0.7, 0, 1, 1);
+    }
+  }
+
+  &-content {
+    @apply flex-no-grow;
+
+    @apply text-like-green;
+    @apply text-center;
+
+    @apply bg-white;
+
+    @apply rounded;
+
+    @apply p-24;
+
+    @apply w-full;
+    @apply max-w-phone;
+
+    &-- {
+      &enter,
+      &leave-to {
+        opacity: 0;
+        transform: translateY(100px) scale(0.95);
+      }
+
+      &enter-active,
+      &leave-active {
+        transition-property: opacity, transform !important;
+        transition-duration: 0.5s;
+      }
+      &enter-active {
+        transition-timing-function: cubic-bezier(0, 0, 0.3, 1);
+      }
+      &leave-active {
+        transition-timing-function: cubic-bezier(0.7, 0, 1, 1);
+      }
+    }
+  }
+}
+</style>
