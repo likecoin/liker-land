7caab75cdb72cfa4639f1032ba7f5124b33044c7
Ng Wing Tat, David <i@ngwingt.at>
2019-04-16 18:29:22 +0800

    ✨ Move language select to settings page

---
+++ src/pages/settings/language.vue
@@ -0,0 +1,71 @@
+<template>
+  <ul class="language-list-view list-view list-view--rounded">
+    <li
+      v-for="locale in getAvailableLocales"
+      :key="locale"
+      class="list-view-item"
+    >
+      <button
+        class="list-view-item__content-view"
+        @click="onClickLocale(locale)"
+      >
+        <span>{{ $t(`Locale.${locale}`) }}</span>
+      </button>
+
+      <div
+        v-if="getLocale === locale"
+        class="list-view-item__accessory-view"
+      >
+        <TickIcon />
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+
+import TickIcon from '~/assets/icons/tick.svg';
+
+export default {
+  components: {
+    TickIcon,
+  },
+  computed: {
+    ...mapGetters(['getAvailableLocales', 'getLocale']),
+  },
+  methods: {
+    ...mapActions(['setLocale']),
+
+    onClickLocale(locale) {
+      this.$i18n.locale = locale;
+      this.setLocale(locale);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.language-list-view {
+  .list-view-item {
+    &__content-view {
+      @apply text-gray-4a;
+    }
+
+    &__accessory-view {
+      @apply flex;
+      @apply justify-end;
+      @apply items-center;
+
+      svg {
+        @apply text-like-cyan;
+
+        @apply w-24;
+        @apply h-24;
+
+        @apply fill-current;
+      }
+    }
+  }
+}
+</style>
