cfa3a8abc0c3101f56768b23bef48ce3ca95b0a5
William Chong <williamchong007@msn.com>
2019-05-10 20:54:02 +0800

    ⚡️ Add keep-alive in server axios

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -141,6 +141,7 @@ const nuxtConfig = {
     '~/plugins/likecoin-ui-vue.js',
     '~/plugins/portal-vue.js',
     '~/plugins/vue-i18n.js',
+    { src: '~/plugins/axios.server.js', mode: 'server' },
     { src: '~/plugins/ui-plugin.client.js', ssr: false },
     { src: '~/plugins/vue-cookie.client.js', ssr: false },
     { src: '~/plugins/vue-intercom', ssr: false },
