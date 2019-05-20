5f3e20b7dbbbf2a19115a940ec0f3aef657e3fdc
William Chong <williamchong007@msn.com>
2019-05-20 19:35:06 +0800

    ✨ Add stripe checkout result page

---
+++ src/pages/civic/payment/stripe/success.vue
@@ -0,0 +1,173 @@
+<template>
+  <div class="payment-success-page">
+    <div>
+      <transition
+        name="payment-success-page-hero-"
+        appear
+      >
+        <div class="payment-success-page-hero">
+          <RadialBlastGraph class="payment-success-page-hero__bg" />
+
+          <div class="payment-success-page-hero__avatar">
+            <lc-avatar
+              :src="getUserInfo.avatar"
+              size="144"
+              halo="civic-liker"
+            />
+          </div>
+        </div>
+      </transition>
+
+      <div class="payment-success-page-receipt">
+        <div class="payment-success-page-receipt__header bg-like-gradient">
+          <TickIcon
+            class="w-48 h-48 text-like-cyan fill-current"
+          />
+          <div class="text-like-green">
+            {{ $t('PaymentSuccessPage.heading') }}
+          </div>
+        </div>
+        <div class="payment-success-page-receipt__body">
+          <p>{{ $t('PaymentSuccessPage.body') }}</p>
+
+          <div class="mt-24 phone:mx-32 laptop:mx-64 desktop:mx-64">
+            <NuxtLink
+              class="btn btn--outlined btn--block max-w-3 laptop:max-w-2/3 desktop:max-w-2/3 mx-auto my-0 "
+              :to="getHomeRoute"
+            > {{ $t('PaymentSuccessPage.continue') }}</NuxtLink>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { mapActions, mapGetters } from 'vuex';
+import { logTrackerEvent } from '~/util/EventLogger';
+
+import RadialBlastGraph from '~/assets/images/civic/radial-blast.svg';
+import TickIcon from '~/assets/icons/tick.svg';
+
+export default {
+  components: {
+    RadialBlastGraph,
+    TickIcon,
+  },
+  data() {
+    return {};
+  },
+  middleware: 'authenticated',
+  computed: {
+    ...mapGetters(['getUserInfo', 'getHomeRoute']),
+  },
+  head() {
+    return {
+      title: this.$t('PaymentSuccessPage.title'),
+    };
+  },
+  mounted() {
+    logTrackerEvent(
+      this,
+      'Civic',
+      'CivicPaymentSuccess',
+      'CivicPaymentSuccess(stripe)',
+      1
+    );
+    try {
+      this.setUserCivicLiker();
+      logTrackerEvent(
+        this,
+        'Civic',
+        'CivicRegisterComplete',
+        'CivicRegisterComplete(stripe)',
+        1
+      );
+      setTimeout(() => {
+        this.$router.push({
+          name: 'index',
+        });
+      }, 3000);
+    } catch (err) {
+      console.error(err); // eslint-disable-line no-console
+      throw err;
+    }
+  },
+  methods: {
+    ...mapActions(['setUserCivicLiker']),
+  },
+};
+</script>
+
+<style lang="scss">
+.payment-success-page {
+  @apply bg-gray-f7;
+
+  @apply items-center;
+
+  > div {
+    @apply w-full;
+    @apply max-w-phone;
+  }
+
+  &-hero {
+    padding-top: 60%;
+
+    @apply relative;
+    @apply overflow-hidden;
+
+    @apply max-w-phone;
+    @apply w-full;
+
+    &__bg {
+      top: 50%;
+      left: 50%;
+      transform: translateX(-50%) translateY(-50%);
+
+      @apply absolute;
+    }
+
+    &__avatar {
+      @apply absolute;
+      @apply pin;
+
+      @apply flex;
+      @apply justify-center;
+      @apply items-center;
+    }
+
+    &__header {
+      @apply p-8;
+    }
+  }
+
+  &-receipt {
+    @apply relative;
+    @apply z-10;
+    @apply overflow-hidden;
+    @apply rounded;
+
+    @apply bg-white;
+
+    @apply mx-24;
+
+    &__header {
+      @apply text-16;
+      @apply text-center;
+
+      @apply pt-16 px-24 pb-8;
+    }
+
+    &__body {
+      @apply text-gray-4a;
+      @apply text-12;
+
+      @apply p-32;
+      @apply pt-16;
+
+      > p {
+        @apply leading-2;
+      }
+    }
+  }
+}
+</style>
