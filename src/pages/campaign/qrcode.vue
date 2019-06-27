32ed2cbb6ad4d674a050def84d4561fbcf74c49b
William Chong <williamchong007@msn.com>
2019-06-27 18:42:18 +0800

    ✨ Add generic qrcode redirect page

---
+++ src/pages/campaign/qrcode.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  fetch({ redirect, params, query }) {
+    /* page/query to be changed according to campaign needs */
+    redirect(302, {
+      name: 'civic',
+      params,
+      query: { from: 'candycandy772', ...query },
+    });
+  },
+};
+</script>
