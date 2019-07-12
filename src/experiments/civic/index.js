84448666e9cccf3b9d59fe77ca668fae9a230644
William Chong <williamchong007@msn.com>
2019-07-12 15:41:53 +0800

    🐛 Fix did not redirect to civic register after direct login

--- src/experiments/civic/index.js
+++ src/experiments/civic/index.js
@@ -1,6 +1,6 @@
 export default {
   name: 'direct-signin',
-  experimentID: 'zwkC3oVJRNKo6ZLt2Ol5Lw',
+  experimentID: '3bQXKlMMTlGIYJxdSysbwQ',
   isEligible: ({ route }) => !process.server && route.name === 'civic',
   variants: [{ name: 'indirect', weight: 5 }, { name: 'direct', weight: 5 }],
 };
