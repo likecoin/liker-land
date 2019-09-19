79e09d302175b2f79d96dadd74a6af29a51409cd
William Chong <williamchong007@msn.com>
2019-09-19 18:36:46 +0800

    🐛 Guard civic liker in payment page

--- src/pages/civic/register/paypal.vue
+++ src/pages/civic/register/paypal.vue
@@ -4,6 +4,13 @@ import { getPaypalPaymentPageURL } from '@/util/api';
 export default {
   middleware: 'authenticated',
   asyncData({ store, redirect, query }) {
+    if (
+      store.getters.getUserIsCivicLiker &&
+      !store.getters.getUserInfo.isCivicLikerRenewalPeriod
+    ) {
+      redirect({ name: 'settings-civic' });
+      return;
+    }
     const likerId = store.getters.getUserId;
     const { from } = query;
     const customPayload = from
