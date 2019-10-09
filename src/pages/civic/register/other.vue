080f7b43bbb797bc10770c6cc0a3e96a1d31fa75
William Chong <williamchong007@msn.com>
2019-10-09 14:49:21 +0800

    🚧  Add payme instrcutions

--- src/pages/civic/register/other.vue
+++ src/pages/civic/register/other.vue
@@ -1,18 +1,47 @@
+<template>
+  <div class="bg-white">
+    <main class="page-content flex justify-center items-center p-16">
+      <div>
+        <h1>{{ $t('OtherPaymentInfo.title') }}</h1>
+        <h3>{{ $t('OtherPaymentInfo.heading') }}</h3>
+      </div>
+      <p>{{ $t('OtherPaymentInfo.description') }}</p>
+      <h2>{{ $t('OtherPaymentInfo.stepOne') }}</h2>
+      <ul>
+        <li>{{ $t('OtherPaymentInfo.FPSID') }}</li>
+      </ul>
+      <ul>
+        <li>{{ $t('OtherPaymentInfo.PayMeQR') }}</li>
+      </ul>
+      <img :src="PayMeImage">
+      <h2>{{ $t('OtherPaymentInfo.stepTwo') }}</h2>
+      <i18n path="OtherPaymentInfo.EmailInstruction" tag="p" for="tos">
+        <a href="mailto:team@like.co" rel="nofollow noopener noreferrer" target="_blank">team@like.co</a>
+      </i18n>
+      <p>{{ $t('OtherPaymentInfo.Ending') }}</p>
+    </main>
+  </div>
+</template>
+
 <script>
-const PAYMENT_LINK =
-  'https://help.like.co/en/articles/3381375-%E5%A6%82%E4%BD%95%E4%BB%A5-payme-%E6%88%96-%E8%BD%89%E6%95%B8%E5%BF%AB%E7%B9%B3%E4%BB%98-%E8%AE%9A%E8%B3%9E%E5%85%AC%E6%B0%91-%E5%B9%B4%E8%B2%BB';
+import IntercomMixin from '~/mixins/intercom';
+import PayMeImage from '~/assets/images/civic/payme.jpg';
 
 export default {
   middleware: 'authenticated',
+  mixins: [IntercomMixin],
+  data() {
+    return {
+      PayMeImage,
+    };
+  },
   asyncData({ store, redirect, query }) {
     if (
       store.getters.getUserIsCivicLiker &&
       !store.getters.getUserInfo.isCivicLikerRenewalPeriod
     ) {
       redirect({ name: 'settings-civic' });
-      return;
     }
-    redirect(PAYMENT_LINK);
   },
 };
 </script>
