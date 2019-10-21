772785f113934bf40df6a782bd0b14e81ed64aa7
William Chong <6198816+williamchong007@users.noreply.github.com>
2019-10-22 01:56:50 +0800

    🐛 Fix isHK always true, add cache server name dectection

--- src/plugins/geoip.server.js
+++ src/plugins/geoip.server.js
@@ -3,7 +3,12 @@ export default ({ app, store, req, res, query }) => {
   // https://cloud.google.com/appengine/docs/standard/go/reference/request-response-headers#app_engine-specific_headers
   const ipCountry = req.headers['x-appengine-country'];
   const ipCity = req.headers['x-appengine-city'];
-  if (ipCountry === 'HK' || ipCity === 'hong kong') {
-    store.dispatch('setIsHK', true);
+  const cacheServerName = req.headers['x-forwarded-server'] || '';
+  if ((ipCountry && ipCountry !== 'ZZ') || ipCity || cacheServerName) {
+    const isHK =
+      ipCountry === 'HK' ||
+      ipCity === 'hong kong' ||
+      cacheServerName.includes('HKG');
+    store.dispatch('setIsHK', isHK);
   }
 };
