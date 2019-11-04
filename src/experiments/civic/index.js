ac9f153d1baed61cdd4d46695028761d3af29909
William Chong <williamchong007@msn.com>
2019-11-04 16:09:55 +0800

    ⚗️ Change exp goal and update exp id for restart

--- src/experiments/civic/index.js
+++ src/experiments/civic/index.js
@@ -1,6 +1,6 @@
 export default {
   name: 'civic-page',
-  experimentID: '7_gHqKdrRu6QC3YsBvjJdw',
+  experimentID: 'z3qJJyoBT3uLcJFk42QESg',
   isEligible: ({ route }) => !process.server && route.name === 'civic',
   variants: [{ name: 'origin', weight: 5 }, { name: 'monthly', weight: 5 }],
 };
