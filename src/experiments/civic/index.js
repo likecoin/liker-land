e9f576d2e49c98a594dd6e0133c0b6df0d6b652e
Ng Wing Tat, David <i@ngwingt.at>
2020-01-29 10:19:22 +0800

    🐛 Fix exp eligibility

--- src/experiments/civic/index.js
+++ src/experiments/civic/index.js
@@ -1,7 +1,6 @@
 export default {
   name: 'civic-register-page',
   experimentID: 'QBxvQ7G9RpSRypN7noIvOw',
-  isEligible: ({ route }) =>
-    !process.server && /^civic-register.*/.test(route.name),
+  isEligible: ({ route }) => !process.server && /^civic/.test(route.name),
   variants: [{ name: 'origin', weight: 5 }, { name: 'variant', weight: 5 }],
 };
