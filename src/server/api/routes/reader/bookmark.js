8088f3504911f3b7b43ae50e9d2581ddb0c47139
William Chong <williamchong007@msn.com>
2020-06-16 22:34:36 +0800

    ✨ Integrate bookmark api

--- src/server/api/routes/reader/bookmark.js
+++ src/server/api/routes/reader/bookmark.js
@@ -2,7 +2,11 @@ const { Router } = require('express');
 const parse = require('url-parse');
 const { FieldValue, userCollection } = require('../../util/firebase');
 const { setPrivateCacheHeader } = require('../../middleware/cache');
-const { apiPostArticleForInfo } = require('../../util/api');
+const {
+  apiFetchBookmarks,
+  apiPostBookmarks,
+  apiDeleteBookmarks,
+} = require('../../util/api');
 
 const router = Router();
 
@@ -13,8 +17,15 @@ router.get('/reader/bookmark', async (req, res, next) => {
       res.sendStatus(403);
       return;
     }
-    const userDoc = await userCollection.doc(req.session.user).get();
+    const [userDoc, apiBookmarks] = await [
+      userCollection.doc(req.session.user).get(),
+      apiFetchBookmarks(),
+    ];
     const { bookmarks = [] } = userDoc.data();
+    const bookmarksSet = new Set(bookmarks);
+    if (apiBookmarks.data.list) {
+      apiBookmarks.data.list.forEach(b => bookmarksSet.add(b.url));
+    }
     res.json({ bookmarks });
   } catch (err) {
     next(err);
@@ -38,11 +49,13 @@ router.post('/reader/bookmark', async (req, res, next) => {
       res.sendStatus(400);
       return;
     }
-    await apiPostArticleForInfo(url, req);
-    const userRef = userCollection.doc(req.session.user);
-    await userRef.update({
-      bookmarks: FieldValue.arrayUnion(url),
-    });
+    try {
+      await apiPostBookmarks(url, req);
+    } catch (err) {
+      if (!(err.response && err.response.data === 'URL_NOT_FOUND')) {
+        throw err;
+      }
+    }
     res.sendStatus(200);
   } catch (err) {
     next(err);
@@ -70,6 +83,13 @@ router.delete('/reader/bookmark', async (req, res, next) => {
     await userRef.update({
       bookmarks: FieldValue.arrayRemove(url),
     });
+    try {
+      await apiDeleteBookmarks(url, req);
+    } catch (err) {
+      if (!(err.response && err.response.data === 'URL_NOT_FOUND')) {
+        throw err;
+      }
+    }
     res.sendStatus(200);
   } catch (err) {
     next(err);
