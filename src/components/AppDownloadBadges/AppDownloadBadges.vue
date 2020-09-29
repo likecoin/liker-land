16bd9fbf77e9ae69237c4ca34e65761a128f2d73
Ng Wing Tat, David <i@ngwingt.at>
2020-09-29 11:17:02 +0800

    ✨ Add get-app page

---
+++ src/components/AppDownloadBadges/AppDownloadBadges.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="app-download-badges">
+    <a
+      class="phone:hidden"
+      :href="url"
+      target="_blank"
+    ><Apple /></a>
+    <a
+      class="phone:hidden"
+      :href="url"
+      target="_blank"
+    ><Google /></a>
+    <a
+      class="button rounded-8 text-like-green text-14 text-center font-400 m-8 p-12 phone:inline tablet:hidden laptop:hidden"
+      :href="url"
+      target="_blank"
+    >{{ $t(`AppDownloadBadges.${type === 'start' ? 'Start' : 'Download'}`) }}</a>
+    <a
+      v-if="type === 'start'"
+      class="text-12 text-like-green hover:underline phone:inline tablet:hidden laptop:hidden"
+      :href="url"
+      target="_blank"
+    >{{ $t('AppDownloadBadges.Download') }}</a>
+  </div>
+</template>
+
+<script>
+import Apple from './apple.svg';
+import Google from './google.svg';
+
+export default {
+  name: 'AppDownloadBadges',
+  components: {
+    Apple,
+    Google,
+  },
+  props: {
+    type: {
+      type: String,
+      default: 'default',
+    },
+    from: {
+      type: String,
+      default: '',
+    },
+  },
+  computed: {
+    url() {
+      let url = 'https://likerland.app.link/';
+      if (this.from) {
+        url = `${url}?event=app_referral&referrer=${this.from}`;
+      }
+      return url;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.app-download-badges {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  @media screen and (max-width: 527px) {
+    flex-direction: column;
+  }
+
+  a {
+    margin: 5px;
+  }
+
+  .button {
+    min-width: 156px;
+    box-shadow: 2px 4px 6px rgba(0, 0, 0, 0.3);
+
+    transition-property: background-color, box-shadow, transform;
+    transition-duration: 0.2s;
+    transition-timing-function: ease-in;
+
+    @apply bg-like-cyan-light;
+
+    &:hover {
+      background-color: config('colors.like-cyan');
+    }
+
+    &:active {
+      box-shadow: 1px 2px 6px rgba(0, 0, 0, 0.3);
+      transform: translateX(1px) translateY(2px);
+
+      background-color: config('colors.like-cyan-dark');
+    }
+  }
+}
+</style>
---
+++ src/pages/getapp.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  .getapp-page
+    PageHeader
+      template
+        SiteNavBar.text-like-green
+
+    main.page-content
+      GetAppPage(:from="$route.query.from")
+</template>
+
+<script>
+import GetAppPage from '~/components/GetAppPage/GetAppPage';
+import PageHeader from '~/components/PageHeader';
+import SiteNavBar from '~/components/SiteNavBar';
+
+import { CrispMixinFactory } from '~/mixins/crisp';
+
+export default {
+  components: {
+    GetAppPage,
+    PageHeader,
+    SiteNavBar,
+  },
+  mixins: [CrispMixinFactory()],
+};
+</script>
+
+<style lang="scss">
+.getapp-page {
+  background: linear-gradient(20deg, #d2f0f0, #f0e6b4);
+}
+</style>
