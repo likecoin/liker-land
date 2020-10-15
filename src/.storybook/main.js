6d0a081c857383baed8b8c097f08402be9cc3a62
Ng Wing Tat, David <i@ngwingt.at>
2020-10-15 21:48:34 +0800

    ➕ Init Storybook

---
+++ src/.storybook/main.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  "stories": [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ],
+  "webpackFinal": (config) => {
+    /**
+     * core-js compatibility issue
+     * https://github.com/storybookjs/storybook/issues/11255
+     */
+    config.resolve.alias['core-js/modules'] = path.resolve(
+      __dirname,
+      '..',
+      'node_modules/@storybook/core/node_modules/core-js/modules'
+    );
+    return config;
+  }
+}
