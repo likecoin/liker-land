e998a2db5bd96b7996e532d989408358de420d84
William Chong <me@williamchong.cloud>
2020-11-05 21:02:05 +0800

    🔧 Add new function config `constant.api_url`

--- functions/apiHttp/index.js
+++ functions/apiHttp/index.js
@@ -10,8 +10,8 @@ app.use(helmet());
 app.set('trust proxy', 1);
 app.use('/api', api);
 
-if ((functions.config().constant || {}).external_url) {
-  process.env.API_URL = functions.config().constant.external_url;
+if ((functions.config().constant || {}).api_url) {
+  process.env.API_URL = functions.config().constant.api_url;
 }
 
 const internalHttp = functions.https.onRequest(app);
