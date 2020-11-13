686c2a2eaec0807f3fcc7879b3de4758aa4da8c2
Ng Wing Tat, David <i@ngwingt.at>
2020-11-13 17:18:14 +0800

    🔧 Integrate Storybook with TailwindCSS

--- src/.storybook/preview.js
+++ src/.storybook/preview.js
@@ -1,6 +1,7 @@
 // HACK: Consistent import to match require at client side for Nuxt.js
 import '../plugins/vue-stack-grid.client';
 import '../plugins/ui-plugin.client';
+import '../assets/css/index.scss';
 
 export const parameters = {
   layout: 'centered',
