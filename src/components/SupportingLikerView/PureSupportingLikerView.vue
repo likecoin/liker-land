8e505e99ba03c620232b9e16d31a2868766fabb3
Ng Wing Tat, David <i@ngwingt.at>
2020-11-23 10:12:13 +0800

    ✨ Add pure supporting liker view

---
+++ src/components/SupportingLikerView/PureSupportingLikerView.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="supporting-liker-view bg-white rounded-8 text-center">
+    <div
+      class="flex flex-col justify-center items-center p-24 cursor-pointer"
+      @click="$emit('click-identity')"
+    >
+      <Identity
+        :avatar-url="avatarUrl"
+        :avatar-size="64"
+        :is-avatar-outlined="isCivicLiker"
+      />
+      <span class="mt-12 text-like-green text-20 font-500 underline">{{ displayName }}</span>
+    </div>
+    <div
+      v-if="$slots.footer"
+      class="border-gray-d8 border-t-1 px-20 py-16"
+    >
+      <slot name="footer" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Identity from '../Identity/Identity';
+
+export default {
+  name: 'PureSupportingLikerView',
+  components: {
+    Identity,
+  },
+  props: {
+    avatarUrl: {
+      type: String,
+      default: '',
+    },
+    displayName: {
+      type: String,
+      default: '',
+    },
+    isCivicLiker: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
