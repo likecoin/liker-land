a94414a9eb6068d270ca457a9be4a4c50dfda144
Ng Wing Tat, David <i@ngwingt.at>
2020-12-03 21:16:36 +0800

    🗑 Deprecate welcome dialog

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,22 +1,14 @@
 <template>
   <div class="page-layout page-layout--default">
-    <DialogContainer />
-
     <nuxt class="page-wrapper sliding-menu-pushee" />
-
     <SlidingMenu ref="slidingMenu" />
-
   </div>
 </template>
 
 <script>
-import DialogContainer from '~/components/DialogContainer';
 import slidingMenuMixin from '~/mixins/sliding-menu';
 
 export default {
-  components: {
-    DialogContainer,
-  },
   mixins: [slidingMenuMixin],
 };
 </script>
