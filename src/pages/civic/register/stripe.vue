d7055efbf8b31cd494377cb5db6fe89da45bb541
Ng Wing Tat, David <i@ngwingt.at>
2020-12-10 00:08:50 +0800

    💄 Unify loading indicator for payment flow

--- src/pages/civic/register/stripe.vue
+++ src/pages/civic/register/stripe.vue
@@ -1,8 +1,6 @@
 <template>
-  <div>
-    <LcLoadingIndicator v-if="!isFetched" class="mx-auto" />
-    <!-- Used to display form errors. -->
-    <div v-if="error">{{ error }}</div>
+  <div class="payment-page">
+    <Spinner class="mx-auto my-96" />
   </div>
 </template>
 
@@ -11,9 +9,13 @@ import { mapGetters } from 'vuex';
 import { getStripePaymentStatusAPI, getStripePaymentAPI } from '~/util/api';
 import { logTrackerEvent } from '~/util/EventLogger';
 
+import Spinner from '~/components/Spinner/Spinner';
+
 export default {
   middleware: 'authenticated',
-  layout: 'dialog',
+  components: {
+    Spinner,
+  },
   data() {
     return {
       isFetched: false,
