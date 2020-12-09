63fa7df262a3f050728692dfc2846ebafd140bb2
Ng Wing Tat, David <i@ngwingt.at>
2020-12-10 00:08:28 +0800

    🐛 Fix CL state not update after success

--- src/pages/civic/payment/stripe/success.vue
+++ src/pages/civic/payment/stripe/success.vue
@@ -1,107 +1,24 @@
 <template>
   <div class="payment-success-page">
-    <div>
-      <transition
-        name="payment-success-page-hero-"
-        appear
-      >
-        <div class="payment-success-page-hero">
-          <RadialBlastGraph class="payment-success-page-hero__bg" />
-
-          <div class="payment-success-page-hero__avatar">
-            <lc-avatar
-              :src="getUserInfo.avatar"
-              size="144"
-              halo="civic-liker"
-            />
-          </div>
-        </div>
-      </transition>
-
-      <div class="payment-success-page-receipt">
-        <div class="payment-success-page-receipt__header bg-like-gradient">
-          <TickIcon
-            class="w-48 h-48 text-like-cyan fill-current"
-          />
-          <div class="text-like-green">
-            {{ $t('PaymentSuccessPage.heading') }}
-          </div>
-        </div>
-        <div class="payment-success-page-receipt__body">
-          <p>{{ $t('PaymentSuccessPage.body') }}</p>
-
-          <div class="mt-24 phone:mx-32 laptop:mx-64 desktop:mx-64">
-            <a
-              v-if="isPopup"
-              href="#"
-              class="btn btn--outlined btn--block max-w-3 laptop:max-w-2/3 desktop:max-w-2/3 mx-auto my-0 "
-              @click.prevent="onClickClose"
-            > {{ getCloseButtonText }}</a>
-            <NuxtLink
-              v-else
-              class="btn btn--outlined btn--block max-w-3 laptop:max-w-2/3 desktop:max-w-2/3 mx-auto my-0 "
-              :to="getHomeRoute"
-            > {{ $t('PaymentSuccessPage.continue') }}</NuxtLink>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Spinner class="mx-auto my-96" />
   </div>
 </template>
 <script>
-import { mapActions, mapGetters } from 'vuex';
 import { logTrackerEvent } from '~/util/EventLogger';
 
-import RadialBlastGraph from '~/assets/images/civic/radial-blast.svg';
-import TickIcon from '~/assets/icons/tick.svg';
-
-const parse = require('url-parse');
+import Spinner from '~/components/Spinner/Spinner';
 
 export default {
   components: {
-    RadialBlastGraph,
-    TickIcon,
-  },
-  data() {
-    return {
-      isPopup: '',
-    };
+    Spinner,
   },
   middleware: 'authenticated',
-  computed: {
-    ...mapGetters(['getUserInfo', 'getHomeRoute']),
-    getCloseButtonText() {
-      if (this.isPopup && this.isPopup !== '1') {
-        try {
-          const { hostname } = parse(this.isPopup);
-          if (hostname) {
-            return this.$t('PaymentSuccessPage.backToPreviousSite', {
-              hostname,
-            });
-          }
-        } catch (err) {
-          console.error(err); // eslint-disable-line no-console
-        }
-      }
-      return this.$t('PaymentSuccessPage.backToPrevious');
-    },
-  },
-  asyncData({ query, redirect }) {
-    if (query.from) {
-      redirect({
-        name: 'id',
-        params: { id: query.from },
-        query: { civic_welcome: 1 },
-      });
-    }
-  },
   head() {
     return {
       title: this.$t('PaymentSuccessPage.title'),
     };
   },
   mounted() {
-    let isPopup;
     logTrackerEvent(
       this,
       'Civic',
@@ -109,12 +26,7 @@ export default {
       'CivicPaymentSuccess(stripe)',
       1
     );
-    if (window.sessionStorage) {
-      isPopup = window.sessionStorage.getItem('civicLikerIsPopup');
-      if (isPopup) this.isPopup = isPopup;
-    }
     try {
-      this.setUserCivicLiker();
       logTrackerEvent(
         this,
         'Civic',
@@ -122,106 +34,16 @@ export default {
         'CivicRegisterComplete(stripe)',
         1
       );
-      if (!this.isPopup) {
-        setTimeout(() => {
-          this.$router.push({
-            name: 'index',
-          });
-        }, 3000);
+      const { from } = this.$route.query;
+      if (this.$route.query.from) {
+        window.location.href = `/${from}?civic_welcome=1`;
+      } else {
+        window.location.href = `/settings/civic`;
       }
     } catch (err) {
       console.error(err); // eslint-disable-line no-console
       throw err;
     }
   },
-  methods: {
-    ...mapActions(['setUserCivicLiker']),
-    onClickClose() {
-      if (window.sessionStorage) {
-        window.sessionStorage.removeItem('civicLikerIsPopUp');
-      }
-      window.close();
-      /* HACK: Don't stuck user if close didn't work */
-      setTimeout(() => {
-        this.$router.push({
-          name: 'index',
-        });
-      }, 3000);
-    },
-  },
 };
 </script>
-
-<style lang="scss">
-.payment-success-page {
-  @apply bg-gray-f7;
-
-  @apply items-center;
-
-  > div {
-    @apply w-full;
-    @apply max-w-phone;
-  }
-
-  &-hero {
-    padding-top: 60%;
-
-    @apply relative;
-    @apply overflow-hidden;
-
-    @apply max-w-phone;
-    @apply w-full;
-
-    &__bg {
-      top: 50%;
-      left: 50%;
-      transform: translateX(-50%) translateY(-50%);
-
-      @apply absolute;
-    }
-
-    &__avatar {
-      @apply absolute;
-      @apply pin;
-
-      @apply flex;
-      @apply justify-center;
-      @apply items-center;
-    }
-
-    &__header {
-      @apply p-8;
-    }
-  }
-
-  &-receipt {
-    @apply relative;
-    @apply z-10;
-    @apply overflow-hidden;
-    @apply rounded;
-
-    @apply bg-white;
-
-    @apply mx-24;
-
-    &__header {
-      @apply text-16;
-      @apply text-center;
-
-      @apply pt-16 px-24 pb-8;
-    }
-
-    &__body {
-      @apply text-gray-4a;
-      @apply text-12;
-
-      @apply p-32;
-      @apply pt-16;
-
-      > p {
-        @apply leading-2;
-      }
-    }
-  }
-}
-</style>
