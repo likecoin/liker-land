eeadebc888b497e0306c0dd452d969d2f7a033cc
Ng Wing Tat, David <i@ngwingt.at>
2020-12-18 14:49:15 +0800

    🥅 Add spinner in legacy civic page for redirect

--- src/pages/civic/_from.vue
+++ src/pages/civic/_from.vue
@@ -1,7 +1,17 @@
+<template lang="pug">
+  div
+    Spinner.mx-auto.my-96
+</template>
+
 <script>
 import { DEFAULT_CL_SUPPORTER } from '~/constant';
 
+import Spinner from '~/components/Spinner/Spinner';
+
 export default {
+  components: {
+    Spinner,
+  },
   fetch({ redirect, query: { from: qId, ...query }, params: { from: pId } }) {
     const id = pId || qId || DEFAULT_CL_SUPPORTER;
     redirect({ name: 'id-civic', params: { id }, query });
