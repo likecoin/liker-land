e23ef1decbed1259bec546b07de92f9f2927de04
Ng Wing Tat, David <i@ngwingt.at>
2020-12-22 14:18:04 +0800

    🔍 Add OG meta for CL2.0 flow

--- src/pages/_id/civic/index.vue
+++ src/pages/_id/civic/index.vue
@@ -12,6 +12,9 @@
 </template>
 
 <script>
+import { getUserMinAPI } from '~/util/api';
+import { checkUserNameValid } from '~/util/user';
+
 import CivicSubscriptionView from '~/components/CivicSubscriptionView/CivicSubscriptionView';
 import PageHeader from '~/components/PageHeader';
 import SiteNavBar from '~/components/SiteNavBar';
@@ -22,11 +25,58 @@ export default {
     PageHeader,
     SiteNavBar,
   },
-  fetch({ store, redirect, route, query }) {
+  async asyncData({ store, route, redirect, query, $api, error }) {
     const currentUserLikerID = store.getters.getUserId;
-    if (route.params.id === currentUserLikerID) {
-      redirect({ name: 'id', params: { id: currentUserLikerID }, query });
+    const { id } = route.params;
+
+    if (id === currentUserLikerID) {
+      redirect({ name: 'id', params: { id }, query });
+      return undefined;
     }
+
+    if (id && checkUserNameValid(id)) {
+      try {
+        const creator = await $api.$get(getUserMinAPI(id));
+        return { creator };
+      } catch (err) {
+        const msg = (err.response && err.response.data) || err;
+        // eslint-disable-next-line no-console
+        console.error(msg);
+      }
+    }
+    error({ statusCode: 404, message: 'LIKER_NOT_FOUND' });
+    return undefined;
+  },
+  head() {
+    const name = this.creator.displayName.trim();
+    const title = this.$t('CivicEntryPage.Og.Title', { name });
+    const description = this.$t('CivicEntryPage.Og.Description', { name });
+    return {
+      title,
+      meta: [
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: title,
+        },
+        {
+          hid: 'description',
+          name: 'description',
+          content: description,
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: description,
+        },
+        {
+          hid: 'og:image',
+          property: 'og:image',
+          content: 'https://liker.land/images/og/civic-v2.png',
+        },
+      ],
+      link: [{ rel: 'canonical', href: `${this.$route.path}` }],
+    };
   },
 };
 </script>
--- src/pages/creators/index.vue
+++ src/pages/creators/index.vue
@@ -18,5 +18,33 @@ export default {
     PageHeader,
     SiteNavBar,
   },
+  head() {
+    return {
+      title: this.$t('CreatorsPageV2.Hero.Title'),
+      meta: [
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: this.$t('CreatorsPageV2.Hero.Title'),
+        },
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.$t('CreatorsPageV2.Hero.Description'),
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: this.$t('CreatorsPageV2.Hero.Description'),
+        },
+        {
+          hid: 'og:image',
+          property: 'og:image',
+          content: 'https://liker.land/images/og/civic-v2.png',
+        },
+      ],
+      link: [{ rel: 'canonical', href: `${this.$route.path}` }],
+    };
+  },
 };
 </script>
