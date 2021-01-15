cc76830d3f1d110acbf85f54df3861a9de02302a
Ng Wing Tat, David <i@ngwingt.at>
2021-01-15 19:33:37 +0800

    ✨ Add my supporters related state & actions

--- src/store/modules/getters/support.js
+++ src/store/modules/getters/support.js
@@ -3,3 +3,8 @@ export const getCivicSupportingUserInfo = state => id =>
   state.supportingUsers[id];
 export const getCivicIsSupportingUser = state => id =>
   state.supportingUsers[id] && state.supportingUsers[id].quantity > 0;
+
+export const getMySupporters = state => state.supporters;
+export const getMySupportersCount = state => state.supporters.length;
+export const getMySupportersTotalQuantity = state =>
+  state.supporters.reduce((a, s) => a + s.quantity, 0);
--- src/store/modules/support.js
+++ src/store/modules/support.js
@@ -6,12 +6,14 @@ import {
   SUPPORT_SET_SUPPORTING_USERS,
   SUPPORT_SET_SUPPORTING_USER_INFO,
   SUPPORT_REMOVE_SUPPORTING_USER,
+  SUPPORT_SET_SUPPORTERS,
 } from '../mutation-types';
 import * as getters from './getters/support';
 import * as actions from './actions/support';
 
 const state = () => ({
   supportingUsers: {},
+  supporters: [],
 });
 
 const mutations = {
@@ -23,6 +25,9 @@ const mutations = {
   },
   [SUPPORT_REMOVE_SUPPORTING_USER](state, id) {
     Vue.delete(state.supportingUsers, id);
+  },
+  [SUPPORT_SET_SUPPORTERS](state, supporters) {
+    state.supporters = supporters;
   },
 };
 
