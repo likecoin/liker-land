89f4a24229fec81e73dda81b5409fbc3c0426da1
William Chong <me@williamchong.cloud>
2021-01-27 02:42:31 +0800

    🐛 Fix experiment plugin code for gtag

--- src/plugins/experiment.client.js
+++ src/plugins/experiment.client.js
@@ -1,12 +1,16 @@
 /*
-  HACK: vue-analytics might not have inited window.ga early enough for google-optimize
-  set exp value explicitly in vue.$exp here
+  HACK: force set google-optimize params for gtag config
 */
+const { GA_TRACKING_ID } = process.env;
+
 export default ({ app }) => {
   if (app.$exp && app.$gtag) {
     const { experimentID, $variantIndexes } = app.$exp;
     if (!experimentID || !$variantIndexes || !$variantIndexes.length) return;
-    const expCookie = `${experimentID}.${$variantIndexes.join('-')}`;
-    if (app.$gtag) app.$gtag.set('exp', expCookie);
+    if (app.$gtag) {
+      app.$gtag.config(GA_TRACKING_ID, {
+        experiments: [{ id: experimentID, variant: $variantIndexes.join('-') }],
+      });
+    }
   }
 };
