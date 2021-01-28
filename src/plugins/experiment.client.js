069801ef39d3d9cbbb367d00639e71d368345dd9
William Chong <6198816+williamchong@users.noreply.github.com>
2021-01-29 00:27:59 +0800

    🐛 Trim gtag experiment config code (#383)

--- src/plugins/experiment.client.js
+++ src/plugins/experiment.client.js
@@ -1,14 +1,12 @@
 /*
   HACK: force set google-optimize params for gtag config
 */
-const { GA_TRACKING_ID } = process.env;
-
 export default ({ app }) => {
   if (app.$exp && app.$gtag) {
     const { experimentID, $variantIndexes } = app.$exp;
     if (!experimentID || !$variantIndexes || !$variantIndexes.length) return;
     if (app.$gtag) {
-      app.$gtag.config(GA_TRACKING_ID, {
+      app.$gtag.config({
         experiments: [{ id: experimentID, variant: $variantIndexes.join('-') }],
       });
     }
