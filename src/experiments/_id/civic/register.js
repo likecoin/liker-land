37aa309564a03f0ead32cae908bdfbcad4886f21
Ng Wing Tat, David <i@ngwingt.at>
2021-03-03 23:26:51 +0800

    ⚗️ Fix exp not trigger by route change in client side

--- src/experiments/_id/civic/register.js
+++ src/experiments/_id/civic/register.js
@@ -1,7 +1,6 @@
 export default {
   name: 'error-page',
   experimentID: 'C87ExNSlToertq9pakD1nw',
-  isEligible: ({ route }) =>
-    !process.server && route.name === 'id-civic-register',
+  isEligible: ({ route }) => !process.server && /^id-civic/.test(route.name),
   variants: [{ name: 'origin', weight: 5 }, { name: 'variant', weight: 5 }],
 };
