89e9193a83473008a664d19aba2ece15c2f368d9
Ng Wing Tat, David <i@ngwingt.at>
2021-03-03 23:07:51 +0800

    🚑 Fix incorrect exp ID

--- src/experiments/_id/civic/register.js
+++ src/experiments/_id/civic/register.js
@@ -1,6 +1,6 @@
 export default {
   name: 'error-page',
-  experimentID: '3LhbX2MeRN-PFF1ZspPKSg',
+  experimentID: 'C87ExNSlToertq9pakD1nw',
   isEligible: ({ route }) =>
     !process.server && route.name === 'id-civic-register',
   variants: [{ name: 'origin', weight: 5 }, { name: 'variant', weight: 5 }],
