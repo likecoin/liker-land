57c8784c02c880150c75ae10f7c541cb4aa2c6b7
Ng Wing Tat, David <i@ngwingt.at>
2021-03-15 20:32:00 +0800

    🚑 Fix isEligible was not called in computed prop

--- src/mixins/experiment.js
+++ src/mixins/experiment.js
@@ -9,6 +9,12 @@ export default function experimentsMixin(
       [propName]() {
         if (!this.$exp || !isEligible(this)) return false;
         const { name, $activeVariants } = this.$exp;
+        if (
+          this.$exp.isEligible &&
+          !this.$exp.isEligible({ route: this.$route })
+        ) {
+          return false;
+        }
         return (
           name === experiemntName &&
           !!$activeVariants.find(variant => variant.name === variantName)
