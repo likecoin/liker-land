b2ca091d3ccf3520022b655f9d2eb65661967ae3
Ng Wing Tat, David <i@ngwingt.at>
2021-04-13 10:05:23 +0800

    🚸 Show placeholder when loading suggested creators

--- src/components/CivicLikerPage/sections/SuggestedCreatorsSection.vue
+++ src/components/CivicLikerPage/sections/SuggestedCreatorsSection.vue
@@ -10,6 +10,7 @@
         preset="creator"
         :contents="contents"
         :is-waterfall-layout="false"
+        :is-loading="contents.length === 0"
       )
 
       Button.block.mt-16.mx-auto.p-0.max-w-phone-min.border-2.border-like-cyan.border-solid.font-600.rounded-14(
