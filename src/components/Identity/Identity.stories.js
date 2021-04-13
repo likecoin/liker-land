69f6b40b40971d904abc271d90b9024271a11504
Ng Wing Tat, David <i@ngwingt.at>
2021-04-13 09:47:27 +0800

    ✨ Allow controlling avatar outline extrusion

--- src/components/Identity/Identity.stories.js
+++ src/components/Identity/Identity.stories.js
@@ -1,14 +1,17 @@
 import Identity from './Identity';
 
+const defaultProps = {
+  avatarSize: 40,
+  avatarUrl: 'https://www.gravatar.com/avatar/?d=mp',
+  isAvatarOutlined: true,
+  isAvatarOutlineExtruded: true,
+  isAvatarDisabled: false,
+  displayName: 'Display Name',
+};
+
 export default {
   title: 'Identity',
-  args: {
-    avatarSize: 40,
-    avatarUrl: 'https://www.gravatar.com/avatar/?d=mp',
-    isAvatarOutlined: true,
-    isAvatarDisabled: false,
-    displayName: 'Display Name',
-  },
+  args: defaultProps,
 };
 
 export const Default = (_, { argTypes }) => ({
@@ -33,4 +36,40 @@ export const AvatarOnly = (_, { argTypes }) => ({
 
 AvatarOnly.args = {
   displayName: '',
+};
+
+export const Extruded = (_, { argTypes }) => ({
+  components: {
+    Identity,
+  },
+  props: Object.keys(argTypes),
+  computed: {
+    defaultProps() {
+      return defaultProps;
+    },
+  },
+  template: `
+    <table cellpadding="10">
+      <tr style="text-align:center">
+        <td>Default</td>
+        <td>Control</td>
+      </tr>
+      <tr>
+        <td>
+          <div style="margin:10;border: 1px dashed #e6e6e6">
+            <Identity v-bind="defaultProps" />
+          </div>
+        </td>
+        <td>
+          <div style="margin:10;border: 1px dashed #e6e6e6">
+            <Identity v-bind="$props" />
+          </div>
+        </td>
+      </tr>
+    </table>
+  `,
+});
+
+Extruded.args = {
+  isAvatarOutlineExtruded: false,
 };
