371fd7cce5dbbf5913d9c4560e52866719d5dd99
Ng Wing Tat, David <i@ngwingt.at>
2022-02-18 16:48:52 +0800

    💄 [CLW3] Update about button

--- src/components/CivicLikerV3/Page.vue
+++ src/components/CivicLikerV3/Page.vue
@@ -21,7 +21,14 @@
           href="https://matters.news/@likecoin/%E8%AE%9A%E8%B3%9E%E5%85%AC%E6%B0%91-web3-%E6%94%B9%E7%89%88%E8%BF%8E%E8%99%8E%E5%B9%B4-bafyreiayqigrxbmpsrtzgauwpz5muo2lgssmjlfydafqindgupp3gs7syy"
           target="_blank"
           rel="noreferrer noopener"
-        />
+        >
+          <template #prepend>
+            <AlertCircle class="w-16 ml-4" />
+          </template>
+          <template #append>
+            <ExternalLink class="w-16" />
+          </template>
+        </Button>
       </header>
       <CivicDashboardV3 class="pb-64" />
     </div>
@@ -37,6 +44,10 @@ import CivicDashboardV3 from './Dashboard';
 
 export default {
   components: {
+    AlertCircle: () =>
+      import(/* webpackChunkName: "svg-app" */ '~/assets/icons/alert-circle.svg'),
+    ExternalLink: () =>
+      import(/* webpackChunkName: "svg-app" */ '~/assets/icons/external-link.svg'),
     Button,
     CivicDashboardV3,
     PageHeader,
