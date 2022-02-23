5ac533d222f4d4101fd302cfcb05bdc6de429e83
Ng Wing Tat, David <i@ngwingt.at>
2022-02-23 14:12:15 +0800

    👽 [CLW3] Update for new API

--- src/server/api/routes/civic/staking.js
+++ src/server/api/routes/civic/staking.js
@@ -1,8 +1,21 @@
 const { Router } = require('express');
 const { setPrivateCacheHeader } = require('../../middleware/cache');
-const { apiCivicLikerGetStaking } = require('../../util/api');
+const {
+  apiCivicLikerGetStaking,
+  apiCivicLikerGetStakingInfo,
+} = require('../../util/api');
 
 const router = Router();
+
+router.get('/civic/staking/info', async (req, res, next) => {
+  try {
+    const { data } = await apiCivicLikerGetStakingInfo(req);
+    res.set('Cache-Control', 'public, max-age=86400');
+    res.json(data);
+  } catch (err) {
+    next(err);
+  }
+});
 
 router.get('/civic/staking', async (req, res, next) => {
   try {
