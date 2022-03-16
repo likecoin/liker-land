29b82c26e073018e1f4f2b11c3d5c17ef8b76c51
AuroraHuang22 <aurora90376@gmail.com>
2022-03-16 13:30:13 +0800

    ♻️ Refactor redirection

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,17 +1,7 @@
-<template lang="pug">
-  .about-page
-    PageHeader
-      template
-        SiteNavBar.text-white.bg-like-green
-
-    main.page-content
-      AboutPage
-</template>
-
 <script>
 export default {
-  mounted() {
-    window.location.href = 'https://about.like.co/';
+  fetch(context) {
+    context.redirect('https://about.like.co/');
   },
 };
 </script>
