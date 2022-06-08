b366c30dafd616ceec34318dcadc135593cb72dd
Ng Wing Tat, David <i@ngwingt.at>
2022-06-08 13:25:01 +0800

    🚚 Redirect /creators to docs.like.co

--- src/pages/creators/index.vue
+++ src/pages/creators/index.vue
@@ -1,54 +1,7 @@
-<template lang="pug">
-  PageHeader.overflow-x-hidden
-    template
-      SiteNavBar.text-like-green(class="laptop:pb-32")
-
-    main.page-content.max-w-desktop.w-full.mx-auto
-      CreatorsPage
-
-    CivicLikerWeb3Notice(:is-pop-up="true")
-</template>
-
 <script>
-import CivicLikerWeb3Notice from '~/components/CivicLikerWeb3Notice';
-import CreatorsPage from '~/components/CreatorsPage/CreatorsPage';
-import PageHeader from '~/components/PageHeader';
-import SiteNavBar from '~/components/SiteNavBar';
-
 export default {
-  components: {
-    CivicLikerWeb3Notice,
-    CreatorsPage,
-    PageHeader,
-    SiteNavBar,
-  },
-  head() {
-    return {
-      title: this.$t('CreatorsPageV2.Hero.Title'),
-      meta: [
-        {
-          hid: 'og:title',
-          property: 'og:title',
-          content: this.$t('CreatorsPageV2.Hero.Title'),
-        },
-        {
-          hid: 'description',
-          name: 'description',
-          content: this.$t('CreatorsPageV2.Hero.Description'),
-        },
-        {
-          hid: 'og:description',
-          property: 'og:description',
-          content: this.$t('CreatorsPageV2.Hero.Description'),
-        },
-        {
-          hid: 'og:image',
-          property: 'og:image',
-          content: 'https://liker.land/images/og/civic-v2.png',
-        },
-      ],
-      link: [{ rel: 'canonical', href: `${this.$route.path}` }],
-    };
+  fetch({ app, redirect }) {
+    redirect(app.i18n.t('creators_page_redirect_url'));
   },
 };
 </script>
