00fc1531eaf30dc83ceb1244e7753468c05e70bf
Ng Wing Tat, David <i@ngwingt.at>
2022-07-19 14:57:54 +0800

    🔥 Remove /following & /bookmarks

--- src/pages/index/bookmarks.vue
+++ src/pages/index/bookmarks.vue
@@ -1,73 +1,7 @@
-<template>
-  <ContentList
-    :is-loading="isLoading"
-    :items="items"
-    :header-label="$t('BookmarksPage.headerLabel')"
-  >
-    <template #empty>
-      <div class="pt-24 px-40">
-        <EmptyIcon class="block mx-auto" style="width:5.5rem" />
-        <h1>{{ $t('BookmarksPage.empty.title') }}</h1>
-        <p>{{ $t('BookmarksPage.empty.content') }}</p>
-      </div>
-
-      <div class="flex justify-center py-24">
-        <NuxtLink
-          class="btn btn--plain btn--auto-size btn--with-icon btn--icon-top flex-1 m-0 pt-32"
-          :to="{ name: 'index-following' }"
-        >
-          <FollowingIcon class="btn__icon w-24 h-24" />
-          {{ $t('BookmarksPage.empty.goToFollowing') }}
-        </NuxtLink>
-      </div>
-    </template>
-  </ContentList>
-</template>
-
 <script>
-import { mapActions, mapGetters } from 'vuex';
-
-import ContentList from '~/components/ContentList';
-
-import EmptyIcon from '~/assets/images/bookmarks-empty.svg';
-import FollowingIcon from '~/assets/icons/watching.svg';
-
 export default {
-  name: 'Bookmarks',
-  scrollToTop: true,
-  components: {
-    ContentList,
-    EmptyIcon,
-    FollowingIcon,
-  },
-  middleware: 'authenticated',
-  data() {
-    return {
-      isLoading: true,
-    };
-  },
-  computed: {
-    ...mapGetters(['getUserBookmarks']),
-    items() {
-      const items = this.getUserBookmarks.map(referrer => ({ referrer }));
-      return items.reverse();
-    },
-  },
-  mounted() {
-    this.fetchContent();
-  },
-  methods: {
-    ...mapActions(['refreshBookmarkList']),
-    async fetchContent() {
-      try {
-        this.isLoading = true;
-        await this.refreshBookmarkList();
-      } catch (err) {
-        console.error(err); // eslint-disable-line no-console
-      } finally {
-        this.isLoading = false;
-      }
-    },
+  fetch({ redirect }) {
+    redirect({ name: 'index' });
   },
 };
 </script>
