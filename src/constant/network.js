38e4c92b3dbf80ee951d3df3b303079302d3aae3
WeiJun0827 <weijunhung@outlook.com>
2022-08-01 17:49:00 +0800

    🔨 Add `wallet` store module

---
+++ src/constant/network.js
@@ -0,0 +1,28 @@
+import {
+  LIKECOIN_CHAIN_ID,
+  LIKECOIN_CHAIN_NFT_RPC,
+  LIKECOIN_CHAIN_API,
+  LIKECOIN_CHAIN_DENOM,
+  LIKECOIN_CHAIN_MIN_DENOM,
+} from '.';
+
+const { IS_TESTNET } = process.env;
+
+export const LIKECOIN_WALLET_CONNECTOR_CONFIG = {
+  chainId: LIKECOIN_CHAIN_ID,
+  chainName: IS_TESTNET ? 'LikeCoin public test chain' : 'LikeCoin chain',
+  rpcURL: LIKECOIN_CHAIN_NFT_RPC,
+  restURL: LIKECOIN_CHAIN_API,
+  coinType: 118,
+  coinDenom: LIKECOIN_CHAIN_DENOM,
+  coinMinimalDenom: LIKECOIN_CHAIN_MIN_DENOM,
+  coinDecimals: 9,
+  bech32PrefixAccAddr: 'like',
+  bech32PrefixAccPub: 'likepub',
+  bech32PrefixValAddr: 'likevaloper',
+  bech32PrefixValPub: 'likevaloperpub',
+  bech32PrefixConsAddr: 'likevalcons',
+  bech32PrefixConsPub: 'likevalconspub',
+};
+
+export default LIKECOIN_WALLET_CONNECTOR_CONFIG;
---
+++ src/store/modules/actions/wallet.js
@@ -0,0 +1,22 @@
+import { LikeCoinWalletConnector } from '@likecoin/likecoin-wallet-connector';
+
+import { LIKECOIN_WALLET_CONNECTOR_CONFIG } from '@/constant/network';
+import * as types from '@/store/mutation-types';
+
+export function connectWallet({ commit }) {
+  const connector = new LikeCoinWalletConnector({
+    ...LIKECOIN_WALLET_CONNECTOR_CONFIG,
+    onInit: ({ accounts, offlineSigner }) => {
+      commit(types.WALLET_SET_ADDRESS, accounts[0].address);
+      commit(types.WALLET_SET_SIGNER, offlineSigner);
+    },
+  });
+  commit(types.WALLET_SET_CONNECTOR, connector);
+  connector.openConnectWalletModal();
+}
+
+export function disconnectWallet({ commit }) {
+  commit(types.WALLET_SET_ADDRESS, '');
+  commit(types.WALLET_SET_SIGNER, null);
+  commit(types.WALLET_SET_CONNECTOR, null);
+}
