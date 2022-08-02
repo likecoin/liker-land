9963b057a5edcf93776f3396a4540de49336ad82
AuroraHuang22 <75730405+AuroraHuang22@users.noreply.github.com>
2022-08-02 11:40:41 +0800

    ♻️ [Details Page] Refactor sections

---
+++ src/components/NFTPage/CollectorList.vue
@@ -0,0 +1,63 @@
+<template>
+  <DropDownList
+    :class="[
+      'w-full',
+      rootClass
+    ]"
+    :title="$t('nft_details_page_title_collector')"
+  >
+    <template #titleIcon>
+      <IconPerson />
+    </template>
+    <template #content class="flex flex-col my-[12px]">
+      <div v-if="ownerCount">
+        <div v-for="id in ownerList" :key="id.displayName">
+          <div class="flex items-center justify-between">
+            <LinkV2 :to="`/${id}`">{{ id.displayName | ellipsis }}</LinkV2>
+            <Label preset="p6">{{ id.collectedCount }}</Label>
+          </div>
+          <div
+            :class="[
+              'bg-shade-gray',
+              'h-[1px]',
+              'w-full',
+              'my-[12px]',
+            ]"
+          />
+        </div>
+      </div>
+      <div v-else>
+        <div class="flex justify-center">
+          <Label preset="p6" :text="$t('nft_details_page_label_no_record')" />
+        </div>
+      </div>
+    </template>
+  </DropDownList>
+</template>
+<script>
+import { ellipsis } from '~/util/ui';
+
+export default {
+  name: 'CollectorList',
+  filters: {
+    ellipsis,
+  },
+  props: {
+    // rootClass
+    rootClass: {
+      type: String,
+      default: undefined,
+    },
+
+    // content
+    ownerCount: {
+      type: Number,
+      default: undefined,
+    },
+    ownerList: {
+      type: Array,
+      default: undefined,
+    },
+  },
+};
+</script>
