5693371cb3c22b56daa18e3ad22c45973852c783
AuroraHuang22 <aurora90376@gmail.com>
2022-08-24 10:27:06 +0800

    ♻️ Refactor attention component

--- src/components/Attention/Sign.vue
+++ src/components/Attention/Sign.vue
@@ -2,21 +2,33 @@
   <div
     :class="[
       'w-full',
+      'max-w-[700px]',
       'flex',
       'flex-col',
       'items-center',
       'justify-center',
       'px-[44px]',
-      'py-[32px]',
+      'py-[24px]',
       'text-medium-gray',
     ]"
   >
     <div class="flex items-center">
       <IconAttention class="mr-[12px]" />
-      <IconLikerLandApp :class="['inline', 'w-[32px]', 'h-[32px]']" />
+      <slot name="icon" />
     </div>
     <div class="whitespace-pre-line text-medium-gray">
-      {{ $t('attention_likerland_openApp') }}
+      {{ attentionText }}
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    attentionText: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
