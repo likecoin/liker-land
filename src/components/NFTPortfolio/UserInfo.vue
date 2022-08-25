c1a2b32a9f97edf0ee40dbad86662e2d20c9affb
WeiJun0827 <weijunhung@outlook.com>
2022-08-25 15:58:17 +0800

    🎨 Rename params based on master branch

--- src/components/NFTPortfolio/UserInfo.vue
+++ src/components/NFTPortfolio/UserInfo.vue
@@ -1,12 +1,12 @@
 <template>
   <CardV2 :class="['flex', 'flex-col', 'items-center', 'w-full']">
-    <Identity :avatar-url="avatarUrl" :avatar-size="88" :is-avatar-outlined="isCivicLiker" />
+    <Identity :avatar-url="userAvatar" :avatar-size="88" :is-avatar-outlined="isUserCivicLiker" />
     <Label preset="h3" :class="['text-like-green', 'mt-[18px]']">
-      {{ likerId | ellipsis }}
+      {{ userDisplayName | ellipsis }}
     </Label>
-    <div v-if="likerDescription" :class="['w-full', 'h-[1px]', 'bg-shade-gray', 'my-[16px]']" />
+    <div v-if="userDescription" :class="['w-full', 'h-[1px]', 'bg-shade-gray', 'my-[16px]']" />
     <Label preset="p6" class="break-all font-200">
-      {{ likerDescription }}
+      {{ userDescription }}
     </Label>
     <div :class="['w-full', 'h-[1px]', 'bg-shade-gray', 'my-[16px]']" />
     <NFTPortfolioStat 
@@ -48,22 +48,22 @@ export default {
     },
   },
   computed: {
-    avatarUrl() {
+    userAvatar() {
       return (
         this.userInfo?.avatar ||
         `https://avatars.dicebear.com/api/identicon/${this.wallet}.svg`
       );
     },
-    isCivicLiker() {
+    isUserCivicLiker() {
       return !!(
         this.userInfo?.isCivicLikerTrial ||
         this.userInfo?.isSubscribedCivicLiker
       );
     },
-    likerId() {
+    userDisplayName() {
       return this.userInfo?.displayName || this.wallet;
     },
-    likerDescription() {
+    userDescription() {
       return this.userInfo?.description;
     },
   },
