2e4f20bc86cc8bc514500db68248bc768118856a
Wei-Jun Hung <33746295+WeiJun0827@users.noreply.github.com>
2022-08-26 10:05:44 +0800

    ✨ [Portfolio] Display user stat (#561)

---
+++ src/components/NFTPortfolio/UserInfo.vue
@@ -0,0 +1,54 @@
+<template>
+  <CardV2 :class="['flex', 'flex-col', 'items-center', 'w-full']">
+    <Identity :avatar-url="userAvatar" :avatar-size="88" :is-avatar-outlined="isUserCivicLiker" />
+    <Label preset="h3" :class="['text-like-green', 'mt-[18px]']">
+      {{ userDisplayName | ellipsis }}
+    </Label>
+    <template v-if="userDescription">
+      <hr :class="['w-full', 'border-shade-gray', 'my-[16px]']">
+      <Label preset="p6" class="break-all font-200 my-[12px]">
+        {{ userDescription }}
+      </Label>
+    </template>
+    <slot />
+  </CardV2>
+</template>
+<script>
+import { ellipsis } from '~/util/ui';
+
+export default {
+  filters: {
+    ellipsis,
+  },
+  props: {
+    userInfo: {
+      type: Object,
+      default: null,
+    },
+    wallet: {
+      type: String,
+      default: null,
+    },
+  },
+  computed: {
+    userAvatar() {
+      return (
+        this.userInfo?.avatar ||
+        `https://avatars.dicebear.com/api/identicon/${this.wallet}.svg`
+      );
+    },
+    isUserCivicLiker() {
+      return !!(
+        this.userInfo?.isCivicLikerTrial ||
+        this.userInfo?.isSubscribedCivicLiker
+      );
+    },
+    userDisplayName() {
+      return this.userInfo?.displayName || this.wallet;
+    },
+    userDescription() {
+      return this.userInfo?.description;
+    },
+  },
+};
+</script>
