d8aee12ee024c5f3205f88d436b0ce04e0574240
AuroraHuang22 <aurora90376@gmail.com>
2022-08-29 22:06:35 +0800

    💄 Update portfolioItem layout

--- src/components/NFTPortfolio/Item.vue
+++ src/components/NFTPortfolio/Item.vue
@@ -1,75 +1,77 @@
 <template>
-  <NuxtLink
-    v-if="isWritingNFT"
-    class="mx-auto mb-[5px] break-inside-avoid"
-    :to="{ name: 'nft-class-classId', params: { classId } }"
-    target="_blank"
-    @click="handleClickViewDetails"
-  >
-    <NFTPortfolioCard>
-      <div
-        class="h-[180px]"
-        :style="`background-color: ${NFTImageBackgroundColor}`"
-      >
-        <img
-          class="object-cover w-full max-h-[180px]"
-          :src="NFTImageUrl"
+  <li>
+    <NuxtLink
+      v-if="isWritingNFT"
+      class="mx-auto mb-[5px] break-inside-avoid"
+      :to="{ name: 'nft-class-classId', params: { classId } }"
+      target="_blank"
+      @click="handleClickViewDetails"
+    >
+      <NFTPortfolioCard>
+        <div
+          class="h-[180px]"
+          :style="`background-color: ${NFTImageBackgroundColor}`"
         >
-      </div>
-      <div
-        :class="[
-          'flex',
-          'flex-col',
-          'text-center',
-          'whitespace-pre-line',
-          'px-[24px]',
-          'pt-[48px]',
-          'py-[24px]',
-          'relative',
-        ]"
-      >
-        <div class="flex flex-col items-center justify-center mt-[-70px]">
-          <Identity
-            :avatar-url="avatarList[iscnOwner]"
-            :avatar-size="40"
-            :is-avatar-outlined="civicLikerList[iscnOwner]"
-          />
-          <div class="flex mt-[8px]">
-            <Label class="text-medium-gray">by</Label>
-            <Label
-              class="text-like-green ml-[4px] font-[600]"
-            >{{ displayNameList[iscnOwner] | ellipsis }}</Label>
-          </div>
-        </div>
-        <Label preset="h5" class="mt-[12px] break-all" align="center">
-          {{ NFTName }}
-        </Label>
-        <div class="z-[500] flex justify-center my-[16px]">
-          <ProgressIndicator v-if="uiIsOpenCollectModal && isCollecting" />
-          <ButtonV2
-            v-else
-            preset="secondary"
-            @click.stop.prevent="handleClickCollect"
+          <img
+            class="object-cover w-full max-h-[180px]"
+            :src="NFTImageUrl"
           >
-            <span>{{ NFTPrice || '-' }} $LIKE</span>
-            <template #prepend>
-              <IconPrice />
-            </template>
-          </ButtonV2>
         </div>
-        <div class="flex items-center justify-center">
-          <div class="flex items-center text-medium-gray mr-[18px]">
-            <IconMint />
-            <div class="ml-[4px]">{{ mintedCount }}</div>
+        <div
+          :class="[
+            'flex',
+            'flex-col',
+            'text-center',
+            'whitespace-pre-line',
+            'px-[24px]',
+            'pt-[48px]',
+            'py-[24px]',
+            'relative',
+          ]"
+        >
+          <div class="flex flex-col items-center justify-center mt-[-70px]">
+            <Identity
+              :avatar-url="avatarList[iscnOwner]"
+              :avatar-size="40"
+              :is-avatar-outlined="civicLikerList[iscnOwner]"
+            />
+            <div class="flex mt-[8px]">
+              <Label class="text-medium-gray">by</Label>
+              <Label
+                class="text-like-green ml-[4px] font-[600]"
+              >{{ displayNameList[iscnOwner] | ellipsis }}</Label>
+            </div>
           </div>
-          <div class="flex items-center text-medium-gray">
-            <IconOwner />
-            <div class="ml-[4px]">{{ ownerCount }}</div>
+          <Label preset="h5" class="mt-[12px] break-all" align="center">
+            {{ NFTName }}
+          </Label>
+          <div class="z-[500] flex justify-center my-[16px]">
+            <ProgressIndicator v-if="uiIsOpenCollectModal && isCollecting" />
+            <ButtonV2
+              v-else
+              preset="secondary"
+              @click.stop.prevent="handleClickCollect"
+            >
+              <span>{{ NFTPrice || '-' }} $LIKE</span>
+              <template #prepend>
+                <IconPrice />
+              </template>
+            </ButtonV2>
+          </div>
+          <div class="flex items-center justify-center">
+            <div class="flex items-center text-medium-gray mr-[18px]">
+              <IconMint />
+              <div class="ml-[4px]">{{ mintedCount }}</div>
+            </div>
+            <div class="flex items-center text-medium-gray">
+              <IconOwner />
+              <div class="ml-[4px]">{{ ownerCount }}</div>
+            </div>
           </div>
         </div>
-      </div>
-    </NFTPortfolioCard>
-  </NuxtLink>
+      </NFTPortfolioCard>
+    </NuxtLink>
+  </li>
 </template>
 
 <script>
