cd3d21692b010c5cf16266291c9342577347fc20
AuroraHuang22 <aurora90376@gmail.com>
2022-08-30 10:23:16 +0800

    ♻️ Refactor collect modal

---
+++ src/components/EventModal/Collect.vue
@@ -0,0 +1,25 @@
+<template>
+  <TxModal
+    :is-open="isOpen"
+    :has-close-button="false"
+    :header-text="$t('nft_details_page_title_collect')"
+    :complete-text="$t('tx_modal_status_complete_text_collect')"
+    @close="$emit('close')"
+  >
+    <template #header-prepend>
+      <IconPrice />
+    </template>
+    <NFTPageOwning />
+  </TxModal>
+</template>
+
+<script>
+export default {
+  props: {
+    isOpen: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
