9a2a3c7f1b1c0eb16cdfb9b0e08772aa0acd88e6
William Chong <me@williamchong.cloud>
2022-08-31 17:23:50 +0800

    🔥 Remove unused civic api

--- src/server/api/routes/civic/index.js
+++ src/server/api/routes/civic/index.js
@@ -1,89 +1,9 @@
-const axios = require('axios');
 const { Router } = require('express');
 
-const { PAYPAL_PDT_HOOK } = require('../../../config/config');
-const {
-  apiCivicLikerTrialEventById,
-  apiCivicLikerJoinTrialEventById,
-} = require('../../util/api');
-const stripe = require('./stripe');
-const likepay = require('./likepay');
-const support = require('./support');
 const staking = require('./staking');
 
 const router = Router();
 
-router.use(likepay);
-router.use(stripe);
-router.use(support);
 router.use(staking);
-
-router.get('/civic/trial/events/:id', async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const { data } = await apiCivicLikerTrialEventById(id);
-    res.json(data);
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.post('/civic/trial/events/:eventId/join', async (req, res, next) => {
-  try {
-    if (!req.session.user) {
-      res.sendStatus(401);
-      return;
-    }
-    const { eventId } = req.params;
-    const { data } = await apiCivicLikerJoinTrialEventById(eventId, req);
-    res.json(data);
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.post('/civic/payment/paypal', async (req, res, next) => {
-  try {
-    if (!req.session.user) {
-      res.sendStatus(401);
-      return;
-    }
-    if (PAYPAL_PDT_HOOK) {
-      const {
-        amt,
-        cc,
-        cm,
-        sig,
-        st,
-        tx,
-        from,
-        referrer,
-        utmCampaign,
-        utmContent,
-        utmMedium,
-        utmSource,
-        version,
-      } = req.body;
-      await axios.post(PAYPAL_PDT_HOOK, {
-        amt,
-        cc,
-        cm,
-        sig,
-        st,
-        tx,
-        from,
-        referrer,
-        utmCampaign,
-        utmContent,
-        utmMedium,
-        utmSource,
-        version,
-      });
-    }
-    res.sendStatus(200);
-  } catch (err) {
-    next(err);
-  }
-});
 
 module.exports = router;
