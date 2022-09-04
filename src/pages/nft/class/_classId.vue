285cb46670818836a3698491d03bc19af77b21bd
WeiJun0827 <weijunhung@outlook.com>
2022-09-04 23:18:23 +0800

    🐛 Do not fetch purchase info of primitive NFT

--- src/pages/nft/class/_classId.vue
+++ src/pages/nft/class/_classId.vue
@@ -263,13 +263,14 @@ export default {
     await store.dispatch('fetchNFTMetadata', classId);
   },
   async mounted() {
-    await Promise.all([
+    const promises = [
       this.updateDisplayNameList(this.iscnOwner),
-      this.updateNFTPurchaseInfo(),
       this.updateNFTOwners(),
       this.updateNFTHistory(),
       this.getLIKEPrice(),
-    ]);
+    ];
+    if (this.isWritingNFT) promises.push(this.updateNFTPurchaseInfo());
+    await Promise.all(promises);
     this.isLoading = false;
     if (this.action === 'collect') {
       logTrackerEvent(this, 'NFT', 'NFTCollect(NFTWidget)', this.classId, 1);
