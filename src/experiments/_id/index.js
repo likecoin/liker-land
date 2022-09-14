36b5d4229d11e790d485c9234cd936f0f3f138fe
William Chong <me@williamchong.cloud>
2022-09-14 13:39:44 +0800

    ⚗️ Test ab test is working

--- src/experiments/_id/index.js
+++ src/experiments/_id/index.js
@@ -1,9 +1,7 @@
 export default {
   name: 'id',
-  experimentID: 'Us72JhlBSHCMQ0e_4oW4qA',
+  experimentID: 'wFPnxOVMTdS2hi6VQtHuoA',
   isEligible: ({ route }) =>
-    !process.server &&
-    /^id/.test(route.name) &&
-    route.params.id !== 'standnews',
+    !process.server && route.name === 'campaign-writing-nft',
   variants: [{ name: 'origin', weight: 5 }, { name: 'variant', weight: 5 }],
 };
