a9060008c77ca0b31feeb9335ab191fc05e84e12
Ng Wing Tat, David <i@ngwingt.at>
2022-09-23 00:35:55 +0800

    🚸 Use popup for settings

--- src/util/links.js
+++ src/util/links.js
@@ -2,4 +2,4 @@ import { LIKE_CO_URL_BASE } from '~/constant';
 
 export const getLikeCoURL = (path = '') => `${LIKE_CO_URL_BASE}${path}`;
 
-export const getLikerIdSettingsURL = () => getLikeCoURL('/in/settings');
+export const getLikerIdSettingsURL = () => getLikeCoURL('/in/settings?popup=1');
