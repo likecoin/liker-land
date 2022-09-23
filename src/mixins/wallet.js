af8b50f70c5f3d0c80c0d54537d8743d6ef5102a
Ng Wing Tat, David <i@ngwingt.at>
2022-09-23 17:33:50 +0800

    📈 Log connect wallet event

--- src/mixins/wallet.js
+++ src/mixins/wallet.js
@@ -1,5 +1,7 @@
 import { mapActions, mapGetters } from 'vuex';
 
+import { logTrackerEvent } from '~/util/EventLogger';
+
 export default {
   computed: {
     ...mapGetters([
@@ -29,12 +31,26 @@ export default {
   },
   methods: {
     ...mapActions([
-      'connectWallet',
+      'openConnectWalletModal',
       'disconnectWallet',
+      'initWallet',
       'initIfNecessary',
       'restoreSession',
       'walletFetchLIKEBalance',
     ]),
+    async connectWallet() {
+      const connection = await this.openConnectWalletModal();
+      if (!connection) return false;
+      const { method } = connection;
+      logTrackerEvent(
+        this,
+        'user',
+        `connected_wallet_${method}`,
+        'connected_wallet',
+        1
+      );
+      return this.initWallet(connection);
+    },
     async navigateToMyDashboard() {
       if (!this.getAddress) {
         const isConnected = await this.connectWallet();
