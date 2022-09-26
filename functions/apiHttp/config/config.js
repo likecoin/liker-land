f247553227335bd088f2fda28e5d153460447877
William Chong <me@williamchong.cloud>
2022-09-27 01:00:33 +0800

    🔥 Remove dead code

--- functions/apiHttp/config/config.js
+++ functions/apiHttp/config/config.js
@@ -14,15 +14,6 @@ config.COOKIE_SECRET = functions.config().cookie.secret;
 
 config.CRISP_USER_HASH_SECRET = functions.config().crisp.user_hash_secret;
 
-config.PAYPAL_PDT_HOOK = functions.config().hook.paypal_pdt;
-
-config.LIKEPAY_HOOK = functions.config().hook.likepay;
-
-config.STRIPE_PRIVATE_KEY = functions.config().stripe.private_key;
-config.STRIPE_WEBHOOK_SECRET = functions.config().stripe.webhook_secret;
-config.STRIPE_PLAN_ID = functions.config().stripe.plan_id;
-config.STRIPE_CIVIC_V2_PRICE_ID = functions.config().stripe.civic_v2_price_id;
-
 config.EXTERNAL_URL = functions.config().constant.external_url;
 
 module.exports = config;
