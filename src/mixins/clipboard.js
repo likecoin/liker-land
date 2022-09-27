746993edaaf1f2c02e52d31eb4e618ccfe5d749c
hsueh kuan tsai <b123456785678@gmail.com>
2022-09-27 12:01:45 +0800

    🚧 Wait check title/text

--- src/mixins/clipboard.js
+++ src/mixins/clipboard.js
@@ -6,8 +6,16 @@ export default {
   mixins: [alertMixin],
   methods: {
     copyToClipboard(text, { alertMessage = this.$t('clipboard_copied') } = {}) {
-      copyToClipboard(text);
-      this.alertPromptSuccess(alertMessage);
+      if (navigator.share) {
+        navigator.share({
+          title: 'Just title',
+          text: 'Just text',
+          url: text,
+        });
+      } else {
+        copyToClipboard(text);
+        this.alertPromptSuccess(alertMessage);
+      }
     },
     copyURLPath(path, options) {
       const host = `${window.location.protocol}//${window.location.host}`;
