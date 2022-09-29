8ecd7903c6d02ffa6104d74b82ab9809d79ac41a
Ng Wing Tat, David <i@ngwingt.at>
2022-09-29 17:15:08 +0800

    🐛 Fix storybook build

--- src/.storybook/postcss.config.js
+++ src/.storybook/postcss.config.js
@@ -2,7 +2,6 @@ const tailwindcss = require('tailwindcss');
 
 module.exports = {
   plugins: [
-    require('postcss-import')(),
     tailwindcss('./tailwind.config.js'),
     require('autoprefixer'),
   ],
