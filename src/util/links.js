829a4af4d580aeae8409a8479f74e72d8431c50f
Ng Wing Tat, David <i@ngwingt.at>
2022-09-29 14:52:30 +0800

    🚸 [Settings] Sync language for like.co

--- src/util/links.js
+++ src/util/links.js
@@ -2,5 +2,7 @@ import { LIKE_CO_URL_BASE } from '~/constant';
 
 export const getLikeCoURL = (path = '') => `${LIKE_CO_URL_BASE}${path}`;
 
-export const getLikerIdSettingsURL = ({ wallet = '' } = {}) =>
-  getLikeCoURL(`/in/settings?popup=1&user_wallet=${wallet}`);
+export const getLikerIdSettingsURL = ({ wallet = '', language = 'en' } = {}) =>
+  getLikeCoURL(
+    `/in/settings?popup=1&user_wallet=${wallet}&language=${language}`
+  );
