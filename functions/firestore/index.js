7a5d82a029bd805aa0e482ece0066c258ff437b5
Ng Wing Tat, David <i@ngwingt.at>
2022-10-27 12:47:50 +0800

    ✨ Show Liker info in NFT mint subscription email

--- functions/firestore/index.js
+++ functions/firestore/index.js
@@ -1,19 +1,42 @@
 const { firestore, config } = require('firebase-functions');
-const { getBasicTemplate } = require('@likecoin/edm');
+const { getBasicWithAvatarTemplate } = require('@likecoin/edm');
+const axios = require('axios').default;
 
 const { sendEmail } = require('../modules/sendgrid');
 
-const { EXTERNAL_URL } = process.env;
+const { EXTERNAL_URL, LIKECOIN_API_BASE } = process.env;
 
 module.exports = firestore
   .document(`${config().db.firestore_nft_mint_subscriptions_root}/{id}`)
-  .onCreate(snapshot => {
+  .onCreate(async snapshot => {
     const subscriptionId = snapshot.id;
     const { subscriberEmail, subscribedWallet } = snapshot.data();
-    const { body } = getBasicTemplate({
+
+    let avatar = `https://avatars.dicebear.com/api/identicon/${subscribedWallet}.svg?background=#ffffff`;
+    let displayName = subscribedWallet;
+    let isSubscribedCivicLiker = false;
+    try {
+      const res = await axios.get(
+        `${LIKECOIN_API_BASE}/users/addr/${subscribedWallet}/min`
+      );
+      ({ avatar, displayName, isSubscribedCivicLiker } = res.data);
+    } catch (err) {
+      if (!err.response || err.response.status !== 404) {
+        // eslint-disable-next-line no-console
+        console.error(err);
+      }
+    }
+
+    const unsubscribeLink = `${EXTERNAL_URL}/${subscribedWallet}/unsubscribe/${subscriptionId}`;
+    const { body } = getBasicWithAvatarTemplate({
       title: 'Writing NFT',
-      subtitle: `You have subscribed ${subscribedWallet}`,
-      content: `Hi,\nyou will receive email when ${subscribedWallet} create any NFT. You can cancel the subscription by ${EXTERNAL_URL}/${subscribedWallet}/unsubscribe/${subscriptionId}`,
+      subtitle: `You have subscribed ${displayName}`,
+      content: `<p>Hi,</p>
+      <p>You will receive email when <b>${displayName}</b> create any Writing NFT.
+      You can cancel the subscription by <a href="${unsubscribeLink}" target="_blank" rel="noreferrer">${unsubscribeLink}</a>.</p>`,
+      avatarURL: avatar,
+      isCivicLiker: isSubscribedCivicLiker,
+      unsubscribeLink,
       language: 'en',
     });
     return sendEmail({
