4fe70d57900403ce6d04e35c61a6e235d8db64ea
AuroraHuang22 <aurora90376@gmail.com>
2022-11-01 14:04:37 +0800

    💄[Details] Update event list UI

---
+++ src/components/NFTPage/EventList/Metadata.vue
@@ -0,0 +1,95 @@
+<template>
+  <div>
+    <Label
+      class="my-[20px]"
+      :text="$t('nft_details_page_section_metadata_intro')"
+      tag="div"
+      preset="p6"
+    />
+    <div class="flex flex-col laptop:flex-row laptop:items-end laptop:justify-between">
+      <div class="flex-col items-start">
+        <Label class="!text-[12px] text-medium-gray font-600">
+          {{ $t('nft_details_page_section_metadata_url') }}&nbsp;<IconLinkExternal />
+        </Label>
+        <a :href="contentUrl" target="_blank" class="block text-medium-gray underline text-[12px]">{{ contentUrl }}</a>
+      </div>
+      <div class="flex mt-[12px] gap-[8px] laptop:mt-0">
+        <ButtonV2
+          v-for="record in records"
+          :key="record.text"
+          :href="record.href"
+          size="mini"
+          preset="tertiary"
+          class="text-medium-gray"
+          content-class="text-[12px]"
+        >
+          {{ record.label }}&nbsp;<IconLinkExternal />
+        </ButtonV2>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {
+  ARWEAVE_ENDPOINT,
+  APP_LIKE_CO_URL_BASE,
+  IPFS_VIEW_GATEWAY_URL,
+} from '~/constant';
+
+export default {
+  name: 'NFTPageMetadataSection',
+  props: {
+    contentUrl: {
+      type: String,
+      default: '',
+    },
+    iscnId: {
+      type: String,
+      default: '',
+    },
+    iscnUrl: {
+      type: String,
+      default: '',
+    },
+    contentFingerprints: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  computed: {
+    records() {
+      const records = [
+        {
+          label: this.$t('nft_details_page_section_metadata_iscn'),
+          href: this.iscnUrl,
+          text: this.iscnId,
+        },
+      ];
+
+      this.contentFingerprints.forEach(fingerprint => {
+        const [protocol, text] = fingerprint.split('://');
+        switch (protocol) {
+          case 'ar':
+            records.push({
+              label: this.$t('nft_details_page_section_metadata_ar'),
+              href: `${ARWEAVE_ENDPOINT}/${text}`,
+              text,
+            });
+            break;
+          case 'ipfs':
+            records.push({
+              label: this.$t('nft_details_page_section_metadata_ipfs'),
+              href: `${IPFS_VIEW_GATEWAY_URL}/${text}`,
+              text,
+            });
+            break;
+          default:
+            break;
+        }
+      });
+      return records;
+    },
+  },
+};
+</script>
