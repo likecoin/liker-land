9ba580d91b4b004da6863fd8c21ffbbe241d58fc
WeiJun0827 <weijunhung@outlook.com>
2022-11-02 22:04:11 +0800

    🚧 Implement hidden/featured NFT classId APIs

--- src/server/api/routes/nft/featured.js
+++ src/server/api/routes/nft/featured.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
-const parse = require('url-parse');
 const { setPrivateCacheHeader } = require('../../middleware/cache');
 const { handleRestfulError } = require('../../middleware/error');
+const { isValidAddress } = require('../../util/cosmos');
 const { walletUserCollection } = require('../../util/firebase');
 
 const router = Router();
@@ -9,7 +9,10 @@ const router = Router();
 router.get('/nft/featured', async (req, res, next) => {
   try {
     const userId = req.query.owner;
-    // TODO: check valid wallet
+    if (!isValidAddress(userId)) {
+      res.sendStatus(400);
+      return;
+    }
     const userDoc = await walletUserCollection(userId).get();
     const { featuredNftClassIds = [] } = userDoc.data();
     res.json({ featured: featuredNftClassIds });
@@ -21,11 +24,22 @@ router.get('/nft/featured', async (req, res, next) => {
 router.post('/nft/featured', async (req, res, next) => {
   try {
     setPrivateCacheHeader(res);
-    if (!req.session.user || req.session.version !== 2) {
+    const { user, version } = req.session;
+    if (!user || version !== 2) {
       res.sendStatus(401);
       return;
     }
-    // TODO
+    const { nftClassIds: inputNftClassIds = [] } = req.body;
+    if (!inputNftClassIds.length) {
+      res.sendStatus(400);
+      return;
+    }
+    const userDoc = await walletUserCollection(user).get();
+    const { featuredNftClassIds = [] } = userDoc.data();
+    const newFeaturedNftClassIds = Array.from(
+      new Set([...featuredNftClassIds, ...inputNftClassIds])
+    );
+    await userDoc.ref.update({ featuredNftClassIds: newFeaturedNftClassIds });
     res.sendStatus(200);
   } catch (err) {
     handleRestfulError(req, res, next, err);
@@ -35,11 +49,24 @@ router.post('/nft/featured', async (req, res, next) => {
 router.delete('/nft/featured', async (req, res, next) => {
   try {
     setPrivateCacheHeader(res);
-    if (!req.session.user || req.session.version !== 2) {
+    const { user, version } = req.session;
+    if (!user || version !== 2) {
       res.sendStatus(401);
       return;
     }
-    // TODO
+    const { nftClassIds: inputNftClassIds = [] } = req.body;
+    if (!inputNftClassIds.length) {
+      res.sendStatus(400);
+      return;
+    }
+    const userDoc = await walletUserCollection(user).get();
+    const { featuredNftClassIds = [] } = userDoc.data();
+    const set = new Set(featuredNftClassIds);
+    inputNftClassIds.forEach(classId => {
+      if (set.has(classId)) set.delete(classId);
+    });
+    const newFeaturedNftClassIds = Array.from(set);
+    await userDoc.ref.update({ featuredNftClassIds: newFeaturedNftClassIds });
     res.sendStatus(200);
   } catch (err) {
     handleRestfulError(req, res, next, err);
--- src/server/api/routes/nft/hidden.js
+++ src/server/api/routes/nft/hidden.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
-const parse = require('url-parse');
 const { setPrivateCacheHeader } = require('../../middleware/cache');
 const { handleRestfulError } = require('../../middleware/error');
+const { isValidAddress } = require('../../util/cosmos');
 const { walletUserCollection } = require('../../util/firebase');
 
 const router = Router();
@@ -9,7 +9,10 @@ const router = Router();
 router.get('/nft/hidden', async (req, res, next) => {
   try {
     const userId = req.query.owner;
-    // TODO: check valid wallet
+    if (!isValidAddress(userId)) {
+      res.sendStatus(400);
+      return;
+    }
     const userDoc = await walletUserCollection(userId).get();
     const { hiddenNftClassIds = [] } = userDoc.data();
     res.json({ hidden: hiddenNftClassIds });
@@ -21,11 +24,22 @@ router.get('/nft/hidden', async (req, res, next) => {
 router.post('/nft/hidden', async (req, res, next) => {
   try {
     setPrivateCacheHeader(res);
-    if (!req.session.user || req.session.version !== 2) {
+    const { user, version } = req.session;
+    if (!user || version !== 2) {
       res.sendStatus(401);
       return;
     }
-    // TODO
+    const { nftClassIds: inputNftClassIds = [] } = req.body;
+    if (!inputNftClassIds.length) {
+      res.sendStatus(400);
+      return;
+    }
+    const userDoc = await walletUserCollection(user).get();
+    const { hiddenNftClassIds = [] } = userDoc.data();
+    const newHiddenNftClassIds = Array.from(
+      new Set([...hiddenNftClassIds, ...inputNftClassIds])
+    );
+    await userDoc.ref.update({ hiddenNftClassIds: newHiddenNftClassIds });
     res.sendStatus(200);
   } catch (err) {
     handleRestfulError(req, res, next, err);
@@ -35,11 +49,24 @@ router.post('/nft/hidden', async (req, res, next) => {
 router.delete('/nft/hidden', async (req, res, next) => {
   try {
     setPrivateCacheHeader(res);
-    if (!req.session.user || req.session.version !== 2) {
+    const { user, version } = req.session;
+    if (!user || version !== 2) {
       res.sendStatus(401);
       return;
     }
-    // TODO
+    const { nftClassIds: inputNftClassIds = [] } = req.body;
+    if (!inputNftClassIds.length) {
+      res.sendStatus(400);
+      return;
+    }
+    const userDoc = await walletUserCollection(user).get();
+    const { hiddenNftClassIds = [] } = userDoc.data();
+    const set = new Set(hiddenNftClassIds);
+    inputNftClassIds.forEach(classId => {
+      if (set.has(classId)) set.delete(classId);
+    });
+    const newHiddenNftClassIds = Array.from(set);
+    await userDoc.ref.update({ hiddenNftClassIds: newHiddenNftClassIds });
     res.sendStatus(200);
   } catch (err) {
     handleRestfulError(req, res, next, err);
