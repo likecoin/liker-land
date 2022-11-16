f05457a245ab5b07944861c3e318a423415ba430
Wei-Jun Hung <weijunhung@outlook.com>
2022-11-16 13:28:47 +0800

    🎨 Return `email` in `/self`

--- src/server/api/routes/users/v2/auth.js
+++ src/server/api/routes/users/v2/auth.js
@@ -24,10 +24,11 @@ router.get('/self', authenticateV2Login, async (req, res, next) => {
     setPrivateCacheHeader(res);
     const { user } = req.session;
     const userDoc = await walletUserCollection.doc(user).get();
-    const { displayName, followers } = userDoc.data();
+    const { displayName, eamil, followers } = userDoc.data();
     res.json({
       user,
       displayName,
+      eamil,
       followers,
     });
   } catch (err) {
