9649d3d5cdc47d2bc19e47b39cde5332c5c62f32
Ng Wing Tat, David <i@ngwingt.at>
2022-11-24 13:44:09 +0800

    🐛 [Portfolio] Fix error when switching wallet

--- src/mixins/portfolio.js
+++ src/mixins/portfolio.js
@@ -257,6 +257,8 @@ export default {
   },
   watch: {
     nftClassListMap(listMap, listMapPrev) {
+      if (!listMap) return;
+
       const nftClassIdListSet = new Set(
         Object.values(listMap)
           .flat()
