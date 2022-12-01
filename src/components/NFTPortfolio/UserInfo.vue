837d0c9672653f1393e703df8826344932371db2
AuroraHuang22 <aurora90376@gmail.com>
2022-12-01 13:31:50 +0800

    ✨ [Portfolio] Enable copy address

--- src/components/NFTPortfolio/UserInfo.vue
+++ src/components/NFTPortfolio/UserInfo.vue
@@ -4,6 +4,30 @@
     <Label preset="h3" :class="['text-like-green', 'mt-[18px]']">
       {{ userDisplayName }}
     </Label>
+    <ToolTips :tool-tip-text="wallet" class="mt-[12px]">
+      <div
+        :class="[
+          'flex',
+          'items-center',
+          'justify-center',
+
+          'px-[12px]',
+          'text-medium-gray',
+          'text-[10px]',
+          'rounded-full',
+          'border-[1px]',
+          'border-medium-gray',
+
+          'cursor-pointer',
+          'hover:text-dark-gray',
+          'transition',
+          'duration-200',
+        ]"
+        @click="copyToClipboard(wallet)"
+      >
+        {{ wallet | ellipsis }}
+      </div>
+    </ToolTips>
     <template v-if="userDescription">
       <hr :class="['w-full', 'border-shade-gray', 'my-[16px]']">
       <Label preset="p6" class="break-all font-200 my-[12px]">
@@ -15,9 +39,14 @@
 </template>
 <script>
 import userInfoMixin from '~/mixins/user-info';
+import clipboardMixin from '~/mixins/clipboard';
+import { ellipsis } from '~/util/ui';
 
 export default {
-  mixins: [userInfoMixin],
+  filters: {
+    ellipsis,
+  },
+  mixins: [userInfoMixin, clipboardMixin],
   props: {
     wallet: {
       type: String,
