f9a3a6ec7cc3dd9dcb57237cf46ce0fc0fa6e6da
AuroraHuang22 <aurora90376@gmail.com>
2022-12-10 00:34:05 +0800

    💄 Add <NFTPageCollectedList />

---
+++ src/components/NFTPage/CollectedList/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <CardV2 class="flex gap-[24px] w-full">
+    <div>
+      <Label
+        class="text-medium-gray text-[12px]"
+        preset="h6"
+        :text="$t('nft_details_page_label_owning')"
+      />
+      <div class="mt-[4px] text-[600]">{{ nftCollectorCollectedCount }}</div>
+    </div>
+    <hr class="w-[2px] bg-shade-gray h-[48px] border-none shrink-0">
+    <div class="min-w-0">
+      <Label
+        class="text-medium-gray text-[12px]"
+        preset="h6"
+        :text="$t('nft_details_page_label_nft_id')"
+      />
+      <div class="mt-[4px] flex items-center relative gap-[8px]">
+        <select
+          v-model="selectedNftId"
+          class="absolute opacity-0"
+          @change="onSelectNFT"
+        >
+          <option
+            v-for="id in nftCollectorCollectedNFTList"
+            :key="id"
+            :value="id"
+          >{{ id }}</option>
+        </select>
+        <div class="truncate">{{ selectedNftId }}</div>
+        <IconArrowDown class="w-[12px] h-[12px] shrink-0" />
+      </div>
+    </div>
+  </CardV2>
+</template>
+<script>
+export default {
+  props: {
+    nftCollectorCollectedCount: {
+      type: Number,
+      default: undefined,
+    },
+    selectedNftId: {
+      type: String,
+      default: undefined,
+    },
+    nftCollectorCollectedNFTList: {
+      type: Array,
+      default: undefined,
+    },
+  },
+  methods: {
+    onSelectNFT() {
+      this.$emit('onSelectNFT');
+    },
+  },
+};
+</script>
