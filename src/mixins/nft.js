7213fff3341756db22311b617ef277a7383856a7
William Chong <me@williamchong.cloud>
2022-12-17 01:22:48 +0800

    🚑️ Fix invalid ISCN ID field

--- src/mixins/nft.js
+++ src/mixins/nft.js
@@ -131,7 +131,7 @@ export default {
       return this.getNFTClassOwnerInfoById(this.classId) || {};
     },
     iscnId() {
-      return this.NFTClassMetadata.parent?.iscn_id_prefix;
+      return this.NFTClassMetadata.parent?.iscnIdPrefix;
     },
     iscnOwner() {
       return (
