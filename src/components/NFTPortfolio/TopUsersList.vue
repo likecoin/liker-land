82d668e0ccb14aa3d0f28d4056cffc9547084d3a
William Chong <me@williamchong.cloud>
2022-12-22 13:49:43 +0800

    💄 Reduce top user card size

--- src/components/NFTPortfolio/TopUsersList.vue
+++ src/components/NFTPortfolio/TopUsersList.vue
@@ -1,34 +1,39 @@
 <template>
-  <CollapsibleCard
+  <CardV2
     class="w-full"
-    :title="label"
     :has-content-padding="true"
     :is-narrow="true"
   >
-    <template #content>
-      <div class="flex items-center justify-between my-[24px]">
-        <span v-for="user in userList" :key="user.id">
-          <ToolTips :tool-tip-text="user.displayName">
-            <LinkV2
-              class="flex items-center gap-[8px]"
-              :to="{
-                name: 'id',
-                params: { id: user.id }
-              }"
-            >
-              <IdentityAvatar
-                :url="user.avatar"
-                :display-name="user.displayName"
-                :size="36"
-                :is-outlined="user.isCivicLiker"
-                :is-lazy-loaded="true"
-              />
-            </LinkV2>
-          </ToolTips>
-        </span>
-      </div>
-    </template>
-  </CollapsibleCard>
+    <Label
+      class="w-min font-600"
+      :text="label"
+      preset="h5"
+      valign="middle"
+      content-class="whitespace-nowrap text-like-green "
+      prepend-class="text-like-green"
+    />
+    <div class="flex items-center mt-[12px]">
+      <span v-for="user in userList" :key="user.id">
+        <ToolTips :tool-tip-text="user.displayName">
+          <LinkV2
+            class="flex items-center gap-[8px]"
+            :to="{
+              name: 'id',
+              params: { id: user.id }
+            }"
+          >
+            <IdentityAvatar
+              :url="user.avatar"
+              :display-name="user.displayName"
+              :size="36"
+              :is-outlined="user.isCivicLiker"
+              :is-lazy-loaded="true"
+            />
+          </LinkV2>
+        </ToolTips>
+      </span>
+    </div>
+  </CardV2>
 </template>
 
 <script>
