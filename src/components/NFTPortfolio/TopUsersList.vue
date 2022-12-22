54a5a8bf5be314644b8d68a71c3e121412fe87ec
AuroraHuang22 <aurora90376@gmail.com>
2022-12-22 11:35:57 +0800

    💄 Update list UI to display items horizontally

--- src/components/NFTPortfolio/TopUsersList.vue
+++ src/components/NFTPortfolio/TopUsersList.vue
@@ -1,31 +1,34 @@
 <template>
-  <CardV2 :class="['inline', 'flex-col', 'items-center', 'w-full']">
-    <Label
-      class="w-min font-600"
-      :text="label"
-      preset="h5"
-      valign="middle"
-      content-class="whitespace-nowrap text-like-green "
-      prepend-class="text-like-green"
-    />
-    <span v-for="user in userList" :key="user.id" class="flex items-center justify-between">
-      <LinkV2
-        class="flex items-center gap-[8px]"
-        :to="{
-          name: 'id',
-          params: { id: user.id }
-        }"
-      >
-        <IdentityAvatar
-          :url="user.avatar"
-          :display-name="user.displayName"
-          :size="36"
-          :is-outlined="user.isCivicLiker"
-          :is-lazy-loaded="true"
-        />
-      </LinkV2>
-    </span>
-  </CardV2>
+  <CollapsibleCard
+    class="w-full"
+    :title="label"
+    :has-content-padding="true"
+    :is-narrow="true"
+  >
+    <template #content>
+      <div class="flex items-center justify-between my-[24px]">
+        <span v-for="user in userList" :key="user.id">
+          <ToolTips :tool-tip-text="user.displayName">
+            <LinkV2
+              class="flex items-center gap-[8px]"
+              :to="{
+                name: 'id',
+                params: { id: user.id }
+              }"
+            >
+              <IdentityAvatar
+                :url="user.avatar"
+                :display-name="user.displayName"
+                :size="36"
+                :is-outlined="user.isCivicLiker"
+                :is-lazy-loaded="true"
+              />
+            </LinkV2>
+          </ToolTips>
+        </span>
+      </div>
+    </template>
+  </CollapsibleCard>
 </template>
 
 <script>
