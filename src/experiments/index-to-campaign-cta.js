b6df76b7a43061d78dcb3caa1b05ee9bb5cb4161
William Chong <me@williamchong.cloud>
2023-01-27 03:53:59 +0800

    ⚗️ Apply home page to campaign CTA test result

--- src/experiments/index-to-campaign-cta.js
+++
@@ -1,6 +0,0 @@
-export default {
-  name: 'index-to-campaign-cta',
-  experimentID: 'DgS_xaxqQd6EOHWDehQ0Cg',
-  isEligible: ({ route }) => !process.server && route.name === 'index',
-  variants: [{ name: 'origin', weight: 1 }, { name: 'variant', weight: 1 }],
-};
