997a2f38c9f68980fac4867388626f75910a6d0e
AuroraHuang22 <aurora90376@gmail.com>
2023-02-02 17:33:53 +0800

    🎨 [Event] Add reverse params to get latest events

--- src/util/api/index.js
+++ src/util/api/index.js
@@ -199,6 +199,7 @@ export const getNFTEvents = ({
   actionType,
   ignoreToList,
   ignoreFromList,
+  reverse,
 }) => {
   const qsPayload = {};
   if (classId) qsPayload.class_id = classId;
@@ -211,6 +212,7 @@ export const getNFTEvents = ({
   if (ignoreFromList) qsPayload.ignore_from_list = ignoreFromList;
   if (key) qsPayload.key = key;
   if (limit) qsPayload.limit = limit;
+  if (reverse) qsPayload['pagination.reverse'] = reverse;
   return `${LIKECOIN_CHAIN_API}/likechain/likenft/v1/event?${querystring.stringify(
     qsPayload
   )}`;
