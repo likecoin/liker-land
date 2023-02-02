9042f2a74ca5706557c2ec533b6a1d5f1799bd73
AuroraHuang22 <aurora90376@gmail.com>
2023-02-02 17:23:32 +0800

    💄 [Event] Update UI with grouping by time

--- src/pages/events/index.vue
+++ src/pages/events/index.vue
@@ -1,51 +1,172 @@
 <template>
-  <Page class="px-[8px]">
-    <div>
-      <ButtonV2 @click="handleRefresh()">{{ $t('event_list_page_event_refresh') }}</ButtonV2>
-      <table class="w-full table-fixed text-[10px]">
-        <thead class="border-b-shade-gray border-b-[2px]">
-          <tr class="text-medium-gray py-[12px]">
-            <th><Label :text="$t('event_list_page_event_header_type')" /></th>
-            <th><Label class="break-normal" :text="$t('event_list_page_event_header_details')" /></th>
-            <th><Label :text="$t('event_list_page_event_header_time')" /></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr
-            v-for="event in populatedEvents"
-            :key="[event.tx_hash, event.class_id, event.nft_id, event.eventType].join('-')"
-            class="py-[12px] border-b-shade-gray border-b-[1px] text-dark-gray cursor-pointer hover:bg-light-gray transition-colors"
-          >
-            <td>
-              <Label content-class="overflow-hidden">
-                {{ event.eventType }}
-              </Label>
-            </td>
-            <td>
-              <LinkV2 :to="event.targetRoute" :is-inline="true" @click.native.stop>
-                <span>{{ event.text }}</span>
-              </LinkV2>
-            </td>
-            <td>
-              <LinkV2 class="text-left" :href="getChainExplorerTx(event.tx_hash)" :is-inline="true" @click.native.stop>
-                <TimeAgo class="px-[2px]" long tooltip :datetime="event.timestamp" />
-              </LinkV2>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+  <Page class="overflow-x-hidden">
+    <div class="flex flex-col relative w-full max-w-[962px] mx-auto mb-[48px]">
+      <Label
+        class="text-like-green mb-[8px]"
+        :text="$t('event_list_page_notifications', { number: notificationCount })"
+      >
+        <template #append>
+          <ButtonV2 preset="tertiary" @click="handleRefresh">
+            <IconRefresh />
+          </ButtonV2>
+        </template>
+      </Label>
+      <hr class="border-medium-gray">
+      <div v-if="isLoading || !processedEvents.length" class="flex items-center justify-center">
+        <CardV2 class="flex p-[8px] my-[48px]">
+          <Label preset="h5" class="text-like-green" :text="$t('nft_portfolio_page_label_loading')" />
+        </CardV2>
+      </div>
+      <div
+        v-for="group in groupedEventsByTime"
+        v-else
+        :key="group.date"
+        class="flex flex-col items-start my-[18px]"
+      >
+        <!-- date group -->
+        <div
+          :class="[
+            'py-[4px]',
+            'px-[8px]',
+            'mt-[12px]',
+            'mb-[8px]',
+            'bg-shade-gray',
+            'rounded-[4px]',
+            'w-auto',
+          ]"
+        >
+          {{ group.date }}
+        </div>
+        <!-- events -->
+        <LinkV2
+          v-for="event in group.events"
+          :key="[event.tx_hash, event.class_id, event.nft_id, event.eventType].join('-')"
+          :to="event.targetRoute"
+          :is-inline="true"
+          :class="[
+            'flex',
+            'justify-between',
+            'items-center',
+
+            'px-[16px]',
+            'py-[20px]',
+            'my-[4px]',
+            'ml-[4px]',
+            'bg-white',
+            'shadow-sm',
+            'rounded-[4px]',
+            'w-full',
+
+            'group',
+            'no-underline',
+            'cursor-pointer',
+            'hover:bg-shade-gray',
+            'duration-100',
+            'transition-colors',
+          ]"
+          @click.native.stop
+        >
+          <div class="flex items-center justify-start gap-[24px] mr-[12px]">
+            <!-- dot -->
+            <div
+              :class="[
+                'flex',
+                'justify-center',
+                'items-start',
+                'pl-[8px]',
+                'pt-[12px]',
+                'self-start',
+              ]"
+            >
+              <div
+                v-if="!event.eventHasSeen"
+                class="bg-danger rounded-[50%] w-[8px] h-[8px]"
+              />
+            </div>
+            <!-- avatar -->
+            <Identity
+              class="self-start flex-shrink-0"
+              :avatar-url="event.displayAvatar"
+              :avatar-size="38"
+            />
+            <!-- info -->
+            <div class="flex flex-col items-start justify-center">
+              <i18n
+                :class="[
+                  'text-dark-gray',
+                  'text-14',
+                  { 'text-medium-gray': event.eventHasSeen },
+                ]"
+                :path="event.i18nPath"
+              >
+                <p
+                  class="inline font-600 text-16"
+                  place="fromName"
+                >{{ event.fromName }}</p>
+                <p
+                  class="inline font-600 text-16"
+                  place="toName"
+                >{{ event.toName }}</p>
+                <p
+                  class="inline"
+                  place="nftName"
+                >{{ event.nftName }}</p>
+                <p
+                  class="inline text-like-green font-600"
+                  place="price"
+                >{{ event.price }}</p>
+
+              </i18n>
+              <div
+                v-if="event.message"
+                :class="[
+                  'flex',
+                  'items-center',
+                  'px-[12px]',
+                  'py-[6px]',
+                  'mt-[4px]',
+                  'border-[2px]',
+                  'border-shade-gray',
+                  'rounded-[4px]',
+
+                  'group-hover:border-light-gray',
+                  'duration-75',
+                  'transition-all'
+                ]"
+              >
+                <Label
+                  preset="p6"
+                  align="middle"
+                  class="text-medium-gray"
+                  :text="event.message"
+                />
+              </div>
+            </div>
+          </div>
+          <!-- timestamp-hour -->
+          <div class="text-medium-gray text-10">
+            {{ formatLocalTime(event.timestamp) }}
+          </div>
+        </LinkV2>
+      </div>
     </div>
   </Page>
 </template>
 <script>
 import { mapActions, mapGetters } from 'vuex';
 import { LIKECOIN_NFT_API_WALLET } from '~/constant';
-import { TimeAgo } from 'vue2-timeago';
 import { getChainExplorerTx } from '~/util/api';
+import { ellipsis } from '~/util/ui';
 
 export default {
   name: 'WalletEventsPage',
-  components: { TimeAgo },
+  layout: 'default',
+  filters: {
+    ellipsis,
+  },
+  data() {
+    return { isLoading: false };
+  },
   computed: {
     ...mapGetters([
       'getEvents',
@@ -53,60 +174,78 @@ export default {
       'getUserInfoByAddress',
       'getNFTClassMetadataById',
       'getHasUnseenEvents',
+      'getEventLastSeenTs',
     ]),
-    populatedEvents() {
+    processedEvents() {
       return this.getEvents.map(e => {
         let eventType = 'unknown';
-        let text = e;
+        let displayAvatar;
+        let i18nPath;
+        let message;
+        let fromName;
+        let toName;
+        let price;
+        let nftName;
+        const eventHasSeen =
+          this.getEventLastSeenTs &&
+          this.getEventLastSeenTs > new Date(e.timestamp).getTime();
+
         let memo = e.memo === 'like.co NFT API' ? '' : e.memo;
         if (e.action === '/cosmos.nft.v1beta1.MsgSend') {
-          const nftName = this.getNFTClassMetadataById(e.class_id)?.name;
+          nftName = this.getNFTClassMetadataById(e.class_id)?.name;
           if (e.sender === LIKECOIN_NFT_API_WALLET) {
             if (e.receiver === this.getAddress) {
               eventType = 'purchase_nft';
               const creator = this.getNFTClassMetadataById(e.class_id)
                 ?.iscn_owner;
-              text = this.$t('event_list_page_event_message_purchase_nft', {
-                nftName,
-                price: e.price,
-                creator:
-                  this.getUserInfoByAddress(creator)?.displayName || creator,
-              });
+              displayAvatar = this.getUserInfoByAddress(creator)?.avatar;
+              i18nPath = 'event_list_page_event_message_purchase_nft';
+              fromName = this.$t('event_list_page_event_self');
+              toName =
+                this.getUserInfoByAddress(creator)?.displayName || creator;
+              // eslint-disable-next-line prefer-destructuring
+              price = e.price;
               memo = e.granterMemo || '';
             } else {
               eventType = 'nft_sale';
-              text = this.$t('event_list_page_event_message_nft_sale', {
-                nftName,
-                price: e.price,
-                buyer:
-                  this.getUserInfoByAddress(e.receiver)?.displayName ||
-                  e.receiver,
-              });
+              fromName =
+                this.getUserInfoByAddress(e.receiver)?.displayName ||
+                e.receiver;
+              // eslint-disable-next-line prefer-destructuring
+              price = e.price;
+              i18nPath = 'event_list_page_event_message_nft_sale';
+              displayAvatar = this.getUserInfoByAddress(e.receiver)?.avatar;
+              memo = e.granterMemo || '';
             }
           } else if (e.receiver === this.getAddress) {
             eventType = 'receive_nft';
-            text = this.$t('event_list_page_event_message_receive_nft', {
-              nftName,
-              sender:
-                this.getUserInfoByAddress(e.sender)?.displayName || e.sender,
-            });
+            fromName =
+              this.getUserInfoByAddress(e.sender)?.displayName || e.sender;
+            i18nPath = 'event_list_page_event_message_receive_nft';
+            displayAvatar = this.getUserInfoByAddress(e.sender)?.avatar;
           } else {
             eventType = 'send_nft';
-            text = this.$t('event_list_page_event_message_send_nft', {
-              nftName,
-              receiver:
-                this.getUserInfoByAddress(e.receiver)?.displayName ||
-                e.receiver,
-            });
+            fromName = this.$t('event_list_page_event_self');
+            toName =
+              this.getUserInfoByAddress(e.receiver)?.displayName || e.receiver;
+            i18nPath = 'event_list_page_event_message_send_nft';
+            displayAvatar = this.getUserInfoByAddress(e.receiver)?.avatar;
           }
-          if (memo)
-            text += ` ${this.$t(
-              'event_list_page_event_message_nft_memo'
-            )}${memo}`;
+          if (memo) {
+            message = memo;
+          }
         }
+
         return {
           eventType,
-          text,
+          displayAvatar,
+          eventHasSeen,
+          message,
+          fromName,
+          toName,
+          price,
+          nftName,
+          i18nPath,
           targetRoute: {
             name: e.nft_id ? 'nft-class-classId-nftId' : 'nft-class-classId',
             params: {
@@ -118,6 +257,12 @@ export default {
         };
       });
     },
+    groupedEventsByTime() {
+      return this.convertToGroupedEvents(this.processedEvents);
+    },
+    notificationCount() {
+      return this.processedEvents.filter(event => !event.eventHasSeen).length;
+    },
   },
   mounted() {
     if (this.getHasUnseenEvents) {
@@ -127,8 +272,58 @@ export default {
   methods: {
     ...mapActions(['fetchWalletEvents', 'updateEventLastSeenTs']),
     getChainExplorerTx,
-    handleRefresh() {
-      this.fetchWalletEvents();
+    async handleRefresh() {
+      this.isLoading = true;
+      await this.fetchWalletEvents();
+      this.isLoading = false;
+    },
+    convertToGroupedEvents(events) {
+      const result = [];
+      const groupedEvents = {};
+
+      events.forEach(event => {
+        let date;
+        const today = new Date().setHours(0, 0, 0, 0);
+        const yesterday = today - 86400000;
+        const eventDate = new Date(event.timestamp).setHours(0, 0, 0, 0);
+        if (eventDate === today) {
+          date = 'Today';
+        } else if (eventDate === yesterday) {
+          date = 'Yesterday';
+        } else {
+          date = event.timestamp.toLocaleDateString('en-US');
+        }
+        if (!groupedEvents[date]) {
+          groupedEvents[date] = [];
+        }
+        groupedEvents[date].push(event);
+      });
+
+      Object.keys(groupedEvents).forEach(date => {
+        result.push({
+          date,
+          events: groupedEvents[date],
+        });
+      });
+
+      return result;
+    },
+    formatLocalTime(timestamp) {
+      const hour =
+        timestamp.getHours() >= 10
+          ? timestamp.getHours()
+          : `0${timestamp.getHours()}`;
+      const minutes =
+        timestamp.getMinutes() >= 10
+          ? timestamp.getMinutes()
+          : `0${timestamp.getMinutes()}`;
+      return `${hour}:${minutes}`;
+    },
+    checkHasSeenEvent(timestamp) {
+      return (
+        this.getEventLastSeenTs &&
+        this.getEventLastSeenTs < new Date(timestamp).getTime()
+      );
     },
   },
 };
