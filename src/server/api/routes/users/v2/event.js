d77e2a4347eeb2c057aa4a1f8a1e6ea9d075c4e1
William Chong <me@williamchong.cloud>
2023-02-06 15:20:04 +0800

    🐛 Fix event api path

--- src/server/api/routes/users/v2/event.js
+++ src/server/api/routes/users/v2/event.js
@@ -1,21 +1,22 @@
 const { Router } = require('express');
-const { walletUserCollection } = require('../../../../modules/firebase');
+const {
+  walletUserCollection,
+  FieldValue,
+} = require('../../../../modules/firebase');
 const { authenticateV2Login } = require('../../../middleware/auth');
 const { setPrivateCacheHeader } = require('../../../middleware/cache');
 
 const router = Router();
 
-router.get('/event/seen', authenticateV2Login, async (req, res, next) => {
+router.post('/event/seen', authenticateV2Login, async (req, res, next) => {
   try {
     setPrivateCacheHeader(res);
     const { user } = req.session;
     await walletUserCollection.doc(user).update({
-      eventLastSeenTs: firestore.FieldValue.serverTimestamp(),
+      eventLastSeenTs: FieldValue.serverTimestamp(),
     });
     res.sendStatus(200);
   } catch (err) {
-    if (req.session) req.session = null;
-    res.clearCookie(AUTH_COOKIE_NAME, CLEAR_AUTH_COOKIE_OPTION);
     next(err);
   }
 });
