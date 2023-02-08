ea5feac87ac9c523e197235751e659ee899a34d9
William Chong <6198816+williamchong@users.noreply.github.com>
2023-02-08 17:44:51 +0800

    🚸 Limit top ranked creators and top user list length (#917)
    * 🚸 Limit top ranked creators and top user list length
    
    * ✨ Random shuffle if top ranked creator hit limit

--- src/util/api/index.js
+++ src/util/api/index.js
@@ -144,10 +144,10 @@ export const getChainNFTClassMetadataEndpoint = classId =>
 export const getChainNFTMetadataEndpoint = (classId, nftId) =>
   `${LIKECOIN_CHAIN_API}/cosmos/nft/v1beta1/nfts/${classId}/${nftId}`;
 
-export const getTopCollectorOfUser = (creator, count = 6) =>
+export const getTopCollectorOfUser = (creator, count = 5) =>
   `${LIKECOIN_CHAIN_API}/likechain/likenft/v1/collector?pagination.limit=${count}&reverse=true&creator=${creator}&ignore_list=${LIKECOIN_NFT_API_WALLET}&include_owner=false`;
 
-export const getTopCreatorOfUser = (collector, count = 6) =>
+export const getTopCreatorOfUser = (collector, count = 5) =>
   `${LIKECOIN_CHAIN_API}/likechain/likenft/v1/creator?pagination.limit=${count}&reverse=true&collector=${collector}&ignore_list=${LIKECOIN_NFT_API_WALLET}&include_owner=false`;
 
 export const getChainNFTClassListingEndpoint = classId =>
--- src/util/misc.js
+++ src/util/misc.js
@@ -18,4 +18,13 @@ export function catchAxiosError(promise) {
   });
 }
 
+export function fisherShuffle(arr) {
+  for (let i = arr.length - 1; i > 0; i -= 1) {
+    const j = Math.floor(Math.random() * (i + 1));
+    // eslint-disable-next-line no-param-reassign
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
+
 export default sleep;
