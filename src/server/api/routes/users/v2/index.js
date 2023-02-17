fa7cf82dfaa33d1033b60f2be7a105eb28ae2282
Wei-Jun Hung <weijunhung@outlook.com>
2023-02-17 13:57:32 +0800

    ✨ Add locale APIs

--- src/server/api/routes/users/v2/index.js
+++ src/server/api/routes/users/v2/index.js
@@ -4,6 +4,7 @@ const auth = require('./auth');
 const email = require('./email');
 const event = require('./event');
 const follow = require('./follow');
+const locale = require('./locale');
 const nfts = require('./nfts');
 
 const router = Router();
@@ -12,6 +13,7 @@ router.use(auth);
 router.use(email);
 router.use(event);
 router.use(follow);
+router.use(locale);
 router.use(nfts);
 
 module.exports = router;
---
+++ src/server/api/routes/users/v2/locale.js
@@ -0,0 +1,44 @@
+const { Router } = require('express');
+const { authenticateV2Login } = require('../../../middleware/auth');
+const { setPrivateCacheHeader } = require('../../../middleware/cache');
+const { handleRestfulError } = require('../../../middleware/error');
+const { walletUserCollection } = require('../../../../modules/firebase');
+
+const { DEFAULT_LOCALE, AVAILABLE_LOCALES } = require('../../../constant');
+
+const router = Router();
+
+router.get('/locale', authenticateV2Login, async (req, res, next) => {
+  try {
+    const { user } = req.session;
+    const userDoc = await walletUserCollection.doc(user).get();
+    if (!userDoc.exists) {
+      res.json({ locale: DEFAULT_LOCALE });
+      return;
+    }
+    const { locale = DEFAULT_LOCALE } = userDoc.data();
+    res.json({ locale });
+  } catch (err) {
+    handleRestfulError(req, res, next, err);
+  }
+});
+
+router.post('/locale', authenticateV2Login, async (req, res, next) => {
+  try {
+    setPrivateCacheHeader(res);
+    const { user } = req.session;
+    const { locale } = req.body;
+    if (!locale || !AVAILABLE_LOCALES.includes(locale)) {
+      res.status(400).send('INVALID_LOCALE');
+      return;
+    }
+    await walletUserCollection.doc(user).update({
+      locale,
+    });
+    res.sendStatus(200);
+  } catch (err) {
+    handleRestfulError(req, res, next, err);
+  }
+});
+
+module.exports = router;
