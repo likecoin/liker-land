7bfdab267593beac484c744e0ad47951e2235d25
William Chong <me@williamchong.cloud>
2023-02-18 02:08:27 +0800

    ✨ Add creator filter in portfolio

--- src/pages/dashboard/index.vue
+++ src/pages/dashboard/index.vue
@@ -51,10 +51,13 @@
         :portfolio-items-sorting="currentNFTClassListSorting"
         :portfolio-items-sorting-order="currentNFTClassListSortingOrder"
         :portfolio-items-sorting-option-list="currentNFTClassSortingOptionList"
+        :portfolio-items-filtering="{ creator: nftCreatorFilter }"
+        :portfolio-collected-creator-list="nftCreatorInfoListOfCollected"
         :is-loading-portfolio-items="isLoading"
         :is-show-other-tab="isShowOtherTab"
         @portfolio-change-tab="handleTabChange"
         @portfolio-change-sorting="handleNFTClassListSortingChange"
+        @portfolio-change-filtering="handleNFTClassListFilteringChange"
         @infinite-scroll="handleInfiniteScroll"
       >
         <template #tab-bar-prepend>
