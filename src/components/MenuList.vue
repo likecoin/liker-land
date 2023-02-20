fd4f489a6b21e8e8d178fa56952e424b8d4eefb4
William Chong <me@williamchong.cloud>
2023-02-20 18:53:24 +0800

    🚸 [Creator Filter] Make list scrollable

--- src/components/MenuList.vue
+++ src/components/MenuList.vue
@@ -1,20 +1,45 @@
 <template>
-  <div
-    :class="[
-      'min-w-[150px]',
-      'w-max',
-      'bg-white',
-      'border-shade-gray',
-      'border-[1px]',
-      'rounded-[24px]',
-      'text-dark-gray',
-      'text-left',
-      'p-[12px]',
-      'grid grid-cols-1',
-      'divide-y',
-      'divide-shade-gray'
-    ]"
-  >
+  <div :class="rootClasses">
     <slot />
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    isFlat: {
+      type: Boolean,
+      default: false,
+    },
+    hasPadding: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  computed: {
+    rootClasses() {
+      const classes = [
+        'min-w-[150px]',
+        'w-max',
+        'text-dark-gray',
+        'text-left',
+        { 'p-[12px]': this.hasPadding },
+        'grid grid-cols-1',
+        'divide-y',
+        'divide-shade-gray',
+      ];
+
+      if (!this.isFlat) {
+        classes.push(
+          'bg-white',
+          'border-shade-gray',
+          'border-[1px]',
+          'rounded-[24px]'
+        );
+      }
+
+      return classes;
+    },
+  },
+};
+</script>
