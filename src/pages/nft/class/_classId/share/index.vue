d6a18d71e6a21c9bc81ec3eaa03f6cc5b61f981f
AuroraHuang22 <aurora90376@gmail.com>
2023-03-20 16:40:56 +0800

    🚑 Add locale to share route

--- src/pages/nft/class/_classId/share/index.vue
+++ src/pages/nft/class/_classId/share/index.vue
@@ -1,23 +1,27 @@
 <script>
 export default {
-  async fetch({ store, params, query, redirect }) {
+  async fetch({ store, params, query, redirect, localeLocation }) {
     const { classId } = params;
     const { referrer } = query;
     if (referrer) {
       const ownerMap = await store.dispatch('fetchNFTOwners', classId);
       const ownerCollectedNFTIds = ownerMap[referrer];
       if (ownerCollectedNFTIds) {
-        redirect({
-          name: 'nft-class-classId-nftId',
-          params: { classId, nftId: ownerCollectedNFTIds[0] },
-        });
-        return;
+        redirect(
+          localeLocation({
+            name: 'nft-class-classId-nftId',
+            params: { classId, nftId: ownerCollectedNFTIds[0] },
+          })
+        );
       }
+      return;
     }
-    redirect({
-      name: 'nft-class-classId',
-      params: { classId },
-    });
+    redirect(
+      localeLocation({
+        name: 'nft-class-classId',
+        params: { classId },
+      })
+    );
   },
 };
 </script>
