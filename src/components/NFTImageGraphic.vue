6d8b48b1f6dc7f5b853ac5d41923bd9f2ca2ad2a
AuroraHuang22 <75730405+AuroraHuang22@users.noreply.github.com>
2023-03-27 17:52:49 +0800

    💄 [ClassDetails/] Add CTA section (#1009)
    * 💄 [ClassDetails/] Add CTA section
    
    * 💄 Update CTA with no creators message
    
    * 💄 show priceSection if nft is primitive
    
    * 💬 Adjust wording

---
+++ src/components/NFTImageGraphic.vue
@@ -0,0 +1,65 @@
+<template>
+  <svg
+    width="148"
+    height="116"
+    viewBox="0 0 148 116"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+  >
+    <defs>
+      <pattern
+        id="pattern0"
+        patternContentUnits="objectBoundingBox"
+        x="0"
+        y="0"
+        width="32"
+        height="32"
+        patternTransform="scale(0.042) rotate(-29.56) translate(-1350,500)"
+      >
+        <image :href="href" x="0" y="0" width="32" height="32" />
+      </pattern>
+      <clipPath id="clip0_601_337">
+        <rect width="148" height="116" fill="white" />
+      </clipPath>
+    </defs>
+    <g clip-path="url(#clip0_601_337)">
+      <path d="M73.9744 116C114.664 116 147.65 110.736 147.65 104.243C147.65 97.75 114.664 92.4863 73.9744 92.4863C33.2845 92.4863 0.298828 97.75 0.298828 104.243C0.298828 110.736 33.2845 116 73.9744 116Z" fill="#ECFAAA" />
+      <mask
+        id="mask0_601_337"
+        style="mask-type:alpha"
+        maskUnits="userSpaceOnUse"
+        x="0"
+        y="0"
+        width="148"
+        height="116"
+      >
+        <path d="M147.64 104.243C147.64 110.736 114.654 116 73.9643 116C33.2744 116 0.288719 110.736 0.288719 104.243C0.288719 97.7501 -8.72421 0 73.9643 0C149.6 0 147.64 97.7501 147.64 104.243Z" fill="#50E3C2" />
+      </mask>
+      <g mask="url(#mask0_601_337)">
+        <path d="M42.3457 10.28V39.6" stroke="#AAF1E7" stroke-width="2" stroke-linecap="round" />
+        <path d="M73.9744 120.64C114.664 120.64 147.65 115.376 147.65 108.883C147.65 102.39 114.664 97.1262 73.9744 97.1262C33.2845 97.1262 0.298828 102.39 0.298828 108.883C0.298828 115.376 33.2845 120.64 73.9744 120.64Z" fill="#50E3C2" />
+        <path d="M74.3982 17.2977L21.2706 47.4237C13.5839 51.7824 10.886 61.5473 15.2448 69.234L45.3708 122.362C49.7295 130.048 59.4943 132.746 67.1811 128.387L120.309 98.2614C127.995 93.9026 130.693 84.1378 126.335 76.4511L96.2085 23.3235C91.8498 15.6368 82.085 12.9389 74.3982 17.2977Z" fill="#ECECEC" />
+        <path d="M74.3982 17.2977L21.2706 47.4237C13.5839 51.7824 10.886 61.5473 15.2448 69.234L45.3708 122.362C49.7295 130.048 59.4943 132.746 67.1811 128.387L120.309 98.2614C127.995 93.9026 130.693 84.1378 126.335 76.4511L96.2085 23.3235C91.8498 15.6368 82.085 12.9389 74.3982 17.2977Z" fill="url(#pattern0)" />
+        <path d="M68.0385 15.2872L14.9109 45.4132C7.46434 49.6358 4.85079 59.0954 9.07334 66.542L39.1993 119.67C43.4219 127.116 52.8816 129.73 60.3281 125.507L113.456 95.3811C120.902 91.1585 123.516 81.6989 119.293 74.2524L89.1672 21.1247C84.9447 13.6782 75.485 11.0647 68.0385 15.2872Z" stroke="#ECFAAA" />
+        <path d="M9.86523 85.6799V144" stroke="#AAF1E7" stroke-width="2" stroke-linecap="round" />
+        <path d="M124.705 57.8401V88.3201" stroke="#AAF1E7" stroke-width="2" stroke-linecap="round" />
+        <path d="M130.506 78.72V109.2" stroke="#AAF1E7" stroke-width="2" stroke-linecap="round" />
+        <path d="M37.7051 25.3601V46.5601" stroke="#AAF1E7" stroke-width="2" stroke-linecap="round" />
+        <path d="M70.1855 110.04V139.36" stroke="#AAF1E7" stroke-width="2" stroke-linecap="round" />
+        <path d="M74.8262 101.92V131.24" stroke="#AAF1E7" stroke-width="2" stroke-linecap="round" />
+      </g>
+    </g>
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    href: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
