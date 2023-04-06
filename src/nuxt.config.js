251f642c12f675f15943223574cdd82481a9bf5d
William Chong <me@williamchong.cloud>
2023-04-06 20:13:14 +0800

    🔍️ Add i18n head

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -275,10 +275,12 @@ const nuxtConfig = {
     locales: [
       {
         code: 'en',
+        iso: 'en',
         file: 'en.json',
       },
       {
         code: 'zh-Hant',
+        iso: 'zh-Hant',
         file: 'zh-Hant.json',
       },
     ],
@@ -289,6 +291,7 @@ const nuxtConfig = {
       useCookie: false,
       redirectOn: 'no prefix',
     },
+    baseUrl: IS_TESTNET ? 'https://rinkeby.liker.land' : 'https://liker.land',
   },
 
   /**
