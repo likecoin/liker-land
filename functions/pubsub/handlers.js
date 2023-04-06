e8ae88e9e6b737c62f680a6c1a98bbadcc98fd2f
Wei-Jun Hung <weijunhung@outlook.com>
2023-04-06 17:06:57 +0800

    🔧 Accept transfer notification by default

--- functions/pubsub/handlers.js
+++ functions/pubsub/handlers.js
@@ -224,7 +224,7 @@ export async function handleTransferEvent(message, data) {
 
   const {
     email,
-    notification: { transfer = false } = {},
+    notification: { transfer = true } = {},
     locale = 'en',
   } = userDocData;
 
