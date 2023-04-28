19e14ed7d77a1dd9fb027ad91ad23bc6ddc0fd67
William Chong <me@williamchong.cloud>
2023-04-29 00:59:48 +0800

    ⚗️ Lossen experiment eligibility

--- src/experiments/class-collect-cta.js
+++ src/experiments/class-collect-cta.js
@@ -1,7 +1,6 @@
 export default {
   name: 'class-collect-cta',
   experimentID: 'cljcOGr3RX-jUiSHCaMu-A',
-  isEligible: ({ route }) =>
-    !process.server && route.name === 'nft-class-classId',
+  isEligible: ({ route }) => !process.server && !!route.name,
   variants: [{ name: 'origin', weight: 1 }, { name: 'variant', weight: 1 }],
 };
