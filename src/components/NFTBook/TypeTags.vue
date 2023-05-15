5493d63aee6e30529db003bfd371a5eb59db3261
AuroraHuang22 <aurora90376@gmail.com>
2023-05-15 13:16:08 +0800

    💄 Update <TypeTags> UI

--- src/components/NFTBook/TypeTags.vue
+++ src/components/NFTBook/TypeTags.vue
@@ -1,19 +1,16 @@
 <template>
-  <div class="flex gap-[8px]">
-    <div
+  <ul class="flex gap-[8px]">
+    <li
       v-for="(type, index) in contentTypes"
       :key="type"
       class="flex gap-[8px] items-center"
     >
-      <ButtonV2
-        size="mini"
-        preset="tertiary"
-        content-class="px-[16px]"
-        :text="getContentType(type)"
-      />
+      <div class="py-[6px] px-[24px] bg-shade-gray rounded-[16px] text-10">
+        {{ getContentType(type) }}
+      </div>
       <IconAdd v-if="index !== contentTypes.length - 1" class="text-medium-gray" />
-    </div>
-  </div>
+    </li>
+  </ul>
 </template>
 <script>
 export default {
