2089bcc4dde3909c4fb2746dbf7343ae8ebf6d6b
AuroraHuang22 <aurora90376@gmail.com>
2023-06-09 09:27:02 +0800

    💄 Wrap <NuxtLink> with <li>

--- src/components/UserStats/IncomeTable.vue
+++ src/components/UserStats/IncomeTable.vue
@@ -1,53 +1,55 @@
 <template>
   <div>
     <ul v-if="currentList.length" class="flex flex-col px-[6px] laptop:px-[12px]">
-      <NuxtLink
-        v-for="item in currentList"
-        :key="item.classId"
-        :to="localeLocation(
-          { name: 'nft-class-classId',
-            params: { classId: item.classId } })"
-        target="_blank"
-        :class="[
-          'flex',
-          'flex-col',
-          'gap-[12px]',
-          'laptop:flex-row',
-          'laptop:gap-0',
+      <li>
+        <NuxtLink
+          v-for="item in currentList"
+          :key="item.classId"
+          :to="localeLocation(
+            { name: 'nft-class-classId',
+              params: { classId: item.classId } })"
+          target="_blank"
+          :class="[
+            'flex',
+            'flex-col',
+            'gap-[12px]',
+            'laptop:flex-row',
+            'laptop:gap-0',
 
-          'justify-start',
-          'items-start',
-          'laptop:justify-between',
-          'laptop:items-center',
+            'justify-start',
+            'items-start',
+            'laptop:justify-between',
+            'laptop:items-center',
 
-          'px-[12px]',
-          'py-[18px]',
-          'border-b-2',
-          'border-shade-gray',
+            'px-[12px]',
+            'py-[18px]',
+            'border-b-2',
+            'border-shade-gray',
 
-          'transition-all',
-          'duration-150',
-          'hover:bg-light-gray',
-        ]"
-      >
-        <div class="flex flex-col justify-start mr-[12px] text-dark-gray">
-          <Label preset="h5" class="text-like-green">{{ item.nftName }}</Label>
-          <div class="flex justify-start gap-[6px] mt-[6px]">
-            <ToolTips v-if="isTypeTotalSales" :tool-tip-text="$t('dashboard_table_class_price')">
-              <div class="py-[4px] px-[8px] rounded-[6px] bg-shade-gray text-10 cursor-default">{{ item.itemSales | formatNumberWithLIKE }}</div>
-            </ToolTips>
-            <ToolTips :tool-tip-text="$t('dashboard_table_class_stake_percentage')">
-              <div class="py-[4px] px-[8px] rounded-[6px] bg-like-cyan-pale text-10 cursor-default">{{ item.royaltyPercentage }}%</div>
-            </ToolTips>
+            'transition-all',
+            'duration-150',
+            'hover:bg-light-gray',
+          ]"
+        >
+          <div class="flex flex-col justify-start mr-[12px] text-dark-gray">
+            <Label preset="h5" class="text-like-green">{{ item.nftName }}</Label>
+            <div class="flex justify-start gap-[6px] mt-[6px]">
+              <ToolTips v-if="isTypeTotalSales" :tool-tip-text="$t('dashboard_table_class_price')">
+                <div class="py-[4px] px-[8px] rounded-[6px] bg-shade-gray text-10 cursor-default">{{ item.itemSales | formatNumberWithLIKE }}</div>
+              </ToolTips>
+              <ToolTips :tool-tip-text="$t('dashboard_table_class_stake_percentage')">
+                <div class="py-[4px] px-[8px] rounded-[6px] bg-like-cyan-pale text-10 cursor-default">{{ item.royaltyPercentage }}%</div>
+              </ToolTips>
+            </div>
           </div>
-        </div>
 
-        <ToolTips :tool-tip-text="$t('dashboard_table_class_net_earning')">
-          <div class="flex-shrink-0 cursor-default text-like-cyan-dark whitespace-nowrap">
-            {{ item.salesEarnings | formatNumberWithLIKE }}
-          </div>
-        </ToolTips>
-      </NuxtLink>
+          <ToolTips :tool-tip-text="$t('dashboard_table_class_net_earning')">
+            <div class="flex-shrink-0 cursor-default text-like-cyan-dark whitespace-nowrap">
+              {{ item.salesEarnings | formatNumberWithLIKE }}
+            </div>
+          </ToolTips>
+        </NuxtLink>
+      </li>
     </ul>
     <div v-else class=" w-full my-[62px]">
       <Label class="text-medium-gray" :text="$t('dashboard_table_no_data')" align="center" />
