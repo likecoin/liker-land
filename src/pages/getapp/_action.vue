cb1873f2b70d87a14cdb461fc55da81b92d53b47
William Chong <me@williamchong.cloud>
2023-06-28 13:43:20 +0800

    ✨ Add /getapp/_action page for handling univeral link actions

---
+++ src/pages/getapp/_action.vue
@@ -0,0 +1,10 @@
+<template>
+  <ProgressIndicator />
+</template>
+<script>
+export default {
+  fetch({ redirect, params, query, localeLocation }) {
+    redirect(302, localeLocation({ name: 'getapp', params, query }));
+  },
+};
+</script>
