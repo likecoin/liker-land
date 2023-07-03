ec7c3a0fd7a1cb1ec0b18f672b516519bd816d19
Wei-Jun Hung <weijunhung@outlook.com>
2023-07-03 18:17:19 +0800

    ⏪️ Revert "🐛 Update default configs in functions"

--- functions/api/config/config.js
+++ functions/api/config/config.js
@@ -1,8 +1,6 @@
 const config = {};
 
-const { IS_TESTNET } = process.env;
-
-config.IS_TESTNET = IS_TESTNET === 'TRUE';
+config.IS_TESTNET = process.env.IS_TESTNET === 'TRUE';
 
 config.FIRESTORE_USER_ROOT = process.env.FIRESTORE_USER_ROOT;
 config.FIRESTORE_NFT_MINT_SUBSCRIPTION_ROOT =
@@ -16,15 +14,7 @@ config.COOKIE_SECRET = process.env.COOKIE_SECRET;
 
 config.CRISP_USER_HASH_SECRET = process.env.CRISP_USER_HASH_SECRET;
 
-config.LIKECOIN_CHAIN_API = IS_TESTNET
-  ? 'https://node.testnet.like.co'
-  : 'https://mainnet-node.like.co';
-config.LIKECOIN_API_BASE = IS_TESTNET
-  ? 'https://api.rinkeby.like.co'
-  : 'https://api.like.co';
-config.EXTERNAL_URL = IS_TESTNET
-  ? 'https://rinkeby.liker.land'
-  : 'https://liker.land';
+config.EXTERNAL_URL = process.env.EXTERNAL_URL;
 
 config.GET_WALLET_API_SECRET = process.env.GET_WALLET_API_SECRET;
 
