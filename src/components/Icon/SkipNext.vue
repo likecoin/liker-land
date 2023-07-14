a2760d0ebfe4dba3f32f76822d3ef2f3804366fc
Ng Wing Tat, David <i@ngwingt.at>
2023-07-15 00:49:26 +0800

    💄 Use icon button for hero animation control

---
+++ src/components/Icon/SkipNext.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <path d="m18.15,19.38V4.62h1.85v14.77h-1.85Zm-14.15,0V4.62l10.65,7.38-10.65,7.38Zm1.85-3.54l5.57-3.85-5.57-3.85v7.69Z" fill="currentColor" />
+  </svg>
+</template>
