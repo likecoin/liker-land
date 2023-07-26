adf5307fc60ca6b168978722215a624fae53c282
AuroraHuang22 <aurora90376@gmail.com>
2023-07-26 11:40:35 +0800

    🎨 Do not filter the router name

--- src/experiments/nft-book-cta-text.js
+++ src/experiments/nft-book-cta-text.js
@@ -1,7 +1,6 @@
 export default {
   name: 'nft-book-cta-text',
   experimentID: 'jVyrPVSrRguBh7QF_AC8Gg',
-  isEligible: ({ route }) =>
-    !process.server && route.name === 'nft-class-classId',
+  isEligible: ({ route }) => !process.server && route.name,
   variants: [{ name: 'origin', weight: 1 }, { name: 'variant', weight: 1 }],
 };
