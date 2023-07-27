18462e12b544be66e5466dd6537d8b23c0072b06
AuroraHuang22 <aurora90376@gmail.com>
2023-07-28 01:34:01 +0800

    💄[_classId] Add <MarkDown> for rendering edition description

---
+++ src/components/Markdown.vue
@@ -0,0 +1,28 @@
+<!-- eslint-disable vue/no-v-html -->
+<template>
+  <div v-html="markdownToHtml" />
+</template>
+
+<script>
+import { marked } from 'marked';
+
+export default {
+  name: 'Markdown',
+  props: {
+    mdString: {
+      type: String,
+      default: '',
+    },
+  },
+  computed: {
+    markdownToHtml() {
+      return marked(this.mdString);
+    },
+  },
+};
+</script>
+
+<style>
+/* Import the custom styles for Markdown rendering */
+@import '@/assets/css/markdownStyle.css';
+</style>
