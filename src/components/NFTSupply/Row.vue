652c718c9906f39055cb0d9672796833ee31a590
Wei-Jun Hung <weijunhung@outlook.com>
2023-10-05 23:25:29 +0800

    👔 Lower the min init price to 0.5 USD

--- src/components/NFTSupply/Row.vue
+++ src/components/NFTSupply/Row.vue
@@ -146,7 +146,7 @@ export default {
       return this.type === 'past';
     },
     priceLabel() {
-      return `${Math.ceil(this.price).toLocaleString('en-US')}`;
+      return this.price.toLocaleString('en-US');
     },
     priceLabelClass() {
       return [
