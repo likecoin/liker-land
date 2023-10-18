027a95b333a0cfd24426c260f44c0305d31c66d4
William Chong <me@williamchong.cloud>
2023-10-18 15:32:20 +0800

    🔍️ Add title and description for book store pages

--- src/pages/store/books.vue
+++ src/pages/store/books.vue
@@ -10,7 +10,7 @@
     ]"
   >
     <section class="w-full">
-      <h2 class="text-[#3AB7A2] text-[48px] font-proxima font-[600]">{{ $t('store_books_page_title') }}</h2>
+      <h2 class="text-[#3AB7A2] text-[48px] font-proxima font-[600]">{{ $t('store_books_page_header') }}</h2>
       <NFTBookShelf
         class="mt-[3rem]"
         :items="nftBooks"
@@ -25,6 +25,7 @@
 import {
   LIKECOIN_NFT_BOOK_FEATURED_ITEMS,
   LIKECOIN_NFT_BOOK_ITEMS,
+  EXTERNAL_HOST,
 } from '~/constant';
 
 import { logTrackerEvent } from '~/util/EventLogger';
@@ -39,7 +40,34 @@ export default {
         href: `/api/nft/metadata?class_id=${classId}`,
       });
     });
+    const title = this.$t('store_books_page_title');
+    const description = this.$t('store_books_page_description');
     return {
+      title,
+      meta: [
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: title,
+        },
+        {
+          hid: 'description',
+          name: 'description',
+          content: description,
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: description,
+        },
+        {
+          hid: 'og:image',
+          property: 'og:image',
+          content: `${EXTERNAL_HOST}/images/og/${
+            this.$i18n.locale === 'zh-Hant' ? 'book-zh.png' : 'book.png'
+          }`,
+        },
+      ],
       link,
     };
   },
