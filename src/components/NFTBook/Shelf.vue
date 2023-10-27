75eaf475902a61f28c9e3893a35227ccaeca08ff
Ng Wing Tat, David <i@ngwingt.at>
2023-10-27 13:32:17 +0800

    ✨ Support multiple NFT Books in one shelf slot

--- src/components/NFTBook/Shelf.vue
+++ src/components/NFTBook/Shelf.vue
@@ -10,20 +10,21 @@
       ]"
     >
       <li
-        v-for="({ classId }, index) in items"
-        :id="classId"
-        :key="classId"
+        v-for="(item, index) in normalizedItems"
+        :id="item.classId"
+        :key="item.classId"
         class="max-w-[220px]"
       >
         <NFTBookItemCard
-          :class-id="classId"
+          :class-id="item.classId"
           preset="shelf"
           :shelf-class="[
             // NOTE: Make the shelf appear to be continuous.
             { 'sm:rounded-l-[0px]': index % 2 !== 0 && index % 3 === 1 },
             { 'desktop:rounded-l-[0px]': index % 3 === 2 },
           ]"
-          @click.native="$emit('click-item', classId)"
+          :is-link-disabled="item.isMultiple"
+          @click.native="handleClickItem(item)"
           @click-avatar="$emit('click-item-avatar', classId)"
         />
       </li>
@@ -33,8 +34,8 @@
         :key="`dummy-${i}`"
         :class="[
           'hidden',
-          { 'sm:block desktop:!hidden': i === 1 && items.length % 2 > 0 },
-          { 'desktop:block': items.length % 3 > 0 },
+          { 'sm:block desktop:!hidden': i === 1 && normalizedItems.length % 2 > 0 },
+          { 'desktop:block': normalizedItems.length % 3 > 0 },
           'relative',
           'w-full',
           'max-w-[220px]',
@@ -53,6 +54,30 @@
         />
       </li>
     </ul>
+    <Dialog
+      :open="dialogNFTClassList.length > 0"
+      :header-text="$t('nft_book_shelf_multiple_nft_class_dialog_title')"
+      panel-container-class="phone:max-w-[520px] laptop:max-w-[768px]"
+      panel-component="CardV2"
+      panel-class="overflow-y-scroll shadow-lg"
+      @close="closeMultipleNFTClassDialog"
+    >
+      <ul class="flex flex-col gap-[2rem]">
+        <li
+          v-for="classId in dialogNFTClassList"
+          :id="classId"
+          :key="classId"
+        >
+          <NFTBookItemCard
+            :class-id="classId"
+            preset="default"
+            component-class="!bg-like-cyan-pale"
+            @click.native="$emit('click-item', classId)"
+            @click-avatar="$emit('click-item-avatar', classId)"
+          />
+        </li>
+      </ul>
+    </Dialog>
   </div>
 </template>
 
@@ -63,6 +88,43 @@ export default {
     items: {
       type: Array,
       default: () => [],
+    },
+  },
+  data() {
+    return {
+      dialogNFTClassList: [],
+    };
+  },
+  computed: {
+    normalizedItems() {
+      return this.items.map(item => {
+        const isMultiple =
+          Array.isArray(item.classId) && item.classId.length > 1;
+
+        const newItem = {
+          ...item,
+          isMultiple,
+          classId: isMultiple ? item.classId[0] : item.classId,
+        };
+
+        if (isMultiple) {
+          newItem.classIds = item.classId;
+        }
+
+        return newItem;
+      });
+    },
+  },
+  methods: {
+    handleClickItem(item) {
+      if (item.isMultiple) {
+        this.dialogNFTClassList = item.classIds;
+      } else {
+        this.$emit('click-item', item.classId);
+      }
+    },
+    closeMultipleNFTClassDialog() {
+      this.dialogNFTClassList = [];
     },
   },
 };
