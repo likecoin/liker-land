9cf316610b32321c26857a5f06c9e16afdaa93bb
William Chong <me@williamchong.cloud>
2023-11-07 03:50:35 +0800

    ⚡️ Shorten redirect chain from /_id/civic

--- src/pages/_id/civic/index.vue
+++ src/pages/_id/civic/index.vue
@@ -1,7 +1,34 @@
 <script>
+import { checkUserNameValid } from '~/util/user';
+import { getUserMinAPI } from '~/util/api';
+
 export default {
-  fetch({ redirect, params, localeLocation }) {
-    redirect(301, localeLocation({ name: 'id', params }));
+  async fetch({ redirect, params, $api, localeLocation }) {
+    const { id } = params;
+    if (id && checkUserNameValid(id)) {
+      try {
+        const userInfo = await $api.$get(getUserMinAPI(id));
+        redirect(
+          301,
+          localeLocation({
+            name: 'id',
+            params: { id: userInfo.likeWallet },
+          })
+        );
+        return;
+      } catch (err) {
+        const msg = (err.response && err.response.data) || err;
+        // eslint-disable-next-line no-console
+        console.error(msg);
+      }
+    }
+    redirect(
+      301,
+      localeLocation({
+        name: 'id',
+        params: { id },
+      })
+    );
   },
 };
 </script>
