dab1ec4531db2dc6d3c76567a07c21a420e6ebaf
William Chong <me@williamchong.cloud>
2023-11-09 19:40:09 +0800

    ⚡️ Prevent multiple connect attempt to likerland inapp browser

--- src/mixins/in-app.js
+++ src/mixins/in-app.js
@@ -2,25 +2,11 @@ import { mapActions, mapGetters } from 'vuex';
 
 import { checkIsLikeCoinAppInAppBrowser } from '~/util/client';
 
-import walletLoginMixin from '~/mixins/wallet-login';
-
 export default {
-  mixins: [walletLoginMixin],
   computed: {
     ...mapGetters(['walletHasLoggedIn']),
     isInInAppBrowser() {
       return checkIsLikeCoinAppInAppBrowser(this.$route);
-    },
-  },
-  watch: {
-    isInInAppBrowser: {
-      immediate: true,
-      async handler(isInInAppBrowser) {
-        if (process.server || !isInInAppBrowser || this.walletHasLoggedIn) {
-          return;
-        }
-        await this.connectWallet();
-      },
     },
   },
   methods: {
