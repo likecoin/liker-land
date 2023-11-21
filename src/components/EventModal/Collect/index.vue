49748ba19a23f62b57be4d9e1ceae9aae3989b97
William Chong <6198816+williamchong@users.noreply.github.com>
2023-11-21 14:59:27 +0800

    ✨ Add share button in nft collect modal (#1409)
    * ✨ Add share button in nft collect modal
    
    * 🎨 Update share button style
    
    Co-authored-by: Ng Wing Tat, David <i@nwt.hk>
    
    * 🐛 Fix invalid import
    
    ---------
    
    Co-authored-by: Ng Wing Tat, David <i@nwt.hk>

--- src/components/EventModal/Collect/index.vue
+++ src/components/EventModal/Collect/index.vue
@@ -175,6 +175,18 @@
           <IconEye class="w-[20px]" />
         </template>
       </ButtonV2>
+
+      <ButtonV2
+        v-if="canShare"
+        preset="outline"
+        class="ml-[12px]"
+        :text="$t('nft_details_page_button_share')"
+        @click="shareNFTResult"
+      >
+        <template #prepend>
+          <IconShare class="w-[20px]" />
+        </template>
+      </ButtonV2>
     </template>
 
     <template v-else-if="isAddedToShoppingCart" #button>
@@ -362,6 +374,7 @@ import { formatNumberWithLIKE, oscillate } from '~/util/ui';
 
 import clipboardMixin from '~/mixins/clipboard';
 import nftMixin from '~/mixins/nft';
+import { EXTERNAL_HOST } from '~/constant';
 
 const FOLLOW_PROMPT_STATE = {
   DEFAULT: 'default', // No need to show any follow UI.
@@ -396,6 +409,7 @@ export default {
       modelExposure: 0,
       animationTimer: null,
       isAddedToShoppingCart: false,
+      canShare: false,
     };
   },
   head() {
@@ -613,6 +627,7 @@ export default {
     if (this.classId) {
       this.resetState();
     }
+    this.canShare = !!navigator.share;
   },
   beforeDestroy() {
     this.stopExposureAnimation();
@@ -769,6 +784,28 @@ export default {
     },
     onInputCollectMessage(e) {
       this.memo = e.target.value;
+    },
+    shareNFTResult() {
+      logTrackerEvent(this, 'NFT', 'NFTClickShare', this.classId, 1);
+      if (navigator?.share) {
+        const title = this.$t('nft_collect_modal_share_title', {
+          name: this.NFTName,
+        });
+        const text = this.$t('nft_collect_modal_share_text', {
+          name: this.NFTName,
+          creator: this.creatorDisplayName,
+        });
+        const url = this.justCollectedNFTId
+          ? `${EXTERNAL_HOST}/nft/class/${this.classId}/${
+              this.justCollectedNFTId
+            }`
+          : `${EXTERNAL_HOST}/nft/class/${this.classId}`;
+        navigator.share({
+          title,
+          text,
+          url,
+        });
+      }
     },
     async handleClickFollow() {
       try {
