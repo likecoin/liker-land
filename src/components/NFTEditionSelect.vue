006c2418c655cf6acf7c8c182df310e0cf56907c
Wei-Jun Hung <weijunhung@outlook.com>
2023-12-05 22:20:22 +0800

    🐛 Fix editions validator

--- src/components/NFTEditionSelect.vue
+++ src/components/NFTEditionSelect.vue
@@ -93,7 +93,7 @@ export default {
         if (!items.length) return true;
         return items.every(
           item =>
-            item.value &&
+            item.value >= 0 &&
             item.name &&
             item.priceLabel &&
             !Number.isNaN(item.stock) &&
