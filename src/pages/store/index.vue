dd090c9236e6c858e5fcd7d0eea0c03d1fc2437f
William Chong <me@williamchong.cloud>
2023-12-06 17:30:32 +0800

    💄 Add back button in book store page

--- src/pages/store/index.vue
+++ src/pages/store/index.vue
@@ -71,7 +71,7 @@ import navigationListenerMixin from '~/mixins/navigation-listener';
 import walletMixin from '~/mixins/wallet';
 
 export default {
-  name: 'WritingNFTPage',
+  name: 'StoreIndexPage',
   mixins: [inAppMixin, navigationListenerMixin, walletMixin],
   layout: 'default',
   async fetch({ route, redirect, localeLocation, store }) {
