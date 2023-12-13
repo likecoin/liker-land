27159543f9996f83237958ff71854829ff273bfe
AuroraHuang22 <aurora90376@gmail.com>
2023-12-13 23:23:54 +0800

    💄 Update button style

--- src/components/Gutenberg/Button.vue
+++ src/components/Gutenberg/Button.vue
@@ -5,10 +5,13 @@
       'relative',
       'border-[1px]',
       'border-like-green',
+      'border-opacity-75',
       'rounded-[48px]',
       'py-[8px]',
       'px-[24px]',
+      'group',
       'cursor-pointer',
+      hoverBorderClasses,
     ]"
     v-on="$listeners"
   >
@@ -26,6 +29,10 @@
         'py-[8px]',
         'px-[24px]',
         '-z-1',
+
+        hoverClasses,
+        'transition',
+        'duration-200',
       ]"
     >
       {{ text }}
@@ -44,6 +51,18 @@ export default {
     preset: {
       type: String,
       default: 'secondary',
+    },
+  },
+  computed: {
+    hoverBorderClasses() {
+      return this.preset === 'primary'
+        ? 'group-hover:border-opacity-50 active:border-opacity-70'
+        : 'group-hover:border-opacity-50 active:border-opacity-70';
+    },
+    hoverClasses() {
+      return this.preset === 'primary'
+        ? 'group-hover:!bg-like-cyan group-hover:!text-like-cyan'
+        : 'group-hover:!bg-medium-gray group-hover:!text-medium-gray ';
     },
   },
 };
