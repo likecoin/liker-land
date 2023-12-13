8758c39b1a50a71a34dc5341b7ba766e06585aed
William Chong <6198816+williamchong@users.noreply.github.com>
2023-12-13 15:24:19 +0800

    💄 Add gifting for book (#1450)
    * 💄 Add gift dialog in book
    
    * 💄 Add gift success page
    
    * 💄 Add gift message in claim page
    
    * 💄 Hide gift button for free book
    
    * 💄 Update GiftDialog UI
    
    * 💄 Display gift info on claim page (#6)
    
    * 💬 Update gift related text
    
    * 💬 Update book gift dialog placeholder
    
    Co-authored-by: Wei-Jun Hung <33746295+WeiJun0827@users.noreply.github.com>
    
    * 💩 Block LIKE book payment for gift
    
    ---------
    
    Co-authored-by: AuroraHuang22 <aurora90376@gmail.com>
    Co-authored-by: AuroraHuang22 <75730405+AuroraHuang22@users.noreply.github.com>
    Co-authored-by: Wei-Jun Hung <33746295+WeiJun0827@users.noreply.github.com>

--- src/pages/nft/class/_classId/index.vue
+++ src/pages/nft/class/_classId/index.vue
@@ -79,6 +79,7 @@
                 :value="defaultSelectedValue"
                 @change="handleEditionSelectChange"
                 @click-collect="handleCollectFromEditionSelector"
+                @click-gift="handleGiftFromEditionSelector"
                 @click-compare="handleClickCompareItemsButton"
               />
             </template>
@@ -290,6 +291,12 @@
         </section>
       </div>
     </div>
+    <NFTBookGiftDialog
+      :open="isGiftDialogOpen"
+      :selected-value="giftSelectedValue"
+      @submit="handleGiftSubmit"
+      @close="() => (isGiftDialogOpen = false)"
+    />
   </Page>
 </template>
 
@@ -327,6 +334,9 @@ export default {
       isOpenTransferModal: false,
       isTransferring: false,
       isCollecting: false,
+
+      isGiftDialogOpen: false,
+      giftSelectedValue: 0,
 
       trimmedCount: 10,
     };
@@ -877,7 +887,7 @@ export default {
       );
       return this.handleCollect();
     },
-    async handleCollectFromEdition(selectedValue) {
+    async handleCollectFromEdition(selectedValue, giftInfo = undefined) {
       const editions = this.getNFTBookStorePricesByClassId(this.classId) || {};
       const edition = editions[selectedValue];
       const hasStock = edition?.stock;
@@ -911,7 +921,8 @@ export default {
               },
             })
           );
-        } else if (edition.price > 0 && this.nftPriceInLIKE > 0) {
+          // gift does not support LIKE payment for now
+        } else if (!giftInfo && edition.price > 0 && this.nftPriceInLIKE > 0) {
           await this.initIfNecessary();
           if (this.hasConnectedWallet) {
             logPurchaseFlowEvent(
@@ -929,9 +940,16 @@ export default {
             classId: this.classId,
             priceIndex: edition.index,
             platform: this.platform,
-            gaClientId,
           });
-          window.open(link, '_blank', 'noopener');
+          const { url } = await this.$axios.$post(link, {
+            gaClientId,
+            giftInfo,
+          });
+          if (url) {
+            window.location.href = url;
+          } else {
+            throw new Error('Failed to get purchase link');
+          }
         }
       } else if (this.nftIsCollectable) {
         this.handleGotoCollectFromControlBar();
@@ -943,6 +961,28 @@ export default {
         this,
         'NFT',
         'nft_class_details_edition_selector_collect',
+        this.classId,
+        1
+      );
+    },
+    async handleGiftSubmit({ selectedValue, giftInfo }) {
+      logTrackerEvent(
+        this,
+        'NFT',
+        'nft_class_details_gift_submit',
+        this.classId,
+        1
+      );
+      await this.handleCollectFromEdition(selectedValue, giftInfo);
+      this.isGiftDialogOpen = false;
+    },
+    handleGiftFromEditionSelector(selectedValue) {
+      this.giftSelectedValue = selectedValue;
+      this.isGiftDialogOpen = true;
+      logTrackerEvent(
+        this,
+        'NFT',
+        'nft_class_details_edition_selector_gift',
         this.classId,
         1
       );
