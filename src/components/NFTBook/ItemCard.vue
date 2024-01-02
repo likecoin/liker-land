ddd75848c48d9955822c680d58a25909f25512c8
AuroraHuang22 <75730405+AuroraHuang22@users.noreply.github.com>
2024-01-02 15:54:23 +0800

    🎨 Add ellipsis for iscnWorkAuthor (#1480)

--- src/components/NFTBook/ItemCard.vue
+++ src/components/NFTBook/ItemCard.vue
@@ -132,7 +132,7 @@
             <span class="text-like-cyan-gray text-10">{{
               $t('identity_type_author')
             }}</span>
-            <span :class="['font-[600] truncate', displayNameStyle]">{{
+            <span :class="['font-[600]', displayNameStyle]">{{
               iscnWorkAuthor
             }}</span>
           </li>
