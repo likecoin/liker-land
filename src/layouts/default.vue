4104a24ec80ed9dcdcd3824f7335afb010f86ade
William Chong <me@williamchong.cloud>
2024-03-14 11:53:29 +0800

    ⚡️ Remove service worker and cache

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -169,6 +169,29 @@ export default {
       },
     },
   },
+  async mounted() {
+    // Remove these if we use PWA again
+    // Deregister service workers, delete all cache
+    try {
+      if (window.navigator && window.navigator.serviceWorker) {
+        const registrations = await window.navigator.serviceWorker.getRegistrations();
+        if (registrations?.length) {
+          registrations.forEach(registration => {
+            registration.unregister();
+          });
+        }
+      }
+      if (window.caches) {
+        const keyList = await window.caches.keys();
+        if (keyList?.length) {
+          await Promise.all(keyList.map(key => caches.delete(key)));
+        }
+      }
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(error);
+    }
+  },
   methods: {
     ...mapActions(['uiCloseTxModal']),
     onClickAlertBanner(type = 'primary') {
