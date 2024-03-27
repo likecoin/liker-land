0ef02c072238876813c020030e4b4f9d3c4d833d
Ng Wing Tat, David <i@ngwingt.at>
2024-03-27 15:38:28 +0800

    💄 Add new NFT Book Cover components

---
+++ src/components/NFTBook/Cover.vue
@@ -0,0 +1,87 @@
+<template>
+  <div
+    :class="[
+      'relative',
+      'flex',
+      'justify-center',
+      'items-center',
+      'w-full',
+      'h-full',
+      'min-h-[200px]',
+    ]"
+  >
+    <img
+      :class="[imageClassesInCommon, imgClass]"
+      :src="resizedSrc"
+      :alt="isLoaded ? alt : ''"
+      @load="handleMediaLoad"
+      @error="handleImageError"
+    />
+
+    <img
+      v-if="isShowPlaceholder"
+      :class="[imageClassesInCommon, 'aspect-[4/5]', 'animate-pulse']"
+      src="~/assets/images/nft/primitive-nft.jpg"
+    />
+  </div>
+</template>
+
+<script>
+import { getImageResizeAPI } from '~/util/api';
+
+export default {
+  props: {
+    src: {
+      type: String,
+      default: '',
+    },
+    alt: {
+      type: String,
+      default: 'Cover',
+    },
+    imgClass: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      isError: false,
+      isLoaded: false,
+    };
+  },
+  computed: {
+    isShowPlaceholder() {
+      return !this.src || !this.isLoaded || this.isError;
+    },
+    imageClassesInCommon() {
+      return [
+        'absolute',
+        'inset-y-0',
+        'h-full',
+        'rounded-[4px]',
+        'object-cover',
+        '[box-shadow:-8px_42px_64px_0px_rgba(0,_0,_0,_0.12)]',
+      ];
+    },
+    resizedSrc() {
+      return getImageResizeAPI(this.src, { width: 300 });
+    },
+  },
+  watch: {
+    src() {
+      this.isError = false;
+      this.isLoaded = false;
+    },
+  },
+  methods: {
+    handleMediaLoad() {
+      this.isLoaded = true;
+    },
+    handleImageError() {
+      this.isError = true;
+      this.isLoaded = true;
+    },
+  },
+};
+</script>
---
+++ src/components/NFTBook/CoverWithFrame.vue
@@ -0,0 +1,65 @@
+<template>
+  <component
+    :is="tag"
+    :class="[
+      'group',
+      'relative',
+      'aspect-[4/5]',
+      'bg-gray-e6',
+      'rounded-[8px]',
+      'overflow-hidden',
+    ]"
+    :to="to"
+  >
+    <div
+      class="absolute inset-0 bg-cover bg-no-repeat opacity-25 brightness-150 pointer-events-none"
+      :style="`background-image: url(${bgImageSrc})`"
+    />
+
+    <div
+      :class="[
+        'absolute',
+        'inset-[24px] desktop:inset-[40px]',
+        'flex',
+        'justify-center',
+        'items-center',
+      ]"
+    >
+      <NFTBookCover :src="src" :alt="alt" :img-class="imgClass" />
+    </div>
+  </component>
+</template>
+
+<script>
+import { getImageResizeAPI } from '~/util/api';
+
+export default {
+  props: {
+    src: {
+      type: String,
+      default: '',
+    },
+    alt: {
+      type: String,
+      default: '',
+    },
+    tag: {
+      type: String,
+      default: 'div',
+    },
+    imgClass: {
+      type: String,
+      default: '',
+    },
+    to: {
+      type: Object,
+      default: null,
+    },
+  },
+  computed: {
+    bgImageSrc() {
+      return getImageResizeAPI(this.src, { width: 4 });
+    },
+  },
+};
+</script>
