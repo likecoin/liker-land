d0ef50b2092e5610c9023cdc6edf05f5abc31238
William Chong <me@williamchong.cloud>
2024-05-08 16:26:34 +0800

    🚸 Use legacy like.co setting link for authcore user

--- src/util/links.js
+++ src/util/links.js
@@ -2,7 +2,11 @@ import { LIKE_CO_URL_BASE } from '~/constant';
 
 export const getLikeCoURL = (path = '') => `${LIKE_CO_URL_BASE}${path}`;
 
-export const getLikerIdSettingsURL = ({ wallet = '', language = 'en' } = {}) =>
+export const getLikerIdSettingsURL = ({
+  wallet = '',
+  language = 'en',
+  legacy = '',
+} = {}) =>
   getLikeCoURL(
-    `/in/settings?popup=1&user_wallet=${wallet}&language=${language}`
+    `/in/settings?popup=1&user_wallet=${wallet}&language=${language}&legacy=${legacy}`
   );
