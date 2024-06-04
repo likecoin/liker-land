1b1cf8c9b8f2a3eee5cfaeda223191dcae39a18a
William Chong <me@williamchong.cloud>
2024-06-04 15:30:39 +0800

    🔍️ Add book list APIs into sitemap

--- src/config/sitemap.js
+++ src/config/sitemap.js
@@ -13,6 +13,9 @@ const axios = Axios.create({
 const locales = ['zh-Hant', 'en'];
 /* Copied from constant due to nuxt.config.js is not es6 import syntax */
 const { IS_TESTNET } = process.env;
+const EXTERNAL_HOST = IS_TESTNET
+  ? 'https://rinkeby.liker.land'
+  : 'https://liker.land';
 const LIKE_CO_API = IS_TESTNET
   ? 'https://api.rinkeby.like.co'
   : 'https://api.like.co';
@@ -27,6 +30,8 @@ const getTopNFTClasses = `${LIKECOIN_CHAIN_API}/likechain/likenft/v1/ranking?ign
 const getTopCreators = `${LIKECOIN_CHAIN_API}/likechain/likenft/v1/creator?ignore_list=${LIKECOIN_NFT_API_WALLET}`;
 const getTopCollectors = `${LIKECOIN_CHAIN_API}/likechain/likenft/v1/collector?ignore_list=${LIKECOIN_NFT_API_WALLET}`;
 const getLatestBooks = `${LIKE_CO_API}/likernft/book/store/list`;
+const getBookStoreEditorialItems = `${EXTERNAL_HOST}/api/bookstore/items`;
+const getBookStoreList = `${EXTERNAL_HOST}/api/bookstore/lists`;
 
 /* actual routes logic */
 async function getSitemapRoutes() {
@@ -36,6 +41,8 @@ async function getSitemapRoutes() {
     creatorRes,
     collectorRes,
     newBookRes,
+    editorialBookRes,
+    bookListRes,
   ] = await Promise.all(
     [
       getLatestNFTClasses,
@@ -43,6 +50,8 @@ async function getSitemapRoutes() {
       getTopCreators,
       getTopCollectors,
       getLatestBooks,
+      getBookStoreEditorialItems,
+      getBookStoreList,
     ].map(url =>
       axios.get(url).catch(err => {
         // eslint-disable-next-line no-console
@@ -53,7 +62,16 @@ async function getSitemapRoutes() {
   );
   const classes = [].concat(
     ...[newClassRes, topClassRes].map(r => (r.data || {}).classes || []),
-    ...((newBookRes.data || {}).list || []).map(b => ({ id: b.classId }))
+    ...((newBookRes.data || {}).list || []).map(b => ({ id: b.classId })),
+    ...((editorialBookRes.data || {}).results || []).map(b => ({
+      id: b.classId,
+    })),
+    ...(((bookListRes.data || {}).results || {}).featured || []).map(b => ({
+      id: b.classId,
+    })),
+    ...(((bookListRes.data || {}).results || {}).highlighted || []).map(b => ({
+      id: b.classId,
+    }))
   );
   const classIds = classes.map(c => c.id);
   const nftDetailsPageRoutes = [...new Set(classIds)].map(
