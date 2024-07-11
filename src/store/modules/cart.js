4ee6046d810a53079f272cd3106bf69bfbdbbeed
William Chong <me@williamchong.cloud>
2024-07-12 00:48:43 +0800

    ✨ Add book cart in store

--- src/store/modules/cart.js
+++ src/store/modules/cart.js
@@ -9,6 +9,7 @@ import * as TYPES from '../mutation-types';
 
 const state = () => ({
   shoppingCartNFTClassByIdMap: {},
+  shoppingCartBookProductByIdMap: {},
 });
 
 const mutations = {
@@ -33,6 +34,43 @@ const mutations = {
   [TYPES.SHOPPING_CART_REPLACE_ALL_NFT_CLASS](state, map) {
     state.shoppingCartNFTClassByIdMap = map;
   },
+  [TYPES.SHOPPING_CART_ADD_BOOK_PRODUCT](state, newItem) {
+    const {
+      collectionId,
+      classId,
+      priceIndex,
+      coupon,
+      customPriceInDecimal,
+      from,
+    } = newItem;
+    const productId = collectionId || classId;
+    let item = state.shoppingCartBookProductByIdMap[productId];
+    if (!item || item.priceIndex !== priceIndex) {
+      item = {
+        productId,
+        collectionId,
+        classId,
+        coupon,
+        customPriceInDecimal,
+        from,
+        timestamp: Date.now(),
+        quantity: 1,
+        priceIndex: priceIndex || 0,
+      };
+    } else {
+      item = {
+        ...item,
+        quantity: item.quantity + 1,
+      };
+    }
+    Vue.set(state.shoppingCartBookProductByIdMap, productId, item);
+  },
+  [TYPES.SHOPPING_CART_REMOVE_BOOK_PRODUCT](state, { productId }) {
+    Vue.delete(state.shoppingCartBookProductByIdMap, productId);
+  },
+  [TYPES.SHOPPING_CART_REPLACE_ALL_BOOK_PRODUCT](state, map) {
+    state.shoppingCartBookProductByIdMap = map;
+  },
 };
 
 const getters = {
@@ -49,6 +87,23 @@ const getters = {
   },
   getShoppingCartNFTClassQuantity: state => classId =>
     state.shoppingCartNFTClassByIdMap[classId]?.quantity || 0,
+  shoppingCartBookProductList: (state, getter) => {
+    const list = Object.values(state.shoppingCartBookProductByIdMap).filter(
+      item => {
+        const priceInfo = getter.getNFTClassPaymentPriceById(
+          item.productId,
+          item.priceIndex
+        );
+        return !priceInfo || priceInfo?.fiatPrice > 0;
+      }
+    );
+    list.sort((a, b) => a.timestamp - b.timestamp);
+    return list;
+  },
+  shoppingCartBookItems: state =>
+    Object.values(state.shoppingCartBookProductByIdMap),
+  getShoppingCartBookProductQuantity: state => productId =>
+    state.shoppingCartBookProductByIdMap[productId]?.quantity || 0,
 };
 
 const actions = {
@@ -81,6 +136,37 @@ const actions = {
     commit(
       TYPES.SHOPPING_CART_REPLACE_ALL_NFT_CLASS,
       loadShoppingCartFromStorage()
+    );
+  },
+  addBookProductToShoppingCart({ commit, dispatch, getters }, item) {
+    if (getters.shoppingCartBookProductList.length >= BATCH_COLLECT_MAX) {
+      return;
+    }
+    commit(TYPES.SHOPPING_CART_ADD_BOOK_PRODUCT, item);
+    dispatch('saveBookProductShoppingCart');
+  },
+  addBookProductsToShoppingCart({ commit, dispatch }, items) {
+    commit(TYPES.SHOPPING_CART_REPLACE_ALL_BOOK_PRODUCT, {});
+    items.slice(0, BATCH_COLLECT_MAX).forEach(item => {
+      commit(TYPES.SHOPPING_CART_ADD_BOOK_PRODUCT, item);
+    });
+    dispatch('saveBookProductShoppingCart');
+  },
+  removeBookProductFromShoppingCart({ commit, dispatch }, { productId }) {
+    commit(TYPES.SHOPPING_CART_REMOVE_BOOK_PRODUCT, { productId });
+    dispatch('saveBookProductShoppingCart');
+  },
+  clearBookProductShoppingCart({ commit, dispatch }) {
+    commit(TYPES.SHOPPING_CART_REPLACE_ALL_BOOK_PRODUCT, {});
+    dispatch('saveBookProductShoppingCart');
+  },
+  saveBookProductShoppingCart({ state }) {
+    saveShoppingCartToStorage(state.shoppingCartBookProductByIdMap, 'book');
+  },
+  loadBookProductShoppingCart({ commit }) {
+    commit(
+      TYPES.SHOPPING_CART_REPLACE_ALL_BOOK_PRODUCT,
+      loadShoppingCartFromStorage('book')
     );
   },
 };
--- src/util/shopping-cart.js
+++ src/util/shopping-cart.js
@@ -1,18 +1,18 @@
 const STORAGE_KEY = 'liker_land_shopping-cart';
 
-export function saveShoppingCartToStorage(items) {
+export function saveShoppingCartToStorage(items, type = 'wnft') {
   try {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(items));
+    localStorage.setItem(`${STORAGE_KEY}_${type}`, JSON.stringify(items));
   } catch (e) {
     // eslint-disable-next-line no-console
     console.error('Failed to save shopping cart to local storage', e);
   }
 }
 
-export function loadShoppingCartFromStorage() {
+export function loadShoppingCartFromStorage(type = 'wnft') {
   let cart;
   try {
-    cart = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    cart = JSON.parse(localStorage.getItem(`${STORAGE_KEY}_${type}`));
   } catch (e) {
     // eslint-disable-next-line no-console
     console.error('Failed to load shopping cart from local storage', e);
