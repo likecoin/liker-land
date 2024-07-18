af24334d91bf5f1326675862c0d22f128076a9cf
Ng Wing Tat, David <i@ngwingt.at>
2024-07-18 17:53:26 +0800

    🚸 Add close button

--- src/components/Bodhisattva18/BannerCTA.vue
+++ src/components/Bodhisattva18/BannerCTA.vue
@@ -1,10 +1,35 @@
 <template>
   <aside
-    class="sticky bottom-0 z-10 bg-[#E5DFD3] shadow-sm"
+    :class="[
+      { sticky: isFlowing },
+      'bottom-0',
+      'z-10',
+      'bg-[#E5DFD3]',
+      'shadow-sm',
+    ]"
     :style="
       `background-image: linear-gradient(to right, #E5DFD3 50%, #E5DFD355 80%), url('${bannerBg}')`
     "
   >
+    <ButtonV2
+      class="absolute bottom-full right-0 m-[8px] shadow-md"
+      size="tiny"
+      :circle="true"
+      @click="close"
+    >
+      <svg
+        width="14"
+        height="14"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M14.8383 3.74744C15.2289 3.35691 15.862 3.35691 16.2525 3.74744C16.6431 4.13796 16.6431 4.77113 16.2525 5.16165L11.4142 10L16.2525 14.8383C16.6431 15.2289 16.6431 15.862 16.2525 16.2526C15.862 16.6431 15.2289 16.6431 14.8383 16.2526L9.99998 11.4142L5.16164 16.2526C4.77111 16.6431 4.13795 16.6431 3.74742 16.2526C3.3569 15.862 3.3569 15.2289 3.74742 14.8383L8.58577 10L3.74742 5.16165C3.3569 4.77113 3.3569 4.13796 3.74742 3.74744C4.13795 3.35691 4.77111 3.35691 5.16164 3.74744L9.99998 8.58578L14.8383 3.74744Z"
+          fill="currentColor"
+        />
+      </svg>
+    </ButtonV2>
+
     <div
       :class="[
         'flex',
@@ -56,7 +81,7 @@
       </div>
 
       <img
-        class="w-[96px] desktop:w-[120px] -mt-[32px]"
+        class="relative w-[96px] desktop:w-[120px] -mt-[32px]"
         :src="src"
         :alt="$t('bodhisattva18_flowing_cta_alt')"
       />
@@ -78,6 +103,7 @@ export default {
   data() {
     return {
       index: 1,
+      isFlowing: true,
     };
   },
   computed: {
@@ -105,6 +131,9 @@ export default {
       this.interval = setInterval(() => {
         this.index = ((this.index + 6) % 167) + 1;
       }, 200);
+    },
+    close() {
+      this.isFlowing = false;
     },
   },
 };
