9abfff1b41e4a0ad0825642e47a5086ed73e4bff
AuroraHuang22 <aurora90376@gmail.com>
2024-08-18 23:55:13 +0800

    🎨 Add store-articles link to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -73,6 +73,14 @@
                 {{ $t('footer_nav_mint_nft') }}
               </a>
             </div>
+            <div class="grid grid-flow-row gap-y-[16px]">
+              <NuxtLink
+                class="hover:underline"
+                :to="localeLocation({ name: 'store-articles' })"
+              >
+                {{ $t('footer_nav_store_articles') }}
+              </NuxtLink>
+            </div>
           </div>
         </div>
         <div class="flex items-start self-start">
