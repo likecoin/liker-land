75e1158891aebe4573ec21828c6250b2a57626e3
AuroraHuang22 <aurora90376@gmail.com>
2024-08-20 23:01:23 +0800

    💬Update login label in /Settings

--- src/pages/settings/index.vue
+++ src/pages/settings/index.vue
@@ -3,7 +3,7 @@
     <AuthRequiredView
       class="w-full max-w-[1130px] mx-auto"
       :is-loading-start-immediately="isInInAppBrowser"
-      :login-label="$t('dashboard_login_in')"
+      :login-label="$t('settings_login_in')"
       :login-button-label="$t('header_button_connect_to_wallet')"
     >
       <div
