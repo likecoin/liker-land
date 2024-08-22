0c2abacf243739d55fff63b0a5aebd1175c5ec1f
AuroraHuang22 <aurora90376@gmail.com>
2024-08-22 13:48:47 +0800

    📈 Add tracking when tipping dialog is opened

--- src/components/NFTBook/TippingDialog.vue
+++ src/components/NFTBook/TippingDialog.vue
@@ -157,6 +157,13 @@ export default {
       default: undefined,
     },
   },
+  watch: {
+    open(val) {
+      if (val) {
+        logTrackerEvent(this, 'tipping', 'tipping_open', this.currentId, 1);
+      }
+    },
+  },
   data() {
     return {
       selectedIndex: 1,
