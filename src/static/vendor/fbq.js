423f5b0228837f5fa9dc7ab80245c09e44038680
Ng Wing Tat, David <i@ngwingt.at>
2024-08-28 16:58:53 +0800

    📈 Update Facebook Pixel

--- src/static/vendor/fbq.js
+++ src/static/vendor/fbq.js
@@ -6,5 +6,5 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1013529535523534');
+fbq('init', '4022559437964917');
 fbq('track', 'PageView');
