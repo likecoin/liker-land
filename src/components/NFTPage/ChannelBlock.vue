e55755861482bcf3d881a6134940d840df092ff5
AuroraHuang22 <aurora90376@gmail.com>
2024-08-30 10:09:07 +0800

    🎨 Fetch channel displayName for @likerid

--- src/components/NFTPage/ChannelBlock.vue
+++ src/components/NFTPage/ChannelBlock.vue
@@ -47,6 +47,7 @@
 
 <script>
 import { AFFILIATION_CHANNELS } from '~/constant';
+import { getUserMinAPI } from '~/util/api';
 
 export default {
   name: 'NFTPageChannelBlock',
@@ -60,16 +61,48 @@ export default {
       default: false,
     },
   },
-  computed: {
-    affiliationName() {
-      const affiliation = AFFILIATION_CHANNELS.find(
-        channel => channel.id === this.channel
-      );
-
-      if (affiliation) {
-        return affiliation.name[this.$i18n.locale] || affiliation.name.en;
+  data() {
+    return {
+      affiliationName: '',
+      avatarUrl: '',
+    };
+  },
+  watch: {
+    channel: {
+      immediate: true,
+      handler(newVal) {
+        this.fetchAffiliationName(newVal);
+      },
+    },
+  },
+  methods: {
+    async fetchAffiliationName(channel) {
+      if (channel.startsWith('@')) {
+        const id = channel.slice(1);
+        try {
+          const { displayName, avatar } = await this.$api.$get(
+            getUserMinAPI(id)
+          );
+          this.affiliationName = displayName || id;
+          this.avatarUrl = avatar;
+          return;
+        } catch (error) {
+          // eslint-disable-next-line no-console
+          console.error(error);
+          this.affiliationName = id;
+          return;
+        }
       }
-      return this.channel;
+
+      const affiliation = AFFILIATION_CHANNELS.find(
+        store => store.id === channel
+      );
+      if (affiliation) {
+        this.affiliationName =
+          affiliation.name[this.$i18n.locale] || affiliation.name.en;
+      } else {
+        this.affiliationName = channel;
+      }
     },
   },
 };
