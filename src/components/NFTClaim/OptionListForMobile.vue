6438b44fd57ee70451b0eb9849be8368b06866c6
AuroraHuang22 <aurora90376@gmail.com>
2024-09-04 22:53:48 +0800

    📱 Add option list for mobile UI in /claim

---
+++ src/components/NFTClaim/OptionListForMobile.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="fixed left-0 bottom-0 z-50 bg-black bg-opacity-[30%]">
+    <div
+      class="flex flex-col items-center gap-[8px] w-full bg-white pl-[16px] pr-[8px] pt-[12px] pb-[28px]"
+    >
+      <div class="flex self-end">
+        <ButtonV2 preset="plain" @click="close">
+          <IconClose class="w-[20px]" />
+        </ButtonV2>
+      </div>
+      <MenuList :has-padding="false">
+        <ul>
+          <li
+            v-for="(contentUrl, index) in normalizedContentURLs"
+            :key="contentUrl"
+          >
+            <ButtonV2
+              preset="plain"
+              :download="getDownloadFilenameFromURL(contentUrl)"
+              @click="e => handleClickViewContentURL(e, contentUrl, index)"
+            >
+              {{
+                getFilenameFromURL(contentUrl) ||
+                  getContentUrlButtonText(contentUrl)
+              }}&nbsp;<IconLinkExternal />
+            </ButtonV2>
+          </li>
+        </ul>
+      </MenuList>
+    </div>
+  </div>
+</template>
+
+<script>
+import { getContentUrlType } from '~/util/misc';
+import { parseNFTMetadataURL } from '~/util/nft';
+import {
+  getFilenameFromURL,
+  getDownloadFilenameFromURL,
+} from '~/util/nft-book';
+
+export default {
+  name: 'NFTClaimOptionListForMobile',
+  props: {
+    url: {
+      type: String,
+      default: undefined,
+    },
+    classId: {
+      type: String,
+      default: undefined,
+    },
+    contentUrls: {
+      type: Array,
+      default: () => [],
+    },
+    iscnUrl: {
+      type: String,
+      default: undefined,
+    },
+    isNftBook: {
+      type: Boolean,
+      default: false,
+    },
+    isContentViewable: {
+      type: Boolean,
+      default: true,
+    },
+    shouldShowContentUrlButtons: {
+      type: Boolean,
+      default: true,
+    },
+    isContentDownloadable: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  computed: {
+    normalizedContentURLs() {
+      // NOTE: Assuming if only `url` is set, it must contain the actual content rather than the book info
+      if (this.contentUrls.length) return this.contentUrls;
+      if (this.url) return [this.url];
+      return [];
+    },
+  },
+  methods: {
+    close() {
+      this.$emit('close');
+    },
+    parseNFTMetadataURL,
+    getContentUrlButtonText(url) {
+      const type = getContentUrlType(url);
+      switch (type) {
+        case 'epub':
+          return this.$t('nft_details_page_button_view_epub');
+        case 'pdf':
+          return this.$t('nft_details_page_button_view_pdf');
+        default:
+          return this.$t('nft_details_page_button_view_unknown');
+      }
+    },
+    getFilenameFromURL,
+    getDownloadFilenameFromURL,
+    handleClickViewContentURL(e, contentUrl, index) {
+      const type = getContentUrlType(contentUrl);
+      const url = parseNFTMetadataURL(contentUrl);
+      this.$emit('view-content-url', e, url, type);
+      if (['pdf', 'epub'].includes(type)) {
+        e.preventDefault();
+        this.$router.push(
+          this.localeLocation({
+            name: `reader-${type}`,
+            query: {
+              download: this.isContentDownloadable ? '1' : '0',
+              classId: this.classId,
+              format: type,
+              index,
+            },
+          })
+        );
+      }
+    },
+  },
+};
+</script>
