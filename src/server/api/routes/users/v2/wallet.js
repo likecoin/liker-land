5dcddb03d1adfe05129eb8187f7479bd395ec4f3
William Chong <me@williamchong.cloud>
2024-09-05 02:01:24 +0800

    ✨ Add wallet query to wallet api

--- src/server/api/routes/users/v2/wallet.js
+++ src/server/api/routes/users/v2/wallet.js
@@ -7,25 +7,43 @@ const router = Router();
 router.get('/wallet', async (req, res, next) => {
   try {
     const key = req.headers['x-likerland-api-key'];
-    const { email } = req.query;
-    if (!email) {
-      res.status(400).send('MISSING_EMAIL');
+    const { email, wallet } = req.query;
+    if (email && wallet) {
+      res.status(400).send('INVALID_QUERY');
       return;
     }
     if (!key || key !== GET_WALLET_API_SECRET) {
       res.status(403).send('INVALID_KEY');
       return;
     }
-    const userDoc = await walletUserCollection
-      .where('email', '==', email)
-      .limit(1)
-      .get();
-    if (userDoc.empty) {
-      res.status(404).send('WALLET_NOT_FOUND');
+    let userDoc;
+    if (wallet) {
+      userDoc = await walletUserCollection.doc(wallet).get();
+      if (!userDoc.exists) {
+        res.status(404).send('USER_NOT_FOUND');
+        return;
+      }
+    } else if (email) {
+      const userQuery = await walletUserCollection
+        .where('email', '==', email)
+        .limit(1)
+        .get();
+      if (userQuery.empty) {
+        res.status(404).send('USER_NOT_FOUND');
+        return;
+      }
+      [userDoc] = userQuery.docs;
+    } else {
+      res.status(400).send('MISSING_QUERY');
       return;
     }
-    const wallet = userDoc.docs[0].id;
-    res.json({ wallet });
+    const userWallet = userDoc.id;
+    const { lastLoginMethod, registerLoginMethod } = userDoc.data();
+    res.json({
+      wallet: userWallet,
+      lastLoginMethod,
+      registerLoginMethod,
+    });
   } catch (err) {
     next(err);
   }
