23f2ef52278e5221c165c10e2459547eb8bd9cb1
AuroraHuang22 <aurora90376@gmail.com>
2024-09-10 13:36:33 +0800

    🚸 Add Crisp help for empty content in /bookshelf

--- src/components/NFTPortfolio/Empty.vue
+++ src/components/NFTPortfolio/Empty.vue
@@ -13,25 +13,58 @@
         <div
           class="w-[42px] h-[42px] rounded-[50%] bg-shade-gray border-[2px] border-white"
         />
-        <Label
-          class="text-medium-gray mt-[12px]"
-          :text="
-            preset === 'collected'
-              ? $t('portfolio_collected_tab_no_item')
-              : $t('portfolio_created_tab_no_item')
-          "
-        />
+        <Label class="text-medium-gray mt-[12px]" :text="cardText" />
+        <i18n
+          v-if="shouldShowCrisp"
+          class="flex items-center text-medium-gray text-[14px] mt-[12px]"
+          path="nft_claim_claimed_empty_collected_help"
+        >
+          <button
+            place="help"
+            class="cursor-pointer text-like-green font-600 mx-[4px] hover:underline"
+            @click="handleClickHelp"
+          >
+            {{ $t('nft_claim_claimed_help') }}
+          </button>
+        </i18n>
       </div>
     </div>
   </NFTPortfolioCard>
 </template>
+
 <script>
+import crispMixin from '~/mixins/crisp';
+import { logTrackerEvent } from '~/util/EventLogger';
+
 export default {
+  name: 'NFTPortfolioEmpty',
+  mixins: [crispMixin],
   props: {
     // The preset of empty card, option: collected and created
     preset: {
       type: String,
       default: 'collected',
+    },
+  },
+  computed: {
+    cardText() {
+      return this.preset === 'collected'
+        ? this.$t('portfolio_collected_tab_no_item')
+        : this.$t('portfolio_created_tab_no_item');
+    },
+    shouldShowCrisp() {
+      return (
+        this.$route.name.includes('bookshelf') && this.preset === 'collected'
+      );
+    },
+  },
+  methods: {
+    handleClickHelp() {
+      logTrackerEvent(this, 'bookshelf', 'bookshelf_crisp_click', '', 1);
+      if (this.$crisp) {
+        this.showCrisp();
+        this.$crisp.push(['do', 'chat:open']);
+      }
     },
   },
 };
