1deff00c73f6d8c459a1ec837c8d9c3ea2d59f43
AuroraHuang22 <aurora90376@gmail.com>
2024-09-20 17:33:40 +0800

    👔 Add authorize permission to sign message

--- src/util/cosmos.js
+++ src/util/cosmos.js
@@ -24,85 +24,85 @@ export function convertAddressPrefix(address, prefix = 'like') {
   return bech32.encode(prefix, words);
 }
 
-async function signLoginMessageMemo(signer, address, payload) {
-  const signingPayload = {
-    chain_id: LIKECOIN_CHAIN_ID,
-    memo: payload,
-    msgs: [],
-    fee: {
-      gas: '0',
-      amount: [
-        {
-          denom: LIKECOIN_CHAIN_MIN_DENOM,
-          amount: '0',
-        },
-      ],
-    },
-    sequence: '0',
-    account_number: '0',
-  };
-  const { signed, signature } = await signer.signAmino(address, signingPayload);
-  return {
-    signed: stringify(signed),
-    signature,
-    signMethod: 'memo',
-  };
-}
-
-async function signLoginMessageADR036(signer, address, message) {
-  if (!signer.signArbitrary) {
-    throw new Error('Signer does not support signArbitrary');
-  }
+export async function signLoginMessage(signer, address, action, permissions) {
   const chainId = LIKECOIN_CHAIN_ID;
-  const signature = await signer.signArbitrary(chainId, address, message);
-  const signDoc = {
-    msgs: [
-      {
-        type: 'sign/MsgSignData',
-        value: {
-          signer: address,
-          data: window.btoa(message),
-        },
-      },
-    ],
-    account_number: '0',
-    sequence: '0',
-    fee: {
-      gas: '0',
-      amount: [],
-    },
-    memo: '',
-    chain_id: '',
-  };
-  return {
-    signed: stringify(signDoc),
-    signature,
-    signMethod: 'ADR-036',
-  };
-}
-
-export async function signLoginMessage(signer, address) {
   const payload = [
     `${LOGIN_MESSAGE}:`,
     JSON.stringify({
       ts: Date.now(),
       address,
+      action,
+      permissions,
+      likeWallet: address,
     }),
   ].join(' ');
-  const sign = signer.signArbitrary
-    ? signLoginMessageADR036
-    : signLoginMessageMemo;
-  const {
-    signed: message,
-    signature: { signature, pub_key: publicKey },
-    signMethod,
-  } = await sign(signer, address, payload);
-  const data = {
-    signature,
-    publicKey: publicKey.value,
-    message,
-    from: address,
-    signMethod,
-  };
-  return data;
+
+  if (signer?.signArbitrary) {
+    const { signature, pub_key: publicKey } = await signer.signArbitrary(
+      chainId,
+      address,
+      payload
+    );
+    const signDoc = {
+      msgs: [
+        {
+          type: 'sign/MsgSignData',
+          value: {
+            signer: address,
+            data: window.btoa(payload),
+          },
+        },
+      ],
+      account_number: '0',
+      sequence: '0',
+      fee: {
+        gas: '0',
+        amount: [],
+      },
+      memo: '',
+      chain_id: '',
+    };
+    return {
+      signature,
+      signMethod: 'ADR-036',
+      publicKey: publicKey.value,
+      message: stringify(signDoc),
+      wallet: address,
+      from: address,
+      expiresIn: '7d',
+    };
+  }
+
+  if (signer?.signAmino) {
+    const signingPayload = {
+      chain_id: LIKECOIN_CHAIN_ID,
+      memo: payload,
+      msgs: [],
+      fee: {
+        gas: '0',
+        amount: [
+          {
+            denom: LIKECOIN_CHAIN_MIN_DENOM,
+            amount: '0',
+          },
+        ],
+      },
+      sequence: '0',
+      account_number: '0',
+    };
+    const { signed, signature } = await signer.signAmino(
+      address,
+      signingPayload
+    );
+    return {
+      signature: signature.signature,
+      publicKey: signature.pub_key.value,
+      message: stringify(signed),
+      wallet: address,
+      from: address,
+      signMethod: 'memo',
+      expiresIn: '7d',
+    };
+  }
+  throw new Error('SIGNER_NOT_SUPPORT_AMINO');
 }
