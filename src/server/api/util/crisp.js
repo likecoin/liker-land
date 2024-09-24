7489a014025c7dd3abd9853564682667dfd23b1f
William Chong <me@williamchong.cloud>
2024-09-24 14:04:07 +0800

    📈 Create crisp user profile from server events

--- src/server/api/util/crisp.js
+++ src/server/api/util/crisp.js
@@ -1,7 +1,25 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const crypto = require('crypto');
+const Crisp = require('crisp-api');
 
-const { CRISP_USER_HASH_SECRET } = require('../../config/config');
+const {
+  CRISP_USER_HASH_SECRET,
+  CRISP_PLUGIN_IDENTIFIER,
+  CRISP_PLUGIN_KEY,
+  CRISP_WEBSITE_ID,
+} = require('../../config/config');
+
+const CrispClient = new Crisp();
+
+const isCrispPluginEnabled =
+  CRISP_WEBSITE_ID && CRISP_PLUGIN_IDENTIFIER && CRISP_PLUGIN_KEY;
+if (isCrispPluginEnabled) {
+  CrispClient.authenticateTier(
+    'plugin',
+    CRISP_PLUGIN_IDENTIFIER,
+    CRISP_PLUGIN_KEY
+  );
+}
 
 function getCrispUserHash(email) {
   if (!CRISP_USER_HASH_SECRET || !email) return undefined;
@@ -11,6 +29,40 @@ function getCrispUserHash(email) {
     .digest('hex');
 }
 
+async function upsertCrispProfile(email, { displayName, wallet, loginMethod }) {
+  if (!isCrispPluginEnabled) return;
+  let people = null;
+  try {
+    people = await CrispClient.website.getPeopleProfile(
+      CRISP_WEBSITE_ID,
+      email
+    );
+  } catch {
+    // do nothing
+  }
+  if (people) {
+    await CrispClient.website.updatePeopleProfile(CRISP_WEBSITE_ID, email, {
+      person: {
+        nickname: displayName || wallet,
+      },
+      active: Math.floor(Date.now() / 1000),
+    });
+  } else {
+    await CrispClient.website.addNewPeopleProfile(CRISP_WEBSITE_ID, {
+      email,
+      person: {
+        nickname: displayName || wallet || email.split('@')[0],
+      },
+      active: Math.floor(Date.now() / 1000),
+    });
+  }
+  await CrispClient.website.updatePeopleData(CRISP_WEBSITE_ID, email, {
+    like_wallet: wallet,
+    login_method: loginMethod,
+  });
+}
+
 module.exports = {
   getCrispUserHash,
+  upsertCrispProfile,
 };
