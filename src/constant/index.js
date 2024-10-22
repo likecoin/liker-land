da7a240d78225cde129b3c590fd3d8f65fa538fe
AuroraHuang22 <aurora90376@gmail.com>
2024-10-22 19:22:21 +0800

    🔧 Add cross sell items

--- src/constant/index.js
+++ src/constant/index.js
@@ -320,10 +320,18 @@ export const CROSS_SELL_PRODUCT_IDS_MAP = IS_TESTNET
       likenft17zq3r5t2qwfatle04sz680vl3ka0l8wfeu4n5jv26mnvawnfqs4qd773ny: [
         'likenft154xhw0qyds5pgvsyc7379lnkyvwqkvv2zvmmh2gn5qrewljeqwys2sju6x',
       ],
+      likenft1l2v3qdv4qj0pyv2tmme42dv0962csaxxhezk04jr9s2rved08s4s4x6633: [
+        'likenft1434x9tl07ysrjq9lvxv0thgn0zt2fxwhntefd249lawrznw08lrskr3jfp',
+      ],
+      likenft1434x9tl07ysrjq9lvxv0thgn0zt2fxwhntefd249lawrznw08lrskr3jfp: [
+        'likenft1l2v3qdv4qj0pyv2tmme42dv0962csaxxhezk04jr9s2rved08s4s4x6633',
+      ],
     };
 
 export const DISABLED_CROSS_SELL_POPUP_LIST = [
   'likenft17zq3r5t2qwfatle04sz680vl3ka0l8wfeu4n5jv26mnvawnfqs4qd773ny', // 坐監情緒學
+  'likenft1434x9tl07ysrjq9lvxv0thgn0zt2fxwhntefd249lawrznw08lrskr3jfp', // 來一場散步學
+  'likenft1l2v3qdv4qj0pyv2tmme42dv0962csaxxhezk04jr9s2rved08s4s4x6633', // 鯨魚之城
 ];
 
 export const AFFILIATION_CHANNEL_LEGACY_STRINGS = {
