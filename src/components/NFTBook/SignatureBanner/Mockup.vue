9fabf9bdf310d1f0fdf38022d56358c573527af7
Ng Wing Tat, David <i@ngwingt.at>
2024-10-23 11:09:17 +0800

    ✨ Revamp mockup in signature banner

--- src/components/NFTBook/SignatureBanner/Mockup.vue
+++ src/components/NFTBook/SignatureBanner/Mockup.vue
@@ -1,15 +1,42 @@
 <template>
-  <img class="w-full max-w-[450px]" :src="image" />
+  <div class="flex justify-center w-full px-[16px]">
+    <svg
+      class="w-full max-w-[300px]"
+      viewBox="0 0 260 240"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+    >
+      <image
+        x="0"
+        y="0"
+        width="260"
+        height="241"
+        :xlink:href="deviceFrameImage"
+      />
+      <image
+        x="22"
+        y="23"
+        width="217"
+        height="218"
+        :xlink:href="signaturePageImage"
+      />
+    </svg>
+  </div>
 </template>
 
 <script>
-const signedBookImage = require('./signed-book.png');
+const signaturePage = require('./signature-page.png');
+const deviceFrame = require('./device-frame.png');
 
 export default {
   name: 'NFTBookSignatureBanner',
   computed: {
-    image() {
-      return signedBookImage;
+    signaturePageImage() {
+      return signaturePage;
+    },
+    deviceFrameImage() {
+      return deviceFrame;
     },
   },
 };
