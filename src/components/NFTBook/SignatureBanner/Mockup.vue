72084dd73719da8040213294310d9c22dfcb6270
Ng Wing Tat, David <i@ngwingt.at>
2024-10-23 13:13:47 +0800

    💫 Add signature animation

--- src/components/NFTBook/SignatureBanner/Mockup.vue
+++ src/components/NFTBook/SignatureBanner/Mockup.vue
@@ -21,22 +21,42 @@
         height="218"
         :xlink:href="signaturePageImage"
       />
+      <foreignObject x="28" y="172" width="86" height="52">
+        <client-only>
+          <LottieAnimation
+            :animation-data="animationData"
+            :auto-play="true"
+            :loop="true"
+            :speed="1"
+          />
+        </client-only>
+      </foreignObject>
     </svg>
   </div>
 </template>
 
 <script>
+import LottieAnimation from 'lottie-web-vue';
+
+import SignatureAnimation from './signature-animation.json';
+
 const signaturePage = require('./signature-page.png');
 const deviceFrame = require('./device-frame.png');
 
 export default {
   name: 'NFTBookSignatureBanner',
+  components: {
+    LottieAnimation,
+  },
   computed: {
     signaturePageImage() {
       return signaturePage;
     },
     deviceFrameImage() {
       return deviceFrame;
+    },
+    animationData() {
+      return SignatureAnimation;
     },
   },
 };
