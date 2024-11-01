bba027a057f2ee7a4bacd489b682df3b6730846b
William Chong <me@williamchong.cloud>
2024-11-01 14:27:32 +0800

    🚨 Fix lint error

--- src/pages/nft/claim/index.vue
+++ src/pages/nft/claim/index.vue
@@ -108,7 +108,11 @@
               <div v-if="creatorDisplayName" class="flex flex-col w-full">
                 <Label
                   preset="h6"
-                  :text="iscnWorkPublisher ? $t('identity_type_distributor') : $t('identity_type_publisher')"
+                  :text="
+                    iscnWorkPublisher
+                      ? $t('identity_type_distributor')
+                      : $t('identity_type_publisher')
+                  "
                   class=" text-medium-gray font-[500]"
                 />
                 <p class="text-[16px] font-[500] w-full break-words">
