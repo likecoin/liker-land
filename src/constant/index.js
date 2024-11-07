024e2f48daab98f203d89ac00d971282242a2a02
AuroraHuang22 <aurora90376@gmail.com>
2024-11-07 13:51:17 +0800

    🔧 Update cross-sell items for Dung

--- src/constant/index.js
+++ src/constant/index.js
@@ -322,6 +322,8 @@ export const RECOMMENDATION_GROUPS = IS_TESTNET
         'likenft14jz77ywl6hmfxlus6qrat36c2y7l5xkxhlzp76tdvk06mw69pjzsy5zu9l',
         'likenft16jguhkfa6nnu224fwjke2zv5f99n8wl9m097h46zqxnyu33j7rgs7f0xg3',
         'likenft1rfd3pp4q4hvuha8xpl8wzzf7uwannv0wm05s7jz34828e4qjnyzsfd9srz',
+        'likenft19symzw3xmh42gukzts858wf6rsdkn6e4jtc9wp8jh4kphfmffy5s6acyxg',
+        'likenft139537w38zlyhk9yvha4n3fz386kaakq6t3p62t23z3xs8f3v257q4k6mw4'
       ],
       [
         'likenft154xhw0qyds5pgvsyc7379lnkyvwqkvv2zvmmh2gn5qrewljeqwys2sju6x',
