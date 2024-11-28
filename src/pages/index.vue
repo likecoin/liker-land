27c460cce120f744af6d4e89234e22b81acd485f
AuroraHuang22 <aurora90376@gmail.com>
2024-11-28 21:38:41 +0800

    💄 Move read more button to feature section

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -548,6 +548,49 @@
             ]"
             v-text="$t('index_features_title')"
           />
+          <div class="flex justify-center">
+            <NuxtLink
+              :class="[
+                'relative',
+                'mt-[0.75rem]',
+                'flex',
+                'gap-[4px]',
+                'items-center',
+                'group',
+
+                'text-white',
+                'border-white',
+                'border-[1px]',
+                'rounded-[48px]',
+                'px-[20px]',
+                'py-[8px]',
+              ]"
+              :to="localeLocation({ name: 'about-nft-book' })"
+              @click.native="handleClickAboutNFTBook"
+            >
+              {{ $t('index_intro_more_button') }}
+
+              <svg
+                class="transition-transform duration-300 ease-in-out group-hover:translate-x-2"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="M5 12H19" />
+                  <path d="M15 16L19 12" />
+                  <path d="M15 8L19 12" />
+                </g>
+              </svg>
+            </NuxtLink>
+          </div>
 
           <NFTBookSignatureBanner
             class="mt-[32px] desktop:mt-[48px]"
