a386b79175dc2cdfabf26653da5ed788e51ccfcd
AuroraHuang22 <aurora90376@gmail.com>
2024-12-23 20:08:09 +0800

    ✨ Add <ScrollingList /> for buyer message animation

---
+++ src/components/ScrollingList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div
+    class="relative flex"
+    :style="{
+      '--animation-duration': durationTime,
+    }"
+  >
+    <ul class="flex animate-scroll">
+      <slot name="item" />
+    </ul>
+    <ul class="flex animate-scroll delay">
+      <slot name="item" />
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ScrollingList',
+  props: {
+    animationDuration: {
+      type: String,
+      default: '60s',
+    },
+  },
+  computed: {
+    durationTime() {
+      return this.animationDuration;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.animate-scroll {
+  animation: scroll var(--animation-duration, 60s) infinite linear;
+}
+
+.animate-scroll.delay {
+  animation: scroll var(--animation-duration, 60s) infinite linear;
+  animation-delay: calc(var(--animation-duration, 60s) * -1);
+}
+
+@keyframes scroll {
+  0% {
+    transform: translateX(100);
+  }
+  100% {
+    transform: translateX(-100%);
+  }
+}
+</style>
