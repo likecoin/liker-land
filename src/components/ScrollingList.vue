d3273c8ee2b982063567c3f00805107bd866a040
AuroraHuang22 <aurora90376@gmail.com>
2024-12-30 13:21:46 +0800

    💄 Simplify css style

--- src/components/ScrollingList.vue
+++ src/components/ScrollingList.vue
@@ -74,12 +74,11 @@ export default {
 
 <style scoped>
 @screen laptop {
-  .animate-scroll {
-    animation: scroll var(--animation-duration, 60s) infinite linear;
-  }
-
+  .animate-scroll,
   .animate-scroll.delay {
     animation: scroll var(--animation-duration, 60s) infinite linear;
+  }
+  .animate-scroll.delay {
     animation-delay: calc(var(--animation-duration, 60s) * -1);
   }
 }
