0772195c3d0ab2c73ed59d9b3ec55386c045dbdb
William Chong <me@williamchong.cloud>
2025-01-20 19:09:44 +0800

    🐛 Fix crisp.is is called before init

--- src/mixins/crisp.js
+++ src/mixins/crisp.js
@@ -10,7 +10,7 @@ export const CrispMixinFactory = (options = { isBootAtMounted: true }) => ({
     ]),
   },
   mounted() {
-    if (options.isBootAtMounted) this.$nextTick(() => this.showCrisp());
+    if (options.isBootAtMounted) this.showCrisp();
   },
   beforeDestroy() {
     if (options.isBootAtMounted) this.hideCrisp();
@@ -20,22 +20,20 @@ export const CrispMixinFactory = (options = { isBootAtMounted: true }) => ({
       if (!this.$crisp) return false;
       try {
         const { $crisp } = this;
-        if ($crisp.is('chat:hidden')) {
-          const email = this.walletEmail;
-          const wallet = this.loginAddress || this.getAddress;
-          const displayName = this.getLikerInfo?.displayName || wallet;
-          if (email && !$crisp.get('user:email')) {
-            $crisp.push(['set', 'user:email', [email]]);
-          }
-          if (displayName) {
-            $crisp.push(['set', 'user:nickname', [displayName]]);
-          }
-          if (wallet) {
-            $crisp.push(['set', 'session:data', [[['like_wallet', wallet]]]]);
-          }
-          $crisp.push(['do', 'chat:show']);
-          return true;
+        const email = this.walletEmail;
+        const wallet = this.loginAddress || this.getAddress;
+        const displayName = this.getLikerInfo?.displayName || wallet;
+        if (email && !$crisp.get('user:email')) {
+          $crisp.push(['set', 'user:email', [email]]);
         }
+        if (displayName) {
+          $crisp.push(['set', 'user:nickname', [displayName]]);
+        }
+        if (wallet) {
+          $crisp.push(['set', 'session:data', [[['like_wallet', wallet]]]]);
+        }
+        $crisp.push(['do', 'chat:show']);
+        return true;
       } catch (err) {
         console.error(err); // eslint-disable-line no-console
       }
