95381ec67ef5d9e3ec97232d4b480ba3b4856866
William Chong <me@williamchong.cloud>
2025-02-21 00:38:39 +0800

    🔥 Remove metadata api

--- src/server/api/routes/nft/index.js
+++ src/server/api/routes/nft/index.js
@@ -1,11 +1,9 @@
 const { Router } = require('express');
 
-const metadata = require('./metadata');
 const mintSubscription = require('./mint-subscription');
 
 const router = Router();
 
-router.use(metadata);
 router.use(mintSubscription);
 
 module.exports = router;
